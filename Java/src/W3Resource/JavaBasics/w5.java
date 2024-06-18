917d75f179f07a596ff802d09cee0df908b46bec
iTzAswinRoy <aswinroy255@gmail.com>
2024-06-18 19:36:48 +0530

    Source code

---
+++ Java/src/W3Resource/JavaBasics/w5.java
@@ -0,0 +1,23 @@
+package W3Resource.JavaBasics;
+import java.util.Scanner;
+
+public class w5 {
+        public static void main(String[] args) {
+            // Create a Scanner object to read input from the user
+            Scanner in = new Scanner(System.in);
+
+            // Prompt the user to input the first number
+            System.out.print("Input first number: ");
+            // Read and store the first number
+            int num1 = in.nextInt();
+            int num2 = in.nextInt();
+
+
+            // Prompt the user to input the second number
+            System.out.print("Input second number: ");
+            // Read and store the second number
+
+            // Calculate the product of the two numbers and display the result
+            System.out.println(num1 + " x " + num2 + " = " + num1 * num2);
+        }
+}
