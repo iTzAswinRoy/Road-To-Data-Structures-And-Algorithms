4b99155ed9cf34b481aa7be6f04158b012197005
iTzAswinRoy <aswinroy255@gmail.com>
2024-06-18 19:36:39 +0530

    Source code

---
+++ Java/src/W3Resource/JavaBasics/w2.java
@@ -0,0 +1,10 @@
+package W3Resource.JavaBasics;
+
+public class w2 {
+        public static void main(String[] args) {
+            // Calculate the sum of 24 and 26
+            int sum = 24 + 26;
+            // Print the result of the addition
+            System.out.println(sum);
+        }
+}
