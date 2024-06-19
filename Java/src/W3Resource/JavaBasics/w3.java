7f670ee1e0a5659d27aba90a18bb7c2e2bdbc493
iTzAswinRoy <aswinroy255@gmail.com>
2024-06-19 22:47:13 +0530

    Source code

--- Java/src/W3Resource/JavaBasics/w3.java
+++ Java/src/W3Resource/JavaBasics/w3.java
@@ -1,12 +1,23 @@
 package W3Resource.JavaBasics;
-
+import java.util.Scanner;
+// Write a Java program to divide two numbers and print them on the screen.
 public class w3 {
-        public static void main(String[] args) {
-            // Calculate the result of the division 50/3
-            int result = 50 / 3;
-
-            // Print the result of the division
-            System.out.println(result);
-        }
+    public static void main(String[] args) {
+        w3 obj = new w3();      // Creating an object to call the method
+        float result = obj.divide();      // Storing the divide method in a variable
+        obj.print(result);      // Printing the variable using a print method
+    }
+    public float divide(){     // Method to calculate division
+        Scanner in = new Scanner(System.in);    // Creating a scanner object
+        System.out.println("Enter the first number:");
+        float num1 = in.nextInt();    // Getting user inputs
+        System.out.println("Enter the first number:");
+        float num2 = in.nextInt();
+        in.close();     // Closing scanner
+        return num1 / num2;     // Returning the calculation
+    }
+    public void print(float result){      // Creating a print method
+        System.out.println("Divide = "+result);    // This prints the final result
+    }
 }
 
