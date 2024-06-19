5ad7eacedef726e8013f487453a8c4bf16bb9d51
iTzAswinRoy <aswinroy255@gmail.com>
2024-06-19 22:35:49 +0530

    Source code

--- Java/src/W3Resource/JavaBasics/w2.java
+++ Java/src/W3Resource/JavaBasics/w2.java
@@ -1,10 +1,20 @@
 package W3Resource.JavaBasics;
-
+import java.util.Scanner;
+// Write a Java program to print the sum of two numbers.
 public class w2 {
         public static void main(String[] args) {
-            // Calculate the sum of 24 and 26
-            int sum = 24 + 26;
-            // Print the result of the addition
-            System.out.println(sum);
+            w2 obj = new w2();      // Creating an object to call the method
+            int result = obj.sum(1,2);      // Storing the sum method in a variable
+            obj.print(result);      // Printing the variable using a print method
+        }
+        public int sum(int num1, int num2){     // Method to calculate the sum
+            Scanner in = new Scanner(System.in);    // Creating a scanner object
+            System.out.println("Enter the number:");
+            num1 = in.nextInt();    // Getting user inputs
+            num2 = in.nextInt();
+            return num1 + num2;     // Returning the calculation
+        }
+        public void print(int result){      // Creating a print method
+            System.out.println("Sum = "+result);    // This prints the final result
         }
 }
