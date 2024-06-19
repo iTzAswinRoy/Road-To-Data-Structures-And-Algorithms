a4e7bbf88ace7e546a11b465a34f6820abdd0d36
iTzAswinRoy <aswinroy255@gmail.com>
2024-06-19 23:28:53 +0530

    Source code

--- Java/src/W3Resource/JavaBasics/w11.java
+++ Java/src/W3Resource/JavaBasics/w11.java
@@ -1,15 +1,26 @@
 package W3Resource.JavaBasics;
+import java.util.Scanner;
+// Write a Java program to print the area and perimeter of a circle.
 public class w11 {
-        private static final double radius = 7.5;
-        public static void main(String[] args) {
-            // Calculate the perimeter of the circle using the constant radius
-            double perimeter = 2 * Math.PI * radius;
-
-            // Calculate the area of the circle using the constant radius
-            double area = Math.PI * radius * radius;
-
-            // Print the calculated perimeter and area
-            System.out.println("Perimeter is = " + perimeter);
-            System.out.println("Area is = " + area);
-        }
+    public static void main(String[] args) {
+        w11 obj = new w11();      // Creating an object to call the method
+        int radius = obj.input();   // Storing the method in a variable
+        obj.perimeter(radius);      // Calling the perimeter method
+        obj.area(radius);       // Calling the area method
+    }
+    public int input() {    // Method to get inputs from the user
+        Scanner in = new Scanner(System.in);    // Creating a scanner object
+        System.out.println("Enter the radius: ");
+        int r = in.nextInt();   // Getting the radius from the user
+        in.close();     // Closing scanner
+        return r;   // Returning the radius
+    }
+    public void perimeter(int radius){      // Method to calculate perimeter
+        double result = 2 * Math.PI * radius;       // Using the perimeter formula
+        System.out.println("Perimeter of the circle: "+result);     // Printing the result
+    }
+    public void area(int radius){       // Method to find the area of the circle
+        double result = Math.PI * radius * radius;      // Using area formula
+        System.out.println("Area of the circle: "+result);      // Printing the results
+    }
 }
