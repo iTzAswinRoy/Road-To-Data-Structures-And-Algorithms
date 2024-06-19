9df486d0206dd262e4337dd8da45ab8f9b725b1f
iTzAswinRoy <aswinroy255@gmail.com>
2024-06-19 22:49:01 +0530

    Source code

--- Java/src/W3Resource/JavaBasics/w2.java
+++ Java/src/W3Resource/JavaBasics/w2.java
@@ -4,14 +4,16 @@ import java.util.Scanner;
 public class w2 {
         public static void main(String[] args) {
             w2 obj = new w2();      // Creating an object to call the method
-            int result = obj.sum(1,2);      // Storing the sum method in a variable
+            int result = obj.sum();      // Storing the sum method in a variable
             obj.print(result);      // Printing the variable using a print method
         }
-        public int sum(int num1, int num2){     // Method to calculate the sum
+        public int sum(){     // Method to calculate the sum
             Scanner in = new Scanner(System.in);    // Creating a scanner object
-            System.out.println("Enter the number:");
-            num1 = in.nextInt();    // Getting user inputs
-            num2 = in.nextInt();
+            System.out.println("Enter the first number:");
+            int num1 = in.nextInt();    // Getting user inputs
+            System.out.println("Enter the second number:");
+            int num2 = in.nextInt();
+            in.close();     // Closing scanner
             return num1 + num2;     // Returning the calculation
         }
         public void print(int result){      // Creating a print method
