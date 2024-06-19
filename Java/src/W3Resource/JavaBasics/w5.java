a6807eab49b5bb2409d689b62ece268d6cce00f7
iTzAswinRoy <aswinroy255@gmail.com>
2024-06-19 22:58:27 +0530

    Source code

--- Java/src/W3Resource/JavaBasics/w5.java
+++ Java/src/W3Resource/JavaBasics/w5.java
@@ -1,23 +1,22 @@
 package W3Resource.JavaBasics;
 import java.util.Scanner;
-
+// Write a Java program that takes two numbers as input and displays the product of two numbers.
 public class w5 {
-        public static void main(String[] args) {
-            // Create a Scanner object to read input from the user
-            Scanner in = new Scanner(System.in);
-
-            // Prompt the user to input the first number
-            System.out.print("Input first number: ");
-            // Read and store the first number
-            int num1 = in.nextInt();
-            int num2 = in.nextInt();
-
-
-            // Prompt the user to input the second number
-            System.out.print("Input second number: ");
-            // Read and store the second number
-
-            // Calculate the product of the two numbers and display the result
-            System.out.println(num1 + " x " + num2 + " = " + num1 * num2);
-        }
+    public static void main(String[] args) {
+        w5 obj = new w5();      // Creating an object to call the method
+        int result = obj.multiply();      // Storing the multiply method in a variable
+        obj.print(result);      // Printing the variable using a print method
+    }
+    public int multiply(){     // Method to calculate multiplication
+        Scanner in = new Scanner(System.in);    // Creating a scanner object
+        System.out.println("Enter the first number:");
+        int num1 = in.nextInt();    // Getting user inputs
+        System.out.println("Enter the second number:");
+        int num2 = in.nextInt();    // Getting user inputs
+        in.close();     // Closing scanner
+        return num1 * num2;     // Returning the calculation
+    }
+    public void print(int result){// Creating a print method
+        System.out.println("Multiplication = "+result);    // This prints the final result
+    }
 }
