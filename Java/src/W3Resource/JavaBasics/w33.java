de973a481732d846769adeea49bdf85333806dab
iTzAswinRoy <aswinroy255@gmail.com>
2024-06-19 23:57:03 +0530

    Source code

---
+++ Java/src/W3Resource/JavaBasics/w33.java
@@ -0,0 +1,24 @@
+package W3Resource.JavaBasics;
+import java.util.Scanner;
+// 33
+public class w33 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Prompt the user to input an integer
+        System.out.print("Input an integer: ");
+        // Read the integer from the user
+        long n = input.nextLong();
+        // Calculate and display the sum of the digits
+        System.out.println("The sum of the digits is: " + sumDigits(n));
+    }
+    public static int sumDigits(long n) {
+        int sum = 0;
+        // Calculate the sum of the digits
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+        }
+}
+
