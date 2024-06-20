a0d3863aa8409b4af3689f82398088b66b83f586
iTzAswinRoy <aswinroy255@gmail.com>
2024-06-20 17:01:29 +0530

    Source code

--- Java/src/W3Resource/JavaBasics/w32.java
+++ Java/src/W3Resource/JavaBasics/w32.java
@@ -1,30 +1,26 @@
 package W3Resource.JavaBasics;
 import java.util.Scanner;
-// 32
+// Write a Java program to compare two numbers
 public class w32 {
-    public static void main(String args[]) {
-        // Create a Scanner to obtain input from the command window
-        Scanner input = new Scanner(System.in);
-        int number1; // First number to compare
-        int number2; // Second number to compare
-        // Prompt the user to input the first integer
-        System.out.print("Input first inteer: ");
-        number1 = input.nextInt(); // Read the first number from the user
-        // Prompt the user to input the second integer
-        System.out.print("Input second integer: ");
-        number2 = input.nextInt(); // Read the second number from the user
-        // Compare and display the results
-        if (number1 == number2)
-            System.out.printf("%d == %d\n", number1, number2);
-        if (number1 != number2)
-            System.out.printf("%d != %d\n", number1, number2);
-        if (number1 < number2)
-            System.out.printf("%d < %d\n", number1, number2);
-        if (number1 > number2)
-            System.out.printf("%d > %d\n", number1, number2);
-        if (number1 <= number2)
-            System.out.printf("%d <= %d\n", number1, number2);
-        if (number1 >= number2)
-            System.out.printf("%d >= %d\n", number1, number2);
+    public static void main(String[] args) {
+        w32 obj = new w32();    // Creating a class object to call the methods
+        System.out.println("Enter the first number: ");
+        int n1 = obj.userInputs();     // Storing inputs in a variable for num1
+        System.out.println("Enter the second number: ");
+        int n2 = obj.userInputs();      // Storing inputs in a variable for num2
+        obj.compare(n1, n2);    // Calling the method to compare the numbers
+    }
+    public int userInputs(){    // Method to get the user inputs
+        Scanner in = new Scanner(System.in);    // Creating a scanner object
+        return in.nextInt();    // returning the scanner
+    }
+    public void compare(int n1, int n2){    // Method to compare the two numbers
+        if (n1 == n2) {     // Condition if the both numbers are equal
+            System.out.println("Number 1 is equal to number 2");
+        }else if (n1 > n2) {    // Checking if the number is greater or less than
+            System.out.println("Number 1 is greater than number 2");
+        } else {
+            System.out.println("Number 1 is less than number 2");
+        }
     }
 }
