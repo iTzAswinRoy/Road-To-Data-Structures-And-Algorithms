3e5026cdc1e25f744a8a448291731740619442f0
iTzAswinRoy <aswinroy255@gmail.com>
2024-06-21 16:09:55 +0530

    Array List

--- Java/src/W3Resource/JavaBasics/w76.java
+++ Java/src/W3Resource/JavaBasics/w76.java
@@ -1,27 +1,47 @@
 package W3Resource.JavaBasics;
-
+import java.util.Objects;
+import java.util.ArrayList;
+import java.util.Scanner;
+// Write a Java program to test if the first and last element of two integer arrays are the same. The array length must be greater than or equal to 2.
 public class w76 {
-        public static void main(String[] args) {
-            // Define two integer arrays (num_array1 and num_array2)
-
-            // Example 1: Arrays with different first and last elements
-            int[] num_array1 = {50, -20, 0, 30, 40, 60, 12};
-            int[] num_array2 = {45, 20, 10, 20, 30, 50, 11};
-
-            // Example 2: Arrays with the same first and last elements
-            // int[] num_array1 = {50, -20, 0, 30, 40, 60, 50};
-            // int[] num_array2 = {45, 20, 10, 20, 30, 50, 45};
-
-            // Example 3: Arrays with lengths less than 2
-            // int[] num_array1 = {50};
-            // int[] num_array2 = {45};
-
-            if (num_array1.length >= 2 && num_array2.length >= 2) {
-                // Check if both arrays have lengths of at least 2 and if their first or last elements are equal
-                System.out.println(num_array1[0] == num_array2[0] || num_array1[num_array1.length - 1] == num_array2[num_array2.length - 1]);
-            } else {
-                System.out.println("Array lengths less than 2.");
-            }
-        }
+    private int count = 0;      // Declaring a count variable outside the method in order to count the method calls
+    public static void main(String[] args) {    // Main method
+        w76 obj = new w76();    // Creating a class object
+        ArrayList<Integer> arr1 = obj.userInputs();     // Storing values in array 1 using the input method
+        ArrayList<Integer> arr2 = obj.userInputs();     // Storing values in array 2 using the input method
+        System.out.println("The given arrays are:");
+        obj.display(arr1);      // Displaying the array
+        obj.display(arr2);      // Displaying the array
+        System.out.println("Checking if the fist and last element of both the array are equal.\n"+obj.checkArray(arr1, arr2));      // Calling the method to check the array
     }
+    public ArrayList<Integer> userInputs() {     // Method to get user inputs
+        Scanner in = new Scanner(System.in);    // Creating a scanner object
+        count++;    // Incrementing the count as we call the method
+        System.out.println("Enter the size for array "+count+":");
+        int size = in.nextInt();    // Getting the size from the user
+        ArrayList<Integer> temp = new ArrayList<>();
+        System.out.println("Enter the value's of array "+count+":");
+        for (int i = 0; i < size; i++) {    // Iterating over an array list
+            temp.add(in.nextInt());     // Assigning the values to an array list
+        }
+        return temp;     // Returning the inputs
+    }
+    public void display(ArrayList<Integer> data){     // Method to display the array list
+        System.out.println(data);   // Printing the array list
+    }
+    public boolean checkArray(ArrayList<Integer> arr1, ArrayList<Integer> arr2) {     // Method to check if both the array contains same first and last elements
+        if (arr1.size() >= 2 && arr2.size() >= 2) {     // Setting a condition that the array length should be greater than 2
+            for (int i = 0; i < arr1.size(); i++) {     // Iteration over array list 1
+                for (int j = 0; j < arr2.size(); j++) {     // Iteration over array list 2
+                    if(Objects.equals(arr1.getFirst(), arr2.getFirst()) && Objects.equals(arr1.getLast(), arr2.getLast())){     // Applying the main condition over here
+                        return true;    // Returns true if the conditions are met
+                    }
+                }
+            }
+        } else {
+            System.out.println("Invalid array size");
+        }
+        return false;   // Returns false if the conditions are not met
+    }
+}
 
