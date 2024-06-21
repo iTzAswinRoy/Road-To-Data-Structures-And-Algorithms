9ddf990c9162bfb01effccd068233fa298d0d214
iTzAswinRoy <aswinroy255@gmail.com>
2024-06-21 11:42:16 +0530

    Source code

--- Java/src/W3Resource/JavaBasics/w42.java
+++ Java/src/W3Resource/JavaBasics/w42.java
@@ -1,26 +1,25 @@
 package W3Resource.JavaBasics;
-import java.io.Console;
-
+import java.io.Console;     // This imports console libraries
+// Write a Java program to input and display your password.
 public class w42 {
-     public static void main(String[] args) {
-         // Declare a Console variable 'cons'.
-         Console cons;
-         // Check if the system console is available.
-         if ((cons = System.console()) != null) {// Declare a character array 'pass_ward' to store the password.
-             char[] pass_ward = null;
-             try {
-                 // Prompt the user to input their password.
-                 pass_ward = cons.readPassword("Input your Password:");
-                 // Display the password to the console.
-                 System.out.println("Your password was: " + new String(pass_ward));
-                } finally {
-                 // Ensure that the password array is securely cleared.
-                 if (pass_ward != null) {
-                     java.util.Arrays.fill(pass_ward, ' ');
-                 }
-             }
-         } else {
-             // If the system console is not available, throw a runtime exception.
-                throw new RuntimeException("Can't get the password... No console");}
-     }
+    public static void main(String[] args) {
+        w42 obj = new w42();    // Creating an abject
+        Console cons = obj.checkingConsole();   // Creating an abject from the console class
+        if (cons == null) {     // Checking whether the console is available or not
+            System.out.println("No console available");
+            return;
+        }
+        char[] pass_ward = cons.readPassword("Input your Password: ");      // Storing the password in character array
+        System.out.println("Your password was: " + new String(pass_ward));      // Printing the password by converting to sting
+        java.util.Arrays.fill(pass_ward, ' '); // Clear the password from memory
+    }
+    private Console checkingConsole() {     // Method to check if the console is available or not
+        Console checkCons = System.console();       // Creating a Console object
+        if (checkCons != null) {    // Condition for availability
+            System.out.println("Console is available");
+        } else {
+            System.out.println("Console is not available");
+            }
+        return checkCons;   // Returning the console
+    }
 }
