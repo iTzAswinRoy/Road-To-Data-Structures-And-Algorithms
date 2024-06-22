674b8caba27f84d2ca4f72c4a8ea9ad12352869e
iTzAswinRoy <aswinroy255@gmail.com>
2024-06-22 21:17:14 +0530

    (Pending)

---
+++ Java/src/W3Resource/JavaBasics/W192.java
@@ -0,0 +1,5 @@
+package W3Resource.JavaBasics;
+// Write a Java program to rearrange the alphabets in the order
+// followed by the sum of digits in a given string containing uppercase alphabets and integer digits (from 0 to 9).
+public class W192 {
+}
