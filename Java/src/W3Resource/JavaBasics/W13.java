6168ae7a408e5c546869143dd9ad4e88adc69fee
iTzAswinRoy <aswinroy255@gmail.com>
2024-06-24 22:10:25 +0530

    Source code

--- Java/src/W3Resource/JavaBasics/W13.java
+++ Java/src/W3Resource/JavaBasics/W13.java
@@ -1,12 +1,23 @@
 package W3Resource.JavaBasics;
-
+import java.util.Scanner;
+// Write a Java program to print the area and perimeter of a rectangle
 public class W13 {
-        public static void main(String[] strings) {
-            final double width = 5.6;
-            final double height = 8.5;
-            double perimeter = 2 * (height + width);
-            double area = width * height;
-            System.out.printf("Perimeter is 2*(%.1f + %.1f) = %.2f \n", height, width, perimeter);
-            System.out.printf("Area is %.1f * %.1f = %.2f \n", width, height, area);
-        }
+    public static void main(String[] strings) {     // Main method
+        W13 obj = new W13();    // Creating a class object to call a method
+        System.out.println("Enter the width of the rectangle:");
+        int width = obj.userInputs();   // Initializing width by calling the input method
+        System.out.println("Enter the height of the rectangle:");
+        int height = obj.userInputs();   // Initializing height by calling the input method
+        obj.sizeOfAnRectangle(width, height);   // Calling the method to find the size of the rectangle
     }
+    public int userInputs(){    // Method to receive inputs from the user
+        Scanner in = new Scanner(System.in);    // Creating a scanner object
+        return in.nextInt();    // Returning the inputs
+    }
+    public void sizeOfAnRectangle(int w, int h){    // Method to find the size of the rectangle
+        int perimeter = 2 * (h + w);    // Initializing the formula for perimeter of a rectangle
+        int area = w * h;   // Initializing the formula for area of a rectangle
+        System.out.println("Perimeter of a rectangle: "+perimeter);     // Displaying the perimeter of a rectangle
+        System.out.println("Area of a rectangle: "+area);   // Displaying the perimeter of a rectangle
+    }
+}
