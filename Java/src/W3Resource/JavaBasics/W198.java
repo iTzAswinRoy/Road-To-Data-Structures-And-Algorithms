bb6e585dff737929e185460df966a58d64387a1a
iTzAswinRoy <aswinroy255@gmail.com>
2024-06-24 22:23:45 +0530

    Code needs to be improved

--- Java/src/W3Resource/JavaBasics/W198.java
+++ Java/src/W3Resource/JavaBasics/W198.java
@@ -1,4 +1,44 @@
 package W3Resource.JavaBasics;
+
+import java.util.Scanner;
+
 //  Write a Java program to calculate the position of a given prime number.
 public class W198 {
+    public static void main(String[] args) {
+        W198 obj = new W198();
+        int primNumber = obj.userInputs();
+        System.out.println(" postion of prime numbre "+obj.findPrimePosition(primNumber) );
+    }
+
+    public int userInputs() {
+        Scanner in = new Scanner(System.in);
+        return in.nextInt();
+    }
+    public Boolean checkPrime(int number){
+        if(number <= 1){
+            return false;
+        }
+        if(number == 2 || number == 3){
+            return true;
+        }
+        for (int i = 3; i < Math.sqrt(number); i++) {
+            if(number % i == 0){
+                return false;
+            }
+        }
+        return true;
+
+    }
+    public int findPrimePosition(int value){
+        int count = 0;
+        int position = 1;
+        while (count < position){
+            position++;
+            if(checkPrime(value)){
+                count++;
+            }
+        }
+        return position;
+    }
 }
+
