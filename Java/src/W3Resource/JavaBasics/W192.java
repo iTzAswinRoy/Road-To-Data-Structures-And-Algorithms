19273b9f29fdc324d3bc22ffabe7fc36806eacb8
iTzAswinRoy <aswinroy255@gmail.com>
2024-06-25 15:43:25 +0530

    Source Code

--- Java/src/W3Resource/JavaBasics/W192.java
+++ Java/src/W3Resource/JavaBasics/W192.java
@@ -1,46 +1,44 @@
 package W3Resource.JavaBasics;
-
-import java.util.Scanner;
-
+import java.util.Scanner;   // Importing Scanner class
 // Write a Java program to rearrange the alphabets in the order
 // followed by the sum of digits in a given string containing uppercase alphabets and integer digits (from 0 to 9).
 public class W192 {
-    public static void main(String[] args) {
-        W192 obj = new W192();
+    public static void main(String[] args) {    // Main method
+        W192 obj = new W192();  // Creating a class object
         System.out.println("Enter a set of character's and number's in upper case:");
-        String text = obj.userInputs();
+        String upperCaseText = obj.userInputs();     // Creating a variable to store inputs from the user
         System.out.println("Sorting the character's with the sum of number's: ");
-        obj.rearrangeString(text);
+        obj.rearrangeCharacters(upperCaseText);    // Displaying the rearranged string by calling the method
     }
-    public String userInputs(){
-        Scanner in = new Scanner(System.in);
-        return in.nextLine();
+    public String userInputs(){     // Method to get inputs from the user
+        Scanner in = new Scanner(System.in);    // Creating a scanner object
+        return in.nextLine();   // Returning the scanner object
     }
-    public void rearrangeString(String input) {
-        char[] alphabets = new char[input.length()];
-        int alphabetsCount = 0;
-        int numSum = 0;
-        for (int i = 0; i < input.length(); i++) {
-            char character = input.charAt(i);
-            if (character >= 'A' && character <= 'Z') {
-                alphabets[alphabetsCount] = character;
-                alphabetsCount++;
-            } else if (character >= '0' && character <= '9') {
-                numSum += character - '0';
+    public void rearrangeCharacters(String input) {     // Method to rearrange the characters
+        char[] alphabets = new char[input.length()];    // Creating a character array to store the sorted characters
+        int alphabetsCount = 0;     // Creating a count for number of characters
+        int numSum = 0;     // Initializing the sum of number to 0 in order to store the sum value
+        for (int i = 0; i < input.length(); i++) {      // Iterating through each characters in an array
+            char character = input.charAt(i);   // Creating an instance variable to store each character for each iteration
+            if (character >= 'A' && character <= 'Z') {     // Checking if the array contains any characters
+                alphabets[alphabetsCount] = character;      //
+                alphabetsCount++;   // Incrementing count by 1
+            } else if (character >= '0' && character <= '9') {      // Checking if the array contains any numbers
+                numSum += character - '0';     // With the help of ASCII value, we can find the difference between zero character and given character
             }
         }
-        for (int i = 0; i < alphabetsCount - 1; i++) {
-            for (int j = 0; j < alphabetsCount - i - 1; j++) {
-                if(alphabets[j] > alphabets[j+1]) {
-                    char temp = alphabets[j];
+        for (int i = 0; i < alphabetsCount - 1; i++) {      // Using bubble sort to rearrange the alphabets
+            for (int j = 0; j < alphabetsCount - i - 1; j++) {     // Reducing the length for each sorted elements
+                if(alphabets[j] > alphabets[j+1]) {     // Checking if the element is greater than the next element
+                    char temp = alphabets[j];       // Swapping the element with the help of temporary variable
                     alphabets[j] = alphabets[j+1];
-                    alphabets[j+1] = temp;
+                    alphabets[j+1] = temp;      // Now the elements are swapped
                 }
             }
         }
-        for (int i = 0; i < alphabetsCount; i++) {
+        for (int i = 0; i < alphabetsCount; i++) {      // Printing each element in a for loop
             System.out.print(alphabets[i]);
         }
-        System.out.print(numSum);
+        System.out.print(numSum);   // Finally concatenation the sum of numbers along with the rearranged alphabets
     }
 }
