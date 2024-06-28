6025c74ea815c94c80c5e5cc4ccc2417192bf6e1
iTzAswinRoy <aswinroy255@gmail.com>
2024-06-28 22:35:24 +0530

    Updated code

--- Java/src/W3Resource/JavaBasics/W207.java
+++ Java/src/W3Resource/JavaBasics/W207.java
@@ -20,9 +20,6 @@ public class W207 {
         head = newNode;
         size++;
     }
-    public void sortLinkedList(Node node){
-
-    }
     public void createLinkedList(){
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the size of the Linked list "+listCount+":");
@@ -31,6 +28,22 @@ public class W207 {
         for (int i = 0; i < size; i++) {
             insertFirst(in.nextInt());
         }
+    }
+    public void sortLinkedList(){
+        for (Node i = head; i != null ; i = i.next) {
+            for (Node j = head; j.next != null ; j = j.next) {
+                if(j.value > j.next.value){
+                    int temp = j.value;
+                    j.value = j.next.value;
+                    j.next.value = temp;
+                }
+            }
+        }
+
+    }
+    public static W207 mergeLinkedList(W207 list1, W207 list2){
+
+
     }
     public void display(){
         System.out.println("Linked List "+listCount+":");
@@ -42,6 +55,7 @@ public class W207 {
                 System.out.print("END\n");
             }
         }
+
     }
     class Node{
         int value;
@@ -54,12 +68,16 @@ public class W207 {
     public static void main(String[] args) {
         W207 obj1 = new W207();
         obj1.createLinkedList();
-
         W207 obj2 = new W207();
         obj2.createLinkedList();
+
         obj1.display();
+        obj1.sortLinkedList();
+        obj1.display();
+
         obj2.display();
-
-
+        obj2.sortLinkedList();
+        obj2.display();
+        Node result = Node.mergeLinkedList(obj1, obj2;
     }
 }
