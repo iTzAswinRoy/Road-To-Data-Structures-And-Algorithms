b28994539cf7b909dfd97bdb662178f14997276a
iTzAswinRoy <aswinroy255@gmail.com>
2024-06-28 23:24:46 +0530

    Updated code

--- Java/src/W3Resource/JavaBasics/W207.java
+++ Java/src/W3Resource/JavaBasics/W207.java
@@ -5,11 +5,11 @@ public class W207 {
     Node head;
     Node tail;
     int size;
-    int count = 1;
-    int listCount;
+//    int count = 1;
+//    int listCount;
     W207(){
         this.size = 0;
-        this.listCount = count++;
+//        this.listCount = count++;
     }
     public void insertFirst(int data){
         Node newNode = new Node(data);
@@ -22,9 +22,9 @@ public class W207 {
     }
     public void createLinkedList(){
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter the size of the Linked list "+listCount+":");
+//        System.out.println("Enter the size of the Linked list "+listCount+":");
         int size = in.nextInt();
-        System.out.println("Enter the values of linked list "+listCount+":");
+//        System.out.println("Enter the values of linked list "+listCount+":");
         for (int i = 0; i < size; i++) {
             insertFirst(in.nextInt());
         }
@@ -42,11 +42,32 @@ public class W207 {
 
     }
     public static W207 mergeLinkedList(W207 list1, W207 list2){
+        W207 mergeList = new W207();
+        Node curr1 = list1.head;
+        Node curr2 = list2.head;
+        while (curr1 !=  null && curr2 != null){
+            if(curr1.value <= curr2.value){
+                mergeList.insertFirst(curr1.value);
+                curr1 = curr1.next;
+            } else {
+                mergeList.insertFirst(curr2.value);
+                curr2 = curr2.next;
+            }
+        }
+//        while (curr1 != null){
+//            mergeList
+//        }
+
+
+        return mergeList;
+//        if (curr1.value > curr2.value){
+//
+//        }
 
 
     }
     public void display(){
-        System.out.println("Linked List "+listCount+":");
+//        System.out.println("Linked List "+listCount+":");
         Node temp = head;
         while (temp != null){
             System.out.print(temp.value+"->");
@@ -67,17 +88,25 @@ public class W207 {
     }
     public static void main(String[] args) {
         W207 obj1 = new W207();
+        System.out.println("LL 1");
         obj1.createLinkedList();
         W207 obj2 = new W207();
+        System.out.println("LL 2");
         obj2.createLinkedList();
-
+        System.out.println("Display LL 1");
         obj1.display();
+        System.out.println("Sorted LL 1");
         obj1.sortLinkedList();
         obj1.display();
-
+        System.out.println();
+        System.out.println("Display LL 2");
         obj2.display();
+        System.out.println("Sorted LL 2");
         obj2.sortLinkedList();
         obj2.display();
-        Node result = Node.mergeLinkedList(obj1, obj2;
+        System.out.println();
+        System.out.println("Merge lists:");
+        W207 result = W207.mergeLinkedList(obj1, obj2);
+        result.display();
     }
 }
