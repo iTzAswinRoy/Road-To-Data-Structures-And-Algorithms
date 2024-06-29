0a2d7dbfd465bdb8c20a349c358fa4062719d460
iTzAswinRoy <aswinroy255@gmail.com>
2024-06-29 21:24:14 +0530

    Updated code

--- Java/src/W3Resource/JavaBasics/I1.java
+++ Java/src/W3Resource/JavaBasics/I1.java
@@ -1,8 +1,6 @@
 package W3Resource.JavaBasics;
-
-import java.util.Arrays;
-import java.util.Scanner;
-
+import java.util.Arrays;    // Importing array class
+import java.util.Scanner;   // Importing scanner class
 // Generate the following series and store them in an array. Display from the array, Display the Sum
 // Example: if N = 50, then Output: 1, 2, 5, 10, 17, 26, 37, 50, 65, 82, 101, 122 ....... N
 public class I1 {
@@ -17,21 +15,20 @@ public class I1 {
         Scanner in = new Scanner(System.in);    // Creating a scanner object
         return in.nextInt();    // Returning the input value
     }
-    public int[] series(int data){
-        int[] arr = new int[data];
-        int sum = 1;
-        for (int i = 0; i < data; i++) {
-            arr[i] = sum;
-            sum += (i + 1)*2;
-
+    public int[] series(int data){      // Method to create a series of array
+        int[] arr = new int[data];      // Creating an empty array
+        int sum = 1;    // Initializing sum value to 1
+        for (int i = 0; i < data; i++) {    // Iterating through the given array
+            arr[i] = sum;   // Assigning each element to the sum
+            sum += (i + 1) * 2;     // Applying the formula
         }
-        return arr;
+        return arr;     // Returning the array
     }
-    public int sum(int[] arr){
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
+    public int sum(int[] arr){      // Method to calculate the sum of the elements in an array
+        int sum = 0;    // Initializing te sum to 0
+        for (int i = 0; i < arr.length; i++) {      // Iterating through the array
+            sum += arr[i];     // Storing the sum of elements
         }
-        return sum;
+        return sum;     // Returning the sum
     }
 }
