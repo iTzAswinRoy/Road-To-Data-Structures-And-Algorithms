346a80004797356a9deff944d07211e044b05d83
iTzAswinRoy <aswinroy255@gmail.com>
2024-07-03 02:57:25 +0530

    Needs work

---
+++ Java/src/W3Resource/JavaStack/w1.java
@@ -0,0 +1,62 @@
+package W3Resource.JavaStack;
+
+public class w1 {   // Stack class
+    private int[] arr;
+    private int top;
+    w1(int size) {
+        arr = new int[size];
+        top = -1;
+    }
+    public void push(int num){
+        if(top == arr.length-1){
+            System.out.println("Stack is full");
+        } else {
+            top++;
+            arr[top] = num;
+        }
+    }
+    public void pop() {
+        if(top == -1){
+            System.out.println("Stack is empty");
+            return;
+        } else {
+            int popElement = arr[top];
+            top--;
+        }
+    }
+    public void peek(){
+        if (top == -1){
+            System.out.println("stack is empty");
+//            return -1;
+        } else {
+            System.out.println(arr[top]);;
+        }
+    }
+    public void display(){
+        for (int i = top; i >= 0; i--) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        w1 obj = new w1(5);
+        obj.push(1);
+        obj.push(2);
+        obj.push(3);
+        obj.push(4);
+        obj.push(5);
+        obj.display();
+
+        obj.pop();
+        System.out.println();
+        obj.display();
+
+        obj.pop();
+        System.out.println();
+        obj.display();
+
+        System.out.println();
+        obj.peek();
+    }
+}
+
