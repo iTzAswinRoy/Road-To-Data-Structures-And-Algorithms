1f16bfc8fc28c8aa8705f8bb6f378e42e290c267
iTzAswinRoy <aswinroy255@gmail.com>
2024-07-02 15:57:14 +0530

    Updated code

---
+++ Java/src/W3Resource/JavaInheritance/W9.java
@@ -0,0 +1,6 @@
+package W3Resource.JavaInheritance;
+// Write a Java program to create a vehicle class hierarchy. The base class should be Vehicle, with subclasses Truck, Car and Motorcycle.
+// Each subclass should have properties such as make, model, year, and fuel type.
+// Implement methods for calculating fuel efficiency, distance traveled, and maximum speed.
+public class W9 {
+}
