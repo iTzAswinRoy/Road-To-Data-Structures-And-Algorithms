9602080044b44732e1bf29576988b89854212ba4
iTzAswinRoy <aswinroy255@gmail.com>
2024-07-17 12:54:12 +0530

    Updated Code

---
+++ Java/src/JavaCircularLinkedList/CLL4.java
@@ -0,0 +1,77 @@
+package JavaCircularLinkedList;
+// Write a function to insert a node into a circular linked list at a specified position.
+public class CLL4 {
+    Node head;
+    Node tail;
+    int size;
+
+    CLL4() {
+        this.size = 0;
+    }
+
+    public void insertNode(int value) {
+        Node newNode = new Node(value);
+        if (tail == null) {
+            head = newNode;
+            tail = head;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+            tail.next = head;
+        }
+        size++;
+    }
+    public void nodeAtSpecificPosition(int index, int value) {
+        Node prev = tail;
+        Node curr = head;
+        Node newNode = new Node(value);
+        System.out.println("\nInserting node "+value+" at index "+index);
+        if(index > size){
+            System.out.println("\nIndex out of bound!");
+            return;
+        } else{
+            for (int i = 0; i < index; i++) {
+                prev = curr;
+                curr = curr.next;
+            }
+            prev.next = newNode;
+            newNode.next = curr;
+        }
+        size++;
+    }
+    public void display() {
+        Node temp = head;
+        do {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        } while (temp != head);
+        System.out.println("HEAD");
+    }
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args) {
+        CLL4 circularLinkedList = new CLL4();
+
+        circularLinkedList.insertNode(1);
+        circularLinkedList.insertNode(2);
+        circularLinkedList.insertNode(3);
+        circularLinkedList.insertNode(4);
+        circularLinkedList.insertNode(5);
+
+        System.out.println("Original circular linked list:");
+        circularLinkedList.display();
+
+        circularLinkedList.nodeAtSpecificPosition(2,7);
+
+        System.out.println("\nCircular linked list after inserting the node:");
+        circularLinkedList.display();
+    }
+}
