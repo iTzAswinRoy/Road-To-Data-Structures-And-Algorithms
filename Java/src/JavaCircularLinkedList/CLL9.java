6d967b8799586972f30073e384de4752ea2b87e2
iTzAswinRoy <aswinroy255@gmail.com>
2024-07-18 20:15:03 +0530

    Updated Code

--- Java/src/JavaCircularLinkedList/CLL9.java
+++ Java/src/JavaCircularLinkedList/CLL9.java
@@ -9,7 +9,7 @@ public class CLL9 {
         this.size = 0;
     }
 
-    public void insertNode(int value){
+    public void insertNode(int value){      //
         Node newNode = new Node(value);
         if(tail == null){
             head = newNode;
