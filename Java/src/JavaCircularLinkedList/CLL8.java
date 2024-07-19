4a2a833b45ca9dba49203177996daab13fd2f32b
iTzAswinRoy <aswinroy255@gmail.com>
2024-07-19 19:28:08 +0530

    Updated Code

--- Java/src/JavaCircularLinkedList/CLL8.java
+++ Java/src/JavaCircularLinkedList/CLL8.java
@@ -62,7 +62,7 @@ public class CLL8 {
     }
 
      public static void main(String[] args) {
-        CLL8 circularLinkedList = new CLL8();
+        CLL8 circularLinkedList = new CLL8();       //
         circularLinkedList.insertNode(1);
         circularLinkedList.insertNode(2);
         circularLinkedList.insertNode(3);
