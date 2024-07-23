2de5b7a955071d016e190a931452f1dab3f56c31
iTzAswinRoy <aswinroy255@gmail.com>
2024-07-23 19:11:33 +0530

    Arithmetic problem

--- Java/src/JavaBasics/CompareTwoNumbers.java
+++ Java/src/JavaBasics/CompareTwoNumbers.java
@@ -1,13 +1,16 @@
 package JavaBasics;
-import java.util.Scanner;
+import java.util.Scanner;     // Importing scanner class
 // Write a Java program to compare two numbers
-public class W32 {
-    public static void main(String[] args) {
-        W32 obj = new W32();    // Creating a class object to call the methods
+public class CompareTwoNumbers {
+    public static void main(String[] args) {    // Main method
+        CompareTwoNumbers obj = new CompareTwoNumbers();    // Creating a class object to call the methods
+
         System.out.println("Enter the first number: ");
         int n1 = obj.userInputs();     // Storing inputs in a variable for num1
+
         System.out.println("Enter the second number: ");
         int n2 = obj.userInputs();      // Storing inputs in a variable for num2
+
         obj.compare(n1, n2);    // Calling the method to compare the numbers
     }
     public int userInputs(){    // Method to get the user inputs
@@ -16,11 +19,11 @@ public class W32 {
     }
     public void compare(int n1, int n2){    // Method to compare the two numbers
         if (n1 == n2) {     // Condition if the both numbers are equal
-            System.out.println("Number 1 is equal to number 2");
-        }else if (n1 > n2) {    // Checking if the number is greater or less than
-            System.out.println("Number 1 is greater than number 2");
+            System.out.println("Both numbers are equal!");
+        } else if (n1 > n2) {    // Checking if the number is greater or less than
+            System.out.println("Number "+n1+" is greater than number "+n2);
         } else {
-            System.out.println("Number 1 is less than number 2");
+            System.out.println("Number "+n1+" is less than number "+n2);
         }
     }
 }
