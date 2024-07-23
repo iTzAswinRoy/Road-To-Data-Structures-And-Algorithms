7667dd21c31fb430a7a71264aa629bd509524d48
iTzAswinRoy <aswinroy255@gmail.com>
2024-07-23 19:01:51 +0530

    Arithmetic problem

--- Java/src/JavaBasics/FindingCircleDimensions.java
+++ Java/src/JavaBasics/FindingCircleDimensions.java
@@ -5,14 +5,17 @@ public class FindingCircleDimensions {
     public static void main(String[] args) {    // Main method
         FindingCircleDimensions obj = new FindingCircleDimensions();      // Creating an object to call the method
         int radius = obj.input();   // Storing the method in a variable
+
         obj.perimeter(radius);      // Calling the perimeter method
         obj.area(radius);       // Calling the area method
     }
 
     public int input() {    // Method to get inputs from the user
         Scanner in = new Scanner(System.in);    // Creating a scanner object
+
         System.out.println("Enter the radius: ");
         int r = in.nextInt();   // Getting the radius from the user
+
         in.close();     // Closing scanner
         return r;   // Returning the radius
     }
