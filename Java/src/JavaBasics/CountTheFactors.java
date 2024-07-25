8a77bdc655598f51112e6e680984b96b53ce3fad
iTzAswinRoy <aswinroy255@gmail.com>
2024-07-25 18:26:46 +0530

    Arithmetic problem

--- Java/src/JavaBasics/CountTheFactors.java
+++ Java/src/JavaBasics/CountTheFactors.java
@@ -1,9 +1,9 @@
 package JavaBasics;
-import java.util.Scanner;
+import java.util.Scanner;   // Importing scanner class
 // Write a Java program to accept an integer and count the factors of the number
-public class w57 {
+public class CountTheFactors {
     public static void main(String[] args) {    // Main method
-        w57 obj = new w57();    // Creating an object
+        CountTheFactors obj = new CountTheFactors();    // Creating an object
         int number = obj.userInput();   // Storing the value by calling the method
         obj.countFactor(number);     // Calling the method to find the factors of a number
     }
@@ -14,7 +14,7 @@ public class w57 {
     }
     public void countFactor(int value){      // Method to count the factors
         int count = 0;      // Setting to count to 0
-        for (int i = 1; i <= value; i++) {
+        for (int i = 1; i <= value; i++) {      // Iterating through each value
             if (value % i == 0 ){      // Checking if the value give reminder 0
                 count++;    // Incrementing the count by 1
             }
