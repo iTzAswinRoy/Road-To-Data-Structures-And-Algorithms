a94c1bbd458b4786b1925591290c77884cad5485
iTzAswinRoy <aswinroy255@gmail.com>
2024-07-25 18:12:53 +0530

    String

--- Java/src/JavaBasics/ReverseString.java
+++ Java/src/JavaBasics/ReverseString.java
@@ -1,20 +1,22 @@
 package JavaBasics;
 import java.util.Scanner;
 // Write a Java program to reverse a string.
-public class W37 {
-    public static void main(String[] args) {
-        W37 obj = new W37();    // Creating a class object to call the methods
+public class ReverseString {
+    public static void main(String[] args) {        // Main method
+        ReverseString obj = new ReverseString();    // Creating a class object to call the methods
         System.out.println("Enter a sentence: ");
         String sentence = obj.userInputs();     // Storing the inputs in a variable
-        obj.reverseString(sentence);    // Calling the reversed string method
+        obj.reverse(sentence);    // Calling the reversed string method
     }
+
     public String userInputs(){     // Method to get the user inputs
         Scanner in = new Scanner(System.in);    // Creating a scanner object
         return in.nextLine();   // returning the scanner
     }
-    public void reverseString(String data){     // Method to reverse each string
+
+    public void reverse(String data){     // Method to reverse each string
         char[] newArray = data.toCharArray();       // Converting the datatype from string to character array
-        System.out.println("Reversed string: ");
+        System.out.println("\nReversed string: ");
         for (int i = newArray.length - 1; i >= 0; i--) {    // Setting the pointer to the end of the string and iterating through the string
             System.out.print(newArray[i]);      // Printing each letters in a string in reversed order
         }
