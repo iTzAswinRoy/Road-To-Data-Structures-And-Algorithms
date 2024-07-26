86ab9721cb71709b10497d00a196d77803b83895
iTzAswinRoy <aswinroy255@gmail.com>
2024-07-26 10:15:38 +0530

    Arithmetic problem

--- Java/src/JavaBasics/CountTheFactors.java
+++ Java/src/JavaBasics/CountTheFactors.java
@@ -22,6 +22,6 @@ public class CountTheFactors {
                 count++;    // Incrementing the count by 1
             }
         }
-        System.out.println("Number of factors: "+count);    // Displaying the result
+        System.out.println("Number of factors: " + count);    // Displaying the result
     }
 }
