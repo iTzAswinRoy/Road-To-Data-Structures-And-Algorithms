4bd102fa22b7ec6ebf1ce6c157a8fa9abbe7a332
iTzAswinRoy <aswinroy255@gmail.com>
2024-07-26 10:15:47 +0530

    String

--- Java/src/JavaBasics/DisplayPassword.java
+++ Java/src/JavaBasics/DisplayPassword.java
@@ -5,14 +5,17 @@ public class DisplayPassword {
     public static void main(String[] args) {    // Main method
         DisplayPassword obj = new DisplayPassword();    // Creating an abject
         Console cons = obj.checkingConsole();   // Creating an abject from the console class
+
         if (cons == null) {     // Checking whether the console is available or not
             System.out.println("No console available");
             return;     // Exiting the condition
         }
+
         char[] pass_ward = cons.readPassword("Enter your Password: ");      // Storing the password in character array
         System.out.println("\nYour password is " + new String(pass_ward));      // Printing the password by converting to sting
         java.util.Arrays.fill(pass_ward, ' '); // Clear the password from memory
     }
+
     private Console checkingConsole() {     // Method to check if the console is available or not
         Console checkCons = System.console();       // Creating a Console object
         if (checkCons != null) {    // Condition for availability
