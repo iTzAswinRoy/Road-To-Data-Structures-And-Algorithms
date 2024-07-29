40ad0c872cbef66c6078b7c7d9803920009a69bb
iTzAswinRoy <aswinroy255@gmail.com>
2024-07-29 21:20:32 +0530

    Arrays

--- Java/src/JavaBasics/CheckingArrayElements.java
+++ Java/src/JavaBasics/CheckingArrayElements.java
@@ -1,6 +1,6 @@
 package JavaBasics;
+import java.util.Arrays;        // Importing arrays class
 import java.util.Objects;       // Importing object class
-import java.util.ArrayList;     // Importing array list class
 import java.util.Scanner;       // Importing scanner class
 // Write a Java program to test if the first and last element of two integer arrays are the same.
 // The array length must be greater than or equal to 2.
@@ -10,51 +10,48 @@ public class CheckingArrayElements {
     public static void main(String[] args) {    // Main method
         CheckingArrayElements obj = new CheckingArrayElements();    // Creating a class object
 
-        ArrayList<Integer> arr1 = obj.userInputs();     // Storing values in array 1 using the input method
-        ArrayList<Integer> arr2 = obj.userInputs();     // Storing values in array 2 using the input method
+        int[] arr1 = obj.userInputs();     // Storing values in array 1 using the input method
+        int[] arr2 = obj.userInputs();     // Storing values in array 2 using the input method
 
-        System.out.println("The given arrays are:");
-        obj.display(arr1);      // Displaying the array
-        obj.display(arr2);      // Displaying the array
+        System.out.println("\nThe given arrays are");     // Displaying the array
+        System.out.println("Array-1:\n" + Arrays.toString(arr1));
+        System.out.println("\nArray-2:\n" + Arrays.toString(arr2));
 
-        System.out.println("\nChecking if the fist and last element of both the array are equal.\n"+obj.checkArray(arr1, arr2));      // Calling the method to check the array
+        obj.checkArray(arr1, arr2);
     }
 
-    public ArrayList<Integer> userInputs() {     // Method to get user inputs
+    public int[] userInputs() {     // Method to get user inputs
         Scanner in = new Scanner(System.in);    // Creating a scanner object
         count++;    // Incrementing the count as we call the method
 
-        System.out.println("Enter the size for array " + count + ":");
+        System.out.println("Enter the size for array-" + count + ":");
         int size = in.nextInt();    // Getting the size from the user
+        int[] temp = new int[size];     // Creating an array to store inputs
 
-        ArrayList<Integer> temp = new ArrayList<>();
-        System.out.println("Enter the value's of array " + count + ":");
+        System.out.println("Enter the value of array-" + count + ":");
         for (int i = 0; i < size; i++) {    // Iterating over an array list
-            temp.add(in.nextInt());     // Assigning the values to an array list
+            temp[i] = in.nextInt();     // Assigning the values to an array list
         }
-
         return temp;     // Returning the inputs
     }
 
-    public void display(ArrayList<Integer> data){     // Method to display the array list
-        System.out.println(data);   // Printing the array list
-    }
+    public void checkArray(int[] arr1, int[] arr2) {     // Method to check if both the array contains same first and last elements
+        boolean isEquals = false;       // Assigning a boolean variable
 
-    public boolean checkArray(ArrayList<Integer> arr1, ArrayList<Integer> arr2) {     // Method to check if both the array contains same first and last elements
-        if (arr1.size() >= 2 && arr2.size() >= 2) {     // Setting a condition that the array length should be greater than 2
+        if (Objects.equals(arr1[0], arr2[0]) && Objects.equals(arr1[arr1.length-1], arr2[arr2.length-1])) {     // Checking if the elements are same
+            isEquals = true;    // Returns true if the conditions are met
 
-            for (int i = 0; i < arr1.size(); i++) {     // Iteration over array list 1
-                for (int j = 0; j < arr2.size(); j++) {     // Iteration over array list 2
+        } else if (arr1.length == 1 && arr2.length == 1) {      // Checking if the array contains 1 element
 
-                    if(Objects.equals(arr1.getFirst(), arr2.getFirst()) && Objects.equals(arr1.getLast(), arr2.getLast())){     // Applying the main condition over here
-                        return true;    // Returns true if the conditions are met
-                    }
-                }
+            if (Objects.equals(arr1[0], arr2[0])) {     // Checking if the elements are same in both the arrays
+                isEquals = true;    // Returns true if the conditions are met
             }
+
         } else {
-            System.out.println("Invalid array size");
+            System.out.println("\nInvalid array size!");
+            return;     // Exits the method if the size is invalid
         }
-        return false;   // Returns false if the conditions are not met
+        System.out.println("\nChecking if the fist and last element of both the array are equal.\n" + isEquals);      // Displaying the result
     }
 }
 
