c7976083cee290b14f6aff2bf71367af402b3074
iTzAswinRoy <aswinroy255@gmail.com>
2024-07-30 19:32:53 +0530

    (Pending)

--- Java/src/JavaBasics/w155.java
+++ Java/src/JavaBasics/w155.java
@@ -4,17 +4,38 @@ import java.util.Scanner;       // Importing scanner class
 public class w155 {
     public static void main(String[] args) {       // Main method
         w155 obj = new w155();      // Creating an object to call the methods
-        int[][] arr = {{10,20,33},
-                {40,50,60}};       // Assigning the values for a 2D array
+        int[][] arr = obj.userInputs();       // Assigning the values for a 2D array
 
-        obj.transposeArray(arr);       //
-        obj.printArray(obj.transposeArray(arr));    // Calling the method inside the method
+        System.out.println("\nGiven 3D array:");
+        obj.printArray(arr);
+
+        System.out.println("\nAfter transversing an array from 3D to 2D: ");
+        obj.printArray(obj.transposeArray(arr));    // Calling the method inside the method inorder to transverse the array
     }
+
     public int[][] userInputs(){
-        return
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int rows = in.nextInt();
+
+        System.out.println("Enter the number of columns:");
+        int column = in.nextInt();
+
+        int[][] matrix = new int[rows][column];
+        System.out.println("Enter the values of the 3D array:");
+
+        for (int i = 0; i < rows; i++) {
+
+            for (int j = 0; j < column; j++) {
+                matrix[i][j] = in.nextInt();
+            }
+        }
+        return matrix;
     }
+
     public int[][] transposeArray(int[][] data){     // Creating a method to transpose the array
         int[][] new_arr = new int[data[0].length][data.length];      // Creating a new array were the rows and columns are swapped
+
         for (int i = 0; i < data.length; i++) {       // Iterating through the rows
 
             for (int j = 0;j < data[0].length; j++){    // Iterating through the columns
@@ -26,11 +47,16 @@ public class w155 {
 
     public void printArray(int[][] data) {      // Creating a method to print the array
         for (int i = 0; i < data.length; i++) {     // Iterating through the rows
+            System.out.print("[");
 
             for (int j = 0; j < data[0].length; j++) {      // Iterating through the columns
-                System.out.print(data[i][j] + " ");      // Printing the values side by side
+                System.out.print(data[i][j]);      // Printing the values side by side
+
+                if(j < data[0].length - 1){
+                    System.out.print(", ");
+                }
             }
-            System.out.println();       // Printing the values one below the other
+            System.out.println("]");       // Printing the values one below the other
         }
     }
 }
