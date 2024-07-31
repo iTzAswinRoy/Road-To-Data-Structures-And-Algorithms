81407f9496708d2afc0e9304cdc680b1af1c4136
iTzAswinRoy <aswinroy255@gmail.com>
2024-07-31 22:11:41 +0530

    Recursion

---
+++ Java/src/JavaRecursion/FactorialCalculation.java
@@ -0,0 +1,18 @@
+package JavaRecursion;
+
+// Write a recursive function to calculate the factorial of a number n.
+public class FactorialCalculation {
+    public static void main(String[] args){
+        FactorialCalculation obj = new FactorialCalculation();
+
+        int result = obj.factorial(5);
+        System.out.println(result);
+    }
+
+    public int factorial(int num){
+        if(num == 1){
+            return num;
+        }
+       return factorial(num - 1) * num;
+    }
+}
