c57fdaa55ec15cb3c929ab3d6df03c3c71fe0f20
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-04 23:31:40 +0530

    Pending

--- Java/src/JavaBasics/w163.java
+++ Java/src/JavaBasics/w163.java
@@ -1,6 +1,6 @@
 package JavaBasics;
 import java.util.LinkedList; //
-import java.util.Scanner;
+import java.util.Scanner; //
 import java.util.Queue;
 // Write a Java program that will accept an integer and convert it into a binary representation.
 // Now count the number of bits equal to zero in this representation.
