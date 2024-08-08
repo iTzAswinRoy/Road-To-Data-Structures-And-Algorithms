43f361f033fb1acbdff4a9f838a5e8c1f9f9844e
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-08 23:48:39 +0530

    Updated Code

--- Java/src/JavaBasics/w163.java
+++ Java/src/JavaBasics/w163.java
@@ -1,26 +1,27 @@
 package JavaBasics;
 
-import java.util.LinkedList; //
-import java.util.Scanner; //
-import java.util.Queue;  //
+import java.util.LinkedList;    // Importing linked list class
+import java.util.Scanner;    // Importing scanner class
+import java.util.Queue;     // Importing queue class
 
 // Write a Java program that will accept an integer and convert it into a binary representation.
 // Now count the number of bits equal to zero in this representation.
 public class w163 {
     public static void main(String[] args) { //
+
         w163 obj = new w163();  // Creating an object
-        int number = obj.integer();  // Storing the integer value to a variable
+        int number = obj.userInputs();  // Storing the integer value to a variable
         Queue<Integer> binaryRepresentation = obj.convertToBinary(number);  // Storing the binary number to a queue interface
 
-        System.out.println("Input number: \n"+number);  // Displaying the input number
-        System.out.println("Binary representation of "+number+" is "+binaryRepresentation);
-        System.out.println("Number of zero bits: "+ obj.count(binaryRepresentation));   // Displaying the count of zero bits
+        System.out.println("Input number: \n" + number);  // Displaying the input number
+        System.out.println("Binary representation of " + number + " is " + binaryRepresentation);
+        System.out.println("Number of zero bits: " + obj.count(binaryRepresentation));   // Displaying the count of zero bits
     }
 
-    public int integer(){   // Creating a method to get values from the user
+    public int userInputs() {   // Creating a method to get values from the user
         Scanner in = new Scanner(System.in);    // Creating a scanner class
 
-        System.out.println("Enter the number: ");
+        System.out.println("Enter the number:");
         int num = in.nextInt();   // Storing the value to a variable
 
         in.close();   // Closing scanner to avoid data breach
@@ -28,22 +29,22 @@ public class w163 {
         return num;     // returning the input value
     }
 
-    public Queue<Integer> convertToBinary(int num){     // Creating a method to convert the number to binary
+    public Queue<Integer> convertToBinary(int num) {     // Creating a method to convert the number to binary
         Queue<Integer> binaryNumber = new LinkedList<>();  // Creating a queue interface
         int reminder = 0;
 
         if (num == 0){  // If the input value is zero then it returns 0
-            binaryNumber.add(0); //
+            binaryNumber.add(0);    // Add the number to the queue
 
-            return binaryNumber; //
+            return binaryNumber;    // Returning the binary number
         }
-        while(num > 0) {    // Here the conversion of binary number takes place
+        while (num > 0) {    // Here the conversion of binary number takes place
 
             reminder = num % 2;
             binaryNumber.add(reminder);
             num = num / 2;
         }
-        return binaryNumber;  //
+        return binaryNumber;    //
     }
 
     public int count(Queue<Integer> num){    // Creating a method to count the zero bits present in the binary number
