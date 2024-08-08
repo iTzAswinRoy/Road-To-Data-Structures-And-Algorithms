676d00ae79547ecab187abe16fe9fa47364bf226
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-08 23:48:45 +0530

    Updated Code

--- Java/src/JavaBasics/w165.java
+++ Java/src/JavaBasics/w165.java
@@ -1,11 +1,11 @@
 package JavaBasics;
 
-import java.util.Scanner;
-import java.util.Arrays;
+import java.util.Scanner;       // Importing scanner class
+import java.util.Arrays;        // Importing array class
 
 // Write a Java program to move every positive number to the right and every negative number to the left of a given array of integers.
 public class w165 {
-    public static void main(String[] args) {
+    public static void main(String[] args) {    // Main method
         w165 obj = new w165();     // Creating an object ot call the methods
 
         int[] array = obj.inputElements();  // Calling a method to get inputs for the array
@@ -14,16 +14,20 @@ public class w165 {
         obj.printArray(result);     // Printing the result
     }
 
-    public int[] inputElements(){   // Method to get the inputs
-        Scanner in = new Scanner(System.in);    // Creating a scanner object
+    public int[] inputElements(){       // Method to get the inputs
+        Scanner in = new Scanner(System.in);       // Creating a scanner object
+
         System.out.println("Enter the size of the array: ");
-        int[] data = new int[in.nextInt()];    // Getting the size of an array
+        int[] data = new int[in.nextInt()];     // Getting the size of an array
+
         System.out.println("Enter the values for the array: ");
         for (int i = 0; i < data.length; i++) {     // iterating over an array to assign values to an array
             data[i] = in.nextInt();     // Getting inputs using scanner object
         }
         System.out.println("Given array:\n "+Arrays.toString(data));
-        in.close();     // Closing scanner
+
+        in.close();         // Closing scanner
+
         return data;
     }
     public int[] rearrangeArray(int[] data){    // Method to rearrange the array
