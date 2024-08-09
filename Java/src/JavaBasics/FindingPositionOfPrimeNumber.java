5e4bbe7fbd9a6b8e39e60b4d7178003eb053a43b
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-09 23:50:53 +0530

    Arithmetic problem

--- Java/src/JavaBasics/FindingPositionOfPrimeNumber.java
+++ Java/src/JavaBasics/FindingPositionOfPrimeNumber.java
@@ -1,21 +1,28 @@
 package JavaBasics;
+
 import java.util.Scanner;   // Importing scanner class
+
 //  Write a Java program to calculate the position of a given prime number.
-public class W198 {
+public class FindingPositionOfPrimeNumber {
     public static void main(String[] args) {    // Main method
-        W198 obj = new W198();      // Creating a class object
+        FindingPositionOfPrimeNumber obj = new FindingPositionOfPrimeNumber();      // Creating a class object
+
         System.out.println("Enter a prime number:");
         int primeNumber = obj.userInputs();     // Calling the method to get the inputs from the user
+
         if (!obj.isPrime(primeNumber)) {    // Condition to check if the number is prime or not
-            System.out.println("It's not a prime number");
+            System.out.println("\nIt's not a prime number!");
         } else {
-            System.out.println("The position of the prime number is " + obj.findPrimePosition(primeNumber));    // Displaying the position of the prime number
+            System.out.println("\nThe position of the prime number is " + obj.findingPosition(primeNumber));    // Displaying the position of the prime number
         }
     }
+
     public int userInputs() {   // // Method to get the user inputs
         Scanner in = new Scanner(System.in);    // Creating a scanner object
+
         return in.nextInt();    // returning the scanner
     }
+
     public boolean isPrime(int value) {     // Method to check if the number is prime number
         if (value <= 1) {   // If the number is less than 1 then it's not a prime number
             return false;   // Breaks the loop
@@ -27,15 +34,19 @@ public class W198 {
             return false;   // Breaks the loop
         }
         for (int i = 3; i <= Math.sqrt(value); i += 2) {    // Checking if any given number is prime or not
+
             if (value % i == 0) {   // If the reminder is zero then it's not a prime number
                 return false;   // Breaks the loop
             }
         }
         return true;    // If true then it means it found the prime number
     }
-    public int findPrimePosition(int value) {   // Method to find the position of the prime number
+
+    public int findingPosition(int value) {   // Method to find the position of the prime number
         int count = 0;  // Initializing the count to 0
+
         for (int i = 2; i <= value; i++) {     // Iterating through each value till it reaches the given number, starting with the smallest prime number
+
             if (isPrime(i)) {   // Calling the prime method
                 count++;    // If the number is prime then the count will be incremented by 1
             }
