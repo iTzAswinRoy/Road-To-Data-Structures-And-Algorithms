f89572380cad380a49f0ef976cfa266be2b08436
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-09 23:34:22 +0530

    Updated Code

--- Java/src/JavaRecursion/String/RemoveAdjacentDuplicates.java
+++ Java/src/JavaRecursion/String/RemoveAdjacentDuplicates.java
@@ -4,30 +4,23 @@ import java.lang.StringBuilder;
 // Write a recursive function to remove all adjacent duplicate characters in a string.
 public class RemoveAdjacentDuplicates {
     public static void main(String[] args) {
-
         RemoveAdjacentDuplicates obj = new RemoveAdjacentDuplicates();
-//        StringBuilder text = new StringBuilder();
-//         text.append("aaxbbyccz");
 
         String text = "aaxbbyccz";
 
-        System.out.println("Given string:\n"+text);
-        System.out.println("\nThe duplicate characters in the string:\n" + obj.adjacentDuplicates(text, 0, new StringBuilder()));
+        System.out.println("Given string:\n" + text);
+        System.out.println("\nRemoving adjacent duplicates from the string:\n"+obj.adjacentDuplicates(text));
     }
-
-    public String adjacentDuplicates(String data, int index, StringBuilder duplicates, String result){
-        if(index >= data.length()){
-            return duplicates.toString();
-        }
-        int currentChar = data.charAt(index);
-
-        for (int i = 0; i < data.length()-1; i++) {
-            if(currentChar == data.charAt(i+1)){
-                continue;
-
-            }
+    public String adjacentDuplicates(String data){
+        if(data.length() <= 1){
+            return data;
         }
 
+        if(data.charAt(0) == data.charAt(1)){
+            int index = 1;
+            return adjacentDuplicates(data.substring(index+1));
+        }
+        return data.charAt(0) + " " + adjacentDuplicates(data.substring(1));
 
     }
 }
