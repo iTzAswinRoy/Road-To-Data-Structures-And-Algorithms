12b6996bbc9a4f72efb3343bbbcdcd19b907391e
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-11 11:07:30 +0530

    ArrayList

--- Java/src/JavaArrayList/WAL3.java
+++ Java/src/JavaArrayList/WAL3.java
@@ -1,39 +1,40 @@
 package JavaArrayList;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.ArrayList;     // Importing array list class
+import java.util.Scanner;       // Importing scanner class
 
 // Write a Java program to insert an element into the array list at the first position.
 public class WAL3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) {          // Main method
         WAL3 obj = new WAL3();    // Creating an object
         ArrayList<Integer> list = obj.userInputs();     //  Creating an array list
-        System.out.println("List containing name of colours:");
-        obj.display(list);      // Calling the method to display the array list
+
+        System.out.println("List containing name of colours:" + list);     // Displaying the arraylist
+
         obj.insertElementAtFirst(list);
-        System.out.println("List after adding the element:");
-        obj.display(list);
+        System.out.println("\nList after adding the element:" + list);
+
     }
+
     public ArrayList<Integer> userInputs(){
         Scanner in = new Scanner(System.in);
+
         System.out.println("Enter the size of the array list:");
         int size = in.nextInt();
+
         ArrayList<Integer> temp = new ArrayList<Integer>(size);
         System.out.println("Enter the value:");
+
         for (int i = 0; i < size; i++) {
             temp.add(in.nextInt());
         }
         return temp;
     }
+
     public void insertElementAtFirst(ArrayList<Integer> temp){
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter the element to add at the first:");
+
+        System.out.println("\nEnter the element to add at the first:");
         temp.add(0,in.nextInt());
-    }
-    public void display(ArrayList<Integer> temp){
-        for (int i = 0; i < temp.size(); i++) {
-            System.out.print(temp.get(i)+" ");
-        }
-        System.out.println();
     }
 }
