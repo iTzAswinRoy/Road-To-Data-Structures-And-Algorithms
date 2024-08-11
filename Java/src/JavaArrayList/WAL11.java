6f3cfa0df9256165896d9b2624786412dc60d053
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-11 11:08:48 +0530

    ArrayList

--- Java/src/JavaArrayList/WAL11.java
+++ Java/src/JavaArrayList/WAL11.java
@@ -1,35 +1,46 @@
 package JavaArrayList;
+
 import java.util.ArrayList;
 import java.util.Scanner;
+
 // Write a Java program to reverse elements in an array list
 public class WAL11 {
     public static void main(String[] args) {    // Main method
         WAL11 obj = new WAL11();    // Creating an object
         ArrayList<Integer> list = obj.userInputs();     //  Creating an array list
+
         System.out.println("Given list:");
         obj.display(list);      // Calling the method to display the array list
 
         System.out.println("Reversed list:");
         obj.reverseList(list);     // Calling the method to reverse the arraylist
     }
+
     public ArrayList<Integer> userInputs(){     // Method to get inputs from the user
         Scanner in = new Scanner(System.in);
+
         System.out.println("Enter the size of the array list:");
         int size = in.nextInt();
+
         ArrayList<Integer> temp = new ArrayList<Integer>(size);
         System.out.println("Enter the values in the list:");
+
         for (int i = 0; i < size; i++) {
             temp.add(in.nextInt());
         }
         in.close();
+
         return temp;
     }
+
     public void reverseList(ArrayList<Integer> list){
         for (int i = list.size()-1; i >= 0 ; i--) {
+
             list.add(list.set(i, list.get(i)));
             System.out.print(list.get(i)+" ");
         }
     }
+
     public void display(ArrayList<Integer> list) {
         for (int i = 0; i < list.size(); i++) {
             System.out.print(list.get(i)+" ");
