b052f3328c6c921216b7d9cf6103811a9de3fbdc
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-11 11:08:42 +0530

    ArrayList

--- Java/src/JavaArrayList/WAL10.java
+++ Java/src/JavaArrayList/WAL10.java
@@ -18,27 +18,36 @@ public class WAL10 {
     }
     public ArrayList<Integer> userInputs(){
         Scanner in = new Scanner(System.in);
+
         System.out.println("Enter the size of the list:");
         int size = in.nextInt();
+
         ArrayList<Integer> tempList = new ArrayList<>();
         System.out.println("Enter the values to add in the list:");
+
         for (int i = 0; i < size; i++) {
             tempList.add(in.nextInt());
         }
         System.out.println();
         in.close();
+
         return tempList;
     }
+
     public ArrayList<Integer> shuffle(ArrayList<Integer> list){
         Random rnd = new Random();
+
         for (int i = 0; i < list.size(); i++) {
+
             int randomValue = rnd.nextInt(i+1); // The reason for (i + 1) is the nextInt range cant be zero, so it should start with a value
             int temp = list.get(randomValue);
+
             list.set(randomValue, i);
             list.set(i, temp);
         }
         return list;
     }
+
     public void display(ArrayList<Integer> list){
         for (int i = 0; i < list.size(); i++) {
             System.out.print(list.get(i)+" ");
