e88b50cdfc9c39cde4a57982b6ad38d481d447f4
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-12 23:45:46 +0530

    ArrayList

--- Java/src/JavaArrayList/InsertElementAtFirst.java
+++ Java/src/JavaArrayList/InsertElementAtFirst.java
@@ -4,25 +4,24 @@ import java.util.ArrayList;     // Importing array list class
 import java.util.Scanner;       // Importing scanner class
 
 // Write a Java program to insert an element into the array list at the first position.
-public class WAL3 {
+public class InsertElementAtFirst {
     public static void main(String[] args) {          // Main method
-        WAL3 obj = new WAL3();    // Creating an object
+        InsertElementAtFirst obj = new InsertElementAtFirst();    // Creating an object
         ArrayList<Integer> list = obj.userInputs();     //  Creating an array list
 
-        System.out.println("List containing name of colours:" + list);     // Displaying the arraylist
+        System.out.println("Given array list:\n" + list);     // Displaying the arraylist
 
-        obj.insertElementAtFirst(list);      // Calling the method to insert the element at first
-        System.out.println("\nList after adding the element:" + list);      // Displaying the result
-
+        obj.insertFirst(list);      // Calling the method to insert the element at first
+        System.out.println("\nList after adding the element at first:\n" + list);      // Displaying the result
     }
 
-    public ArrayList<Integer> userInputs(){     // Method to get inputs from the user
+    public ArrayList<Integer> userInputs() {     // Method to get inputs from the user
         Scanner in = new Scanner(System.in);        // Creating a scanner object
 
         System.out.println("Enter the size of the array list:");
         int size = in.nextInt();        // Getting the size from the user
 
-        ArrayList<Integer> temp = new ArrayList<Integer>(size);     // Creating a temporary array list
+        ArrayList<Integer> temp = new ArrayList<>(size);     // Creating a temporary array list
         System.out.println("Enter the value:");
 
         for (int i = 0; i < size; i++) {        // Iterating over the array list
@@ -31,10 +30,10 @@ public class WAL3 {
         return temp;        // Returning the array list
     }
 
-    public void insertElementAtFirst(ArrayList<Integer> temp){      // Method the insert element at firts
+    public void insertFirst(ArrayList<Integer> temp) {      // Method the insert element at firts
         Scanner in = new Scanner(System.in);        // Creating a scanner object
 
-        System.out.println("\nEnter the element to add at the first:");
+        System.out.println("\nEnter the element to add at first:");
         temp.add(0,in.nextInt());       // Adding element at first
     }
 }
