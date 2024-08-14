ec2defa24757c85ab6a4992ee0a64f90912753c3
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-14 23:32:51 +0530

    Updated Code

--- Java/src/JavaArrayList/WAL14.java
+++ Java/src/JavaArrayList/WAL14.java
@@ -10,31 +10,43 @@ public class WAL14 {
 
         ArrayList<Integer> list = obj.userInputs();     //  Creating an array list
         System.out.println("The given array list are");
+
         obj.display(list);
         obj.swapList(list);
+    }
 
-    }
-    public ArrayList<Integer> userInputs(){     // Method to get inputs from the user
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the size of the array list:");
-        int size = in.nextInt();
-        ArrayList<Integer> temp = new ArrayList<Integer>(size);
-        System.out.println("Enter the values in the list:");
-        for (int i = 0; i < size; i++) {
-            temp.add(in.nextInt());
+    public ArrayList<Integer> userInputs() {        // Method to get inputs from the user
+        Scanner in = new Scanner(System.in);      // Creating a scanner object
+
+        System.out.println("Enter the size:");
+        int size = in.nextInt();        // Getting the size from the user
+
+        ArrayList<Integer> tempList = new ArrayList<>();        // Creating a temporary array list
+        System.out.println("Enter the values:");
+
+        for (int i = 0; i < size; i++) {        // Iterating over the array list
+            tempList.add(in.nextInt());       // Adding elements for each iteration
         }
-        return temp;
+        in.close();         // Closing scanner for avoid excess memory storage
+
+        return tempList;        // Returning the array list
     }
+
     public void swapList(ArrayList<Integer> list){
         Scanner in = new Scanner(System.in);
+
         System.out.println("\nEnter the elements to be swapped");
         System.out.println("Element-1:");
         int e1 = in.nextInt();
+
         System.out.println("Element-2:");
         int e2 = in.nextInt();
+
         int index1 = -1;
         int index2 = -1;
+
         for (int i = 0; i < list.size(); i++) {
+
             if (list.get(i) == e1 && index1 == -1){
                 index1 = i;
             }
@@ -44,6 +56,7 @@ public class WAL14 {
         }
         if(index1 != -1 && index2 != -1) {
             System.out.println("\nElements after swapping:");
+
             int temp = list.get(index1);
             list.set(index1, list.get(index2));
             list.set(index2, temp);
