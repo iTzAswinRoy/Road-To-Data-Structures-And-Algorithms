8dbdacc1a7d9b96d5efdb30cc81c9731d43c0c20
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-14 23:31:28 +0530

    Updated Code

--- Java/src/JavaArrayList/WAL13.java
+++ Java/src/JavaArrayList/WAL13.java
@@ -18,26 +18,34 @@ public class WAL13 {
 
         System.out.println(obj.compareList(list1, list2));
     }
+
     public ArrayList<Integer> userInputs(){     // Method to get inputs from the user
         Scanner in = new Scanner(System.in);
+
         System.out.println("Enter the size of the array list:");
         int size = in.nextInt();
-        ArrayList<Integer> temp = new ArrayList<Integer>(size);
+
+        ArrayList<Integer> temp = new ArrayList<>(size);
         System.out.println("Enter the values in the list:");
+
         for (int i = 0; i < size; i++) {
             temp.add(in.nextInt());
         }
         return temp;
     }
+
     public boolean compareList(ArrayList<Integer> list1, ArrayList<Integer> list2){
         System.out.println("\nChecking whether the elements in the list are same:");
+
         if(list1.equals(list2)){
             return true;
+
         } else if(list1.size() != list2.size()){
             System.out.print("The size of the lists are different. ");
         }
         return false;
     }
+
     public void display(ArrayList<Integer> list) {
         for (int i = 0; i < list.size(); i++) {
             System.out.print(list.get(i) + " ");
