95f958c6d1a11775f54edcad5658212bad3a34dc
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-14 23:13:30 +0530

    ArrayList

--- Java/src/JavaArrayList/ShuffleElements.java
+++ Java/src/JavaArrayList/ShuffleElements.java
@@ -5,25 +5,26 @@ import java.util.Random;        // Importing random class
 import java.util.Scanner;       // Importing scanner class
 
 // Write a Java program to shuffle elements in an array list.
-public class WAL10 {
+public class ShuffleElements {
     public static void main(String[] args) {        // Main method
-        WAL10 obj = new WAL10();        //Creating an object
-        ArrayList<Integer> list = obj.userInputs();         // Creating a empty list to stroe inputs
+        ShuffleElements obj = new ShuffleElements();        //Creating an object
+        ArrayList<Integer> list = obj.userInputs();         // Creating a empty list to store inputs
 
-        System.out.println("Given array list:\n" + list);
+        System.out.println("Given array list:\n" + list);       // Displaying the list
 
-        System.out.println("Array list after being shuffled:");
-        obj.display(obj.shuffle(list));
+        ArrayList<Integer> shuffledList = obj.shuffle(list);        // Storing the rust in new list
+
+        System.out.println("\nArray list after being shuffled:\n" + shuffledList);      // Displaying the shuffled list
     }
 
     public ArrayList<Integer> userInputs() {        // Method to get inputs from the user
         Scanner in = new Scanner(System.in);      // Creating a scanner object
 
-        System.out.println("Enter the size of the list:");
+        System.out.println("Enter the size:");
         int size = in.nextInt();        // Getting the size from the user
 
         ArrayList<Integer> tempList = new ArrayList<>();        // Creating a temporary array list
-        System.out.println("Enter the values to add in the list:");
+        System.out.println("Enter the values:");
 
         for (int i = 0; i < size; i++) {        // Iterating over the array list
             tempList.add(in.nextInt());       // Adding elements for each iteration
@@ -37,20 +38,13 @@ public class WAL10 {
         Random rnd = new Random();      // Creating an object from random class
 
         for (int i = 0; i < list.size(); i++) {     // Iterating over the array list
-
             int randomValue = rnd.nextInt(i+1);     // The reason for (i + 1) is the nextInt range can't be zero, so it should start with a value
-            int temp = list.get(randomValue);       // Strong the random value from the lsit
 
-            list.set(randomValue, i);
-            list.set(i, temp);
+            // Shuffling by swapping the elements at index i and randomValue
+            int temp = list.get(i);
+            list.set(i, list.get(randomValue));
+            list.set(randomValue, temp);
         }
-        return list;
-    }
-
-    public void display(ArrayList<Integer> list){
-        for (int i = 0; i < list.size(); i++) {
-            System.out.print(list.get(i)+" ");
-        }
-        System.out.println("\n");
+        return list;        // Returning the swapped list
     }
 }
