e51401e8196c8092180f54199939b07e82ecfc16
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-16 16:17:13 +0530

    ArrayList

--- Java/src/JavaArrayList/ExtendingList.java
+++ Java/src/JavaArrayList/ExtendingList.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;         // Importing array list class
 import java.util.Scanner;          // Importing scanner class
 
 // Write a Java program to increase an array list size.
-public class WAL20 {
-    public static void main(String[] args) {    // Main method
-        WAL20 obj = new WAL20();    // Creating an object
+public class ExtendingList {
+    public static void main(String[] args) {        // Main method
+        ExtendingList obj = new ExtendingList();        // Creating an object
 
         ArrayList<Integer> list = obj.userInputs();     //  Creating an array list
-        System.out.println("The given array list:\n"+list);
+        System.out.println("The given array list:\n" + list);     // Displaying the list
 
-        obj.increaseSize(list);
+        obj.increaseSize(list);         // Calling the method to increase the size of the list
     }
 
     public ArrayList<Integer> userInputs() {        // Method to get inputs from the user
@@ -29,16 +29,16 @@ public class WAL20 {
         return tempList;        // Returning the array list
     }
 
-    public void increaseSize(ArrayList<Integer> list){
-        Scanner in = new Scanner(System.in);
+    public void increaseSize(ArrayList<Integer> list) {          // Method to increase the size of the list
+        Scanner in = new Scanner(System.in);        // Creating a scanner object
 
-        System.out.println("Enter the size to be increased:");
-        int extraSize = in.nextInt();
+        System.out.println("\nEnter the size to be increased:");
+        int extraSize = in.nextInt();       // Getting the size from the user
 
         System.out.println("Add new elements to the list:");
-        for (int i = 0; i < extraSize; i++) {
-            list.add(in.nextInt());
+        for (int i = 0; i < extraSize; i++) {        // Iterating over the array list
+            list.add(in.nextInt());         // Getting the size from the user for addition size of the list
         }
-        System.out.println("List after the size being added:\n"+list);
+        System.out.println("\nList after the size being added:\n" + list);        // Displaying the list with increased size
     }
 }
