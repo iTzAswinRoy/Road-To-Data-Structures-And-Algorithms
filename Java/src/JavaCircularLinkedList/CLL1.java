518434e65e53eb80b68b0e16f9294f6a731d8e4f
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-19 23:20:42 +0530

    Updated Code

--- Java/src/JavaCircularLinkedList/CLL1.java
+++ Java/src/JavaCircularLinkedList/CLL1.java
@@ -1,52 +1,54 @@
 package JavaCircularLinkedList;
 // Given a circular linked list, determine if it contains a cycle and return the starting node of the cycle.
-public class CLL1 {     // Circular linked list class
-    Node head;
-    Node tail;
-    int size;
+public class CLL1 {     // Creating a circular linked list class
+    int size;       // Creating size variable
+    Node head;      // Creating head pointer
+    Node tail;      // Creating tail pointer
 
-    CLL1(){
-        this.size = 0;
+    CLL1() {     // Creating a default constructor
+        this.size = 0;      // Initializing the size to 0
     }
 
-    public void insertNode(int value){      //
-        Node newNode = new Node(value);     //
+    public void insertNode(int value) {      // Method to insert node
+        Node newNode = new Node(value);     // Creating a node by passing the value
 
-        if(tail == null){       //
-            head = newNode;
-            tail = head;
-            tail.next = head;
+        // Checking if the tail is null. If it's null, then the list contains only one node
+        if (tail == null) {
+            head = newNode;     // Assigning value to head
+            tail = head;        // Assigning tail is head
+            tail.next = head;       // Referencing tail node pointing towards head in order to perform circular linked list
 
         } else {
-            tail.next = newNode;
-            tail = newNode;
-            tail.next = head;
+            // If the tail is not null, then there's more than one node present in the linked list
+            tail.next = newNode;       // Referencing the tail node pointing towards the new node
+            tail = newNode;        // Assigning new node as tail
+            tail.next = head;       // now the tail is pointing towards head
         }
-        size++;
+        size++;     // Incrementing size by 1
     }
 
-    public int firstNode(){
-        return head.value;
+    public int firstNode() {        // Method to get the first node in the circular linked list
+        return head.value;      // Returning the head node
     }
 
-    public void display(){
-        Node temp = head;
+    public void display() {     // Method to display the circular linked list
+        Node temp = head;       // Creating a temp node pointing at head
 
-        do {
-            System.out.print(temp.value + " -> ");
-            temp = temp.next;
+        do {       // Using do while loop in order to print node starting from head
+            System.out.print(temp.value + " -> ");      // Displaying the node's value
+            temp = temp.next;       // Moving the pointer to the next node
 
-        } while (temp != head);
+        } while (temp != head);     // Condition keeps going until the pointer reaches th head node
 
-        System.out.println("HEAD");
+        System.out.println("HEAD");     // Displaying 'head' for reference
     }
 
-    class Node {
-        int value;
-        Node next;
+    class Node {        // Creating a node class
+        int value;      // Creating a variable contains a value of the node
+        Node next;      // Creating a reference pointer for the node
 
-        Node(int value){
-            this.value = value;
+        Node(int value) {       // Creating a parameterized constructor by passing the value
+            this.value = value;     // Initializing the given value to the current value
         }
     }
 
