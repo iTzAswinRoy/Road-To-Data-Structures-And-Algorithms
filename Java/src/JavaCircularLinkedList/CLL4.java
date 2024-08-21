8635ffdff200dfec886bbeaa991f9d1ab83bb14b
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-21 22:12:15 +0530

    Circular Linked List

--- Java/src/JavaCircularLinkedList/CLL4.java
+++ Java/src/JavaCircularLinkedList/CLL4.java
@@ -1,68 +1,82 @@
 package JavaCircularLinkedList;
+
 // Write a function to insert a node into a circular linked list at a specified position.
-public class CLL4 {     //
-    Node head;
-    Node tail;
-    int size;
+public class CLL4 {       // Circular linked list class
+    int size;       // Creating instance variable for size
+    Node head;      // Creating head pointer
+    Node tail;      // Creating tail pointer
 
-    CLL4() {      //
-        this.size = 0;
+    CLL4() {      // Creating a default constructor
+        this.size = 0;      // Initializing the size to 0
     }
 
-    public void insertNode(int value) {     //
-        Node newNode = new Node(value);
+    public void insertNode(int value) {     // Method to insert node in the circular linked list
+        Node newNode = new Node(value);     // Creating a node by passing the value
+
+        // Checking if the tail is null. If it's null, then the list contains only one node
         if (tail == null) {
-            head = newNode;
-            tail = head;
-            tail.next = head;
+            head = newNode;     // Assigning value to head
+            tail = head;        // Assigning tail as head
+            tail.next = head;       // Setting the tail pointing towards head
+
         } else {
-            tail.next = newNode;
-            tail = newNode;
-            tail.next = head;
+            // If the tail is not null, then there's more than one node present in the linked list
+            tail.next = newNode;        // Setting the tail pointer to the new node
+            tail = newNode;         // Assigning tail as new node
+            tail.next = head;       // Setting the tail pointing towards head
         }
-        size++;
+        size++;     // Incrementing size by 1
     }
-    public void nodeAtSpecificPosition(int index, int value) {      //
-        Node newNode = new Node(value);
-        System.out.println("\nInserting node "+value+" at index "+index);
-        if(index > size){
-            System.out.println("\nIndex out of bound!");
-            return;
+
+    public void nodeAtSpecificPosition(int index, int value) {      // Method to insert a node at a specific position
+        Node newNode = new Node(value);         // Creating a node by passing the value
+
+        System.out.println("\nInserting node " + value + " at index " + index);
+        if(index > size) {      // Checking if the index is greater than the size
+            System.out.println("\nIndex out of bound!");        // Displays error message
+            return;     // Exits the method
+
         } else{
-            Node prev = tail;
-            Node curr = head;
-            for (int i = 0; i < index; i++) {
+            Node prev = tail;       // Creating 'previous' pointer, pointing at tail
+            Node curr = head;       // Creating 'current' pointer, pointing at head
+
+            for (int i = 0; i < index; i++) {       // Iterating till the specific index
+                // For each iterating, we're updating the current and previous point to its next node
                 prev = curr;
                 curr = curr.next;
             }
-            prev.next = newNode;
-            newNode.next = curr;
+            // Once the 'current' pointer reached at the specified index, we're now updating the 'previous' and 'current' pointers
+            prev.next = newNode;        // The 'previous' pointer is pointing towards the new node
+            newNode.next = curr;        // The new node is pointing toward the current node
         }
-        size++;
-    }
-    public void display() {     //
-        Node temp = head;
-        do {
-            System.out.print(temp.value + " -> ");
-            temp = temp.next;
-
-        } while (temp != head);
-
-        System.out.println("HEAD");
+        size++;       // Incrementing size by 1
     }
 
-    class Node {      //
-        int value;
-        Node next;
+    public void display() {       // Method to display the circular linked list
+        Node temp = head;       // Creating a temp node pointing at head
 
-        Node(int value) {       //
-            this.value = value;
+        do {        // Using do while loop in order to print node starting from head
+            System.out.print(temp.value + " -> ");      // Displaying the value of each node
+            temp = temp.next;       // Moving the pointer to the next node
+
+        } while (temp != head);      // Condition keeps going until the pointer reaches th head node
+
+        System.out.println("HEAD");       // Displaying 'head' for reference
+    }
+
+    class Node {      // Creating a node class
+        int value;      // Creating an instance variable to store a value for the node
+        Node next;      // Creating a reference, pointing towards the next node
+
+        Node(int value) {       // Creating a parameter constructor by passing the value
+            this.value = value;     // Assigning the parameter value to the instance variable
         }
     }
 
-    public static void main(String[] args) {       //
-        CLL4 circularLinkedList = new CLL4();
+    public static void main(String[] args) {       // Main method
+        CLL4 circularLinkedList = new CLL4();       // Creating an object of class type
 
+        // Inserting node to the circular linked list
         circularLinkedList.insertNode(1);
         circularLinkedList.insertNode(2);
         circularLinkedList.insertNode(3);
@@ -70,11 +84,11 @@ public class CLL4 {     //
         circularLinkedList.insertNode(5);
 
         System.out.println("Original circular linked list:");
-        circularLinkedList.display();       //
+        circularLinkedList.display();       // Displaying the circular linked list
 
-        circularLinkedList.nodeAtSpecificPosition(2,7);     //
+        circularLinkedList.nodeAtSpecificPosition(2,7);     // Calling the method to insert node at specific index
 
         System.out.println("\nCircular linked list after inserting the node:");
-        circularLinkedList.display();       //
+        circularLinkedList.display();       // Displaying the updated circular linked list
     }
 }
