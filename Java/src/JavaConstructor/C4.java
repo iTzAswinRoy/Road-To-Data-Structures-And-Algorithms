f5a9f8f2a07776e64fe50b19442f3df0f1001e3c
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-22 16:01:27 +0530

    Updated Code

--- Java/src/JavaConstructor/C4.java
+++ Java/src/JavaConstructor/C4.java
@@ -5,18 +5,29 @@ public class C4 {
     private String name;
     private String grade;
 
-    C4 (){
-        this.name = "Aswin";
-        this.grade = "6th";
+    C4 (String name, String grade){
+        this.name = name;
+        this.grade = grade;
     }
 
     C4 (C4 obj){
         this.name = obj.name;
-
         this.grade = obj.grade;
     }
 
-    public static void main(String[] args) {
+    public String getName() {
+        return name;
+    }
 
+    public String getGrade() {
+        return grade;
+    }
+
+    public static void main(String[] args) {
+        C4 obj1 = new C4("Aswin", "12th");
+        C4 obj2 = new C4(obj1);
+
+        System.out.println("Name:"+obj2.getName());
+        System.out.println("Grade:"+obj2.getGrade());
     }
 }
