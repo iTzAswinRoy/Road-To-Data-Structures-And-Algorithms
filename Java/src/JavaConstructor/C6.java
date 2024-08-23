a76067cc487e4de02d228ee0df12045a138b9c73
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-23 22:05:20 +0530

    Updated Code

--- Java/src/JavaConstructor/C6.java
+++ Java/src/JavaConstructor/C6.java
@@ -16,18 +16,25 @@ public class C6 {
         players.add(name);
     }
 
-    public void removePlayer(int index){
-        if(index > players.size()){
-            System.out.println("Index out of bound");
-            return;
-        }
-        players.remove(index);
+    public void removeFirst(){
+        players.removeFirst();
+    }
+    public List<String> getPlayers() {
+        return players;
     }
 
     public static void main(String[] args) {
         String[] team = {"Aswin", "Melwin", "Joel"};
         C6 obj = new C6(team);
 
+        System.out.println("Team list\n"+ obj.getPlayers());
 
+        System.out.println("\nAdding players");
+        obj.addPlayer("Davis");
+        System.out.println(obj.getPlayers());
+
+        System.out.println("\nRemoving players");
+        obj.removeFirst();
+        System.out.println(obj.players);
     }
 }
