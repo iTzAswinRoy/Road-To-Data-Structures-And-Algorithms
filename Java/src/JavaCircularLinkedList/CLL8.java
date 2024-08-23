526b62538e52056537b9787fda1b70617da17232
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-23 23:18:13 +0530

    Updated Code

--- Java/src/JavaCircularLinkedList/CLL8.java
+++ Java/src/JavaCircularLinkedList/CLL8.java
@@ -1,29 +1,32 @@
 package JavaCircularLinkedList;
 // Write a function to reverse a circular linked list.
-public class CLL8 {
-    Node head;
-    Node tail;
-    int size;
+public class CLL8 {         // Circular linked list class
+    int size;       // Creating instance variable for size
+    Node head;      // Creating head pointer
+    Node tail;      // Creating tail pointer
 
-    CLL8(){
-        this.size = 0;
+    CLL8() {     // Creating a default constructor
+        this.size = 0;      // Initializing the size to 0
     }
 
-    public void insertNode(int value){      //
-        Node newNode = new Node(value);
-        if(tail ==  null){
-            head = newNode;
-            tail = head;
-            tail.next = head;
+    public void insertNode(int value) {      // Method to insert node in the circular linked list
+        Node newNode = new Node(value);     // Creating a node by passing the value
+
+        // Checking if the tail is null. If it's null, then the list contains only one node
+        if (tail ==  null) {
+            head = newNode;     // Assigning value to head
+            tail = head;        // Assigning tail as head
+
         } else {
-            tail.next = newNode;
-            tail = newNode;
-            tail.next = head;
+            // If the tail is not null, then there's more than one node present in the linked list
+            tail.next = newNode;        // Setting the tail pointer to the new node
+            tail = newNode;      // Assigning tail as new node
         }
-        size++;
+        tail.next = head;       // Setting the tail pointing towards head
+        size++;        // Incrementing size by 1
     }
 
-    public void reverse(){      //
+    public void reverse() {      //
         if(head == null || head.next == head){
             return;
         }
@@ -42,38 +45,43 @@ public class CLL8 {
         tail = originalHead;
     }
 
-    public void display(){      //
-        Node temp = head;
-        do{
-            System.out.print(temp.value+" -> ");
-            temp = temp.next;
+    public void display() {      // Method to display the circular linked list
+        Node temp = head;       // Creating a 'temp' node pointing at head
 
-        } while (temp != head);
-        System.out.println("HEAD");
+        do {         // Using do while loop in order to print node starting from head
+            System.out.print(temp.value + " -> ");        // Displaying the value of each node
+            temp = temp.next;       // Moving the pointer to the next node
+
+        } while (temp != head);     // Condition keeps going until the pointer reaches the tail node
+
+        System.out.println("HEAD");         // Displaying 'head' for reference
     }
 
-    class Node {    //
-        int value;
-        Node next;
+    class Node {        // Creating a node class
+        int value;      // Creating an instance variable to store a value for the node
+        Node next;      // Creating a reference, pointing towards the next node
 
-        Node(int value) {
-            this.value = value;
+        Node(int value) {       // Creating a parameter constructor by passing the value
+            this.value = value;        // Assigning the parameter value to the instance variable
         }
     }
 
-     public static void main(String[] args) {       //
-        CLL8 circularLinkedList = new CLL8();       //
+     public static void main(String[] args) {       // Main method
+        CLL8 circularLinkedList = new CLL8();      // Creating an object of class type
+
+        // Inserting node to the circular linked list
         circularLinkedList.insertNode(1);
         circularLinkedList.insertNode(2);
         circularLinkedList.insertNode(3);
         circularLinkedList.insertNode(4);
         circularLinkedList.insertNode(5);
 
+        // Displaying the circular linked list
         System.out.println("Original circular linked list:");
         circularLinkedList.display();
 
         System.out.println("\nReversed circular linked list:");
-        circularLinkedList.reverse();
-        circularLinkedList.display();
+        circularLinkedList.reverse();       // Calling the method to reverse the circular linked list
+        circularLinkedList.display();       // Displaying the reversed circular linked list
     }
 }
