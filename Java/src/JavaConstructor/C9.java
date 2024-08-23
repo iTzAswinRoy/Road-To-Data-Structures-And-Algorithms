9a3f604d47b93c92137fbc4c3e5145a08497712e
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-23 22:05:42 +0530

    Updated Code

---
+++ Java/src/JavaConstructor/C9.java
@@ -0,0 +1,47 @@
+package JavaConstructor;
+
+// Create a base class Animal with a constructor that initializes species.
+// Then, create a derived class Dog that calls the base class constructor.
+ class Animal {
+     private String species;
+
+     Animal(String species){
+         this.species = species;
+
+     }
+
+     public void displaySpecies() {
+         System.out.println("Animal class species is " + species);
+     }
+}
+
+class Dog extends Animal {
+     private String breed;
+
+     Dog(String breed) {
+          this(breed, "unknown");
+     }
+
+     Dog(String breed, String species){
+         super(species);
+         this.breed = breed;
+     }
+     public void displayBreed(){
+         System.out.println("Dog breed is " + breed);
+     }
+}
+
+public class C9 {
+    public static void main(String[] args) {
+        Dog myDog = new Dog("Pit bull", "Dog");
+        Dog myDog2 = new Dog("SHitzuh");
+
+
+        myDog.displaySpecies();
+        myDog.displayBreed();
+
+        System.out.println();
+        myDog2.displaySpecies();
+        myDog2.displayBreed();
+    }
+}
