86cbaac39cd1f997d994d2841fbbcd3a25ee368b
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-24 20:18:06 +0530

    Updated Code

--- Java/src/JavaCircularLinkedList/CLL9.java
+++ Java/src/JavaCircularLinkedList/CLL9.java
@@ -5,22 +5,24 @@ public class CLL9 {       // Circular linked list class
     Node head;      // Creating head pointer
     Node tail;      // Creating tail pointer
 
-    CLL9(){     // Creating a default constructor
+    CLL9() {     // Creating a default constructor
         this.size = 0;      // Initializing the size to 0
     }
 
-    public void insertNode(int value){      //
-        Node newNode = new Node(value);     //
-        if(tail == null){
-            head = newNode;
-            tail = head;
-            tail.next = head;
+    public void insertNode(int value) {      // Method to insert node in the circular linked list
+        Node newNode = new Node(value);     // Creating a node by passing the value
+
+        // Checking if the tail is null. If it's null, then the list contains only one node
+        if (tail == null) {
+            head = newNode;     // Assigning value to head
+            tail = head;        // Assigning tail as head
         } else {
-            tail.next = newNode;
-            tail = newNode;
-            tail.next = head;
+            // If the tail is not null, then there's more than one node present in the linked list
+            tail.next = newNode;        // Setting the tail pointer to the new node
+            tail = newNode;      // Assigning tail as new node
         }
-        size++;
+        tail.next = head;       // Setting the tail pointing towards head
+        size++;         // Incrementing size by 1
     }
 
     public void findKthFromEnd(int k){      //
