3cc311725b697fd08de43ffea373a44b2f0bde92
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-28 23:24:51 +0530

    Updated Code

--- Java/src/JavaBasics/w163.java
+++ Java/src/JavaBasics/w163.java
@@ -1,62 +1,68 @@
 package JavaBasics;
 
-import java.util.LinkedList;    // Importing linked list class
 import java.util.Scanner;    // Importing scanner class
-import java.util.Queue;     // Importing queue class
+import java.util.Stack;     // Importing stack class
 
 // Write a Java program that will accept an integer and convert it into a binary representation.
 // Now count the number of bits equal to zero in this representation.
 public class w163 {
-    public static void main(String[] args) { //
+    public static void main(String[] args) {      // Main method
+        w163 obj = new w163();      // Creating an object
 
-        w163 obj = new w163();  // Creating an object
-        int number = obj.userInputs();  // Storing the integer value to a variable
-        Queue<Integer> binaryRepresentation = obj.convertToBinary(number);  // Storing the binary number to a queue interface
+        int number = obj.userInputs();      // Storing the integer value to a variable
+        Stack<Integer> binaryRepresentation = obj.convertToBinary(number);       // Storing the binary number to a queue interface
 
-        System.out.println("Input number: \n" + number);  // Displaying the input number
-        System.out.println("Binary representation of " + number + " is " + binaryRepresentation);
-        System.out.println("Number of zero bits: " + obj.count(binaryRepresentation));   // Displaying the count of zero bits
+        // Displaying the binary representation of the number
+        System.out.println("Binary representation of " + number + ":" );
+        obj.displayBinaryNumber(binaryRepresentation);      // Calling the method to convert number to binary number
+
+        System.out.println("\nNumber of zero bits: " + obj.countZero(binaryRepresentation));    // Calling the method to count of zero bits
     }
 
-    public int userInputs() {   // Creating a method to get values from the user
-        Scanner in = new Scanner(System.in);    // Creating a scanner class
+    public int userInputs() {       // Creating a method to get values from the user
+        Scanner in = new Scanner(System.in);     // Creating a scanner class
 
         System.out.println("Enter the number:");
-        int num = in.nextInt();   // Storing the value to a variable
+        int num = in.nextInt();      // Storing the value to a variable
 
         in.close();   // Closing scanner to avoid data breach
 
         return num;     // returning the input value
     }
 
-    public Queue<Integer> convertToBinary(int num) {     // Creating a method to convert the number to binary
-        Queue<Integer> binaryNumber = new LinkedList<>();  // Creating a queue interface
-        int reminder = 0;
+    public Stack<Integer> convertToBinary(int num) {     // Creating a method to convert the number to binary
+        Stack<Integer> binaryNumber = new Stack<>();     // Creating a queue interface
 
-        if (num == 0){  // If the input value is zero then it returns 0
+        if (num == 0){      // If the input value is zero then it returns 0
             binaryNumber.add(0);    // Add the number to the queue
 
             return binaryNumber;    // Returning the binary number
         }
         while (num > 0) {    // Here the conversion of binary number takes place
 
-            reminder = num % 2;
-            binaryNumber.add(reminder);
-            num = num / 2;
+            int reminder = num % 2;     // Getting the reminder from the number
+            binaryNumber.add(reminder);     // Storing the reminder in the stack
+            num = num / 2;      // Reducing the number by dividing it
         }
-        return binaryNumber;    //
+        return binaryNumber;    // Returning the binary number
     }
 
-    public int count(Queue<Integer> num){    // Creating a method to count the zero bits present in the binary number
-        int count = 0;      //
+    public void displayBinaryNumber(Stack<Integer> data) {       // Method to display the binary number
+        Stack<Integer> result = (Stack<Integer>) data.clone();
 
-        for (int i = 0; i < num.size(); i++) {
-            // Here I'm converting the queue interface to an linked list so that I can access a certain element
-            int element = ((LinkedList<Integer>) num).get(i);
-            if (element == 0) {
-                count++;    // For each iteration the count increase a values by 1
+        while (!result.isEmpty()){
+            System.out.print(result.pop());
+        }
+    }
+
+    public int countZero(Stack<Integer> num) {     // Creating a method to count the zero bits present in the binary number
+        int count = 0;      // Initializing count to zero
+
+        for (int i = 0; i < num.size(); i++) {      // Iterating through the stack
+            if (num.get(i) == 0) {      // Checking if the stack is zero
+                count++;     // For each iteration the count increase a values by 1
             }
         }
-        return count;  // Returning the count value
+        return count;     // Returning the count value
     }
 }
