bcf514e846c2ac479fe6e0965269ac5591977f5d
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-29 23:41:17 +0530

    Updated Code

--- Java/src/JavaBasics/W207.java
+++ Java/src/JavaBasics/W207.java
@@ -1,51 +1,55 @@
 package JavaBasics;
-import java.util.Scanner;
+
+import java.util.Scanner;       // Importing scanner class
+
 // Write a Java program to merge two sorted (ascending) linked lists in ascending order.
-public class W207 {
-    Node head;
-    Node tail;
-    int size;
+public class W207 {     // Creating a class which handles linked list
+    Node head;      // Creating a reference for head
+    Node tail;      // Creating a reference for tail
+    int size;       // Creating a size
+
     static int count = 1;   // Initializing as static to increment the count correctly whenever the instances of the class is called
     int listCount;
 
-    W207(){
-        this.size = 0;
-        this.listCount = count++;
+    W207() {        // Creating a default constructor
+        this.size = 0;       // Initializing the size to 0
+        this.listCount = count++;       // Incrementing count by 1 whenever the object is created
     }
 
-    public void insertLast(int data){
-        Node newNode = new Node(data);
+    public void insertLast(int data) {      // Method to insert node at last
+        Node newNode = new Node(data);      // Creating a node by passing the value
 
-        if (head == null){
-            head = newNode;
-            tail = newNode;
-        } else {
-            tail.next = newNode;
-            tail = newNode;
+        // Checking if the head is null. If it's null, then the list is empty
+        if (head == null) {
+            head = newNode;     // Creating a new head
+
+        } else {        // If there is more than 1 node, then tail is updated
+            tail.next = newNode;      // Assigning the tail pointer to new node
         }
-        size++;
+        tail = newNode;     // Updating the tail
+        size++;     // Incrementing the size by 1
     }
 
-    public void createLinkedList(){
-        Scanner in = new Scanner(System.in);
+    public void insertNode() {        // Method to insert node in the linked list
+        Scanner in = new Scanner(System.in);        // Creating a scanner object
 
         System.out.println("Enter the size of the Linked list " + listCount + ":");
-        int size = in.nextInt();
+        int size = in.nextInt();        // Getting size from the user
 
         System.out.println("Enter the values of linked list " + listCount + ":");
-        for (int i = 0; i < size; i++) {
-            insertLast(in.nextInt());
+        for (int i = 0; i < size; i++) {        // Iterating through the linked list
+            insertLast(in.nextInt());       // Inserting the node for each iteration in the linked list
         }
     }
 
-    public void sortLinkedList(){
+    public void sortLinkedList() {      // Method to sort the linked list
         System.out.println("Sorted linked list " + listCount + ":");
 
         for (Node i = head; i != null ; i = i.next) {
 
             for (Node j = head; j.next != null ; j = j.next) {
 
-                if(j.value > j.next.value){
+                if (j.value > j.next.value) {
                     int temp = j.value;
                     j.value = j.next.value;
                     j.next.value = temp;
@@ -56,15 +60,17 @@ public class W207 {
 
     public static W207 mergeLinkedList(W207 list1, W207 list2) {
         W207 mergeList = new W207();
+
         Node curr1 = list1.head;
         Node curr2 = list2.head;
-        System.out.println("Merged the linked list in ascending order:");
 
+        System.out.println("Merged the linked list in ascending order:");
         while (curr1 !=  null && curr2 != null) {
 
             if (curr1.value <= curr2.value) {
                 mergeList.insertLast(curr1.value);
                 curr1 = curr1.next;
+
             } else {
                 mergeList.insertLast(curr2.value);
                 curr2 = curr2.next;
@@ -74,6 +80,7 @@ public class W207 {
             mergeList.insertLast(curr1.value);
             curr1 = curr1.next;
         }
+
         while (curr2 != null) {
             mergeList.insertLast(curr2.value);
             curr2 = curr2.next;
@@ -85,7 +92,7 @@ public class W207 {
         Node temp = head;
 
         while (temp != null) {
-            System.out.print(temp.value+"->");
+            System.out.print(temp.value + " -> ");
             temp = temp.next;
         }
         System.out.print("END\n");
@@ -103,8 +110,8 @@ public class W207 {
         W207 obj1 = new W207();
         W207 obj2 = new W207();
 
-        obj1.createLinkedList();
-        obj2.createLinkedList();
+        obj1.insertNode();
+        obj2.insertNode();
 
         System.out.println("Linked list 1: ");
         obj1.display();
