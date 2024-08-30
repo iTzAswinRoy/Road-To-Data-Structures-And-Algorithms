a187689518c7c2bfdab7256a4eb2a5a518d38a35
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-30 21:47:27 +0530

    Updated Code

---
+++ Java/src/JavaRecursion/String/R2.java
@@ -0,0 +1,21 @@
+package JavaRecursion.String;
+// subsequence of the string
+public class R2 {
+    public static void main(String[] args) {
+        R2 obj = new R2();
+        obj.subSeq("abc", "");
+    }
+
+    public void subSeq(String data, String result) {
+        System.out.println("subSeq called with data: \"" + data + "\", result: \"" + result + "\"");
+
+        if(data.isEmpty()) {
+            System.out.println(result);
+            return;
+        }
+        char ch = data.charAt(0);
+
+        subSeq(data.substring(1), result + ch);
+        subSeq(data.substring(1), result);
+    }
+}
