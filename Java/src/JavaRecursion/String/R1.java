67a3ada1214407f8b8ddba3f224a1bbde87d7915
iTzAswinRoy <aswinroy255@gmail.com>
2024-08-30 21:47:20 +0530

    Updated Code

---
+++ Java/src/JavaRecursion/String/R1.java
@@ -0,0 +1,24 @@
+package JavaRecursion.String;
+
+// Skip a character
+public class R1 {
+    public static void main(String[] args) {
+        R1 obj = new R1();
+        String temp = "baccad";
+
+        System.out.println(obj.skipChar(temp));
+    }
+
+    public String skipChar(String sen) {
+        if (sen.isEmpty()) {
+            return "";
+        }
+        char currentChar = sen.charAt(0);
+
+        if (sen.charAt(0) == 'a') {
+            return skipChar(sen.substring(1));
+        }
+
+        return currentChar + skipChar(sen.substring(1));
+    }
+}
