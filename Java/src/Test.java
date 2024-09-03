b52602c855975154871d1831ddf7f741a134974f
iTzAswinRoy <aswinroy255@gmail.com>
2024-09-03 21:26:00 +0530

    Updated Code

--- Java/src/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test{
-    public static void main(String[] args) {
-        String t = "aswin";
-        if(t.startsWith("a")){
-            System.out.println("true");
-        }
-        t.contains("s");
-    }
-}
