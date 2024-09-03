361cd8df7b0e1c2844db661a8022220d66139657
iTzAswinRoy <aswinroy255@gmail.com>
2024-09-03 21:26:05 +0530

    Updated Code

--- Java/src/JavaConstructor/C8.java
+++ Java/src/JavaConstructor/C8.java
@@ -1,31 +1,32 @@
 package JavaConstructor;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.ArrayList;      // Importing array list class
 
 // Write a class Library that initializes a list of books using an ArrayList passed to the constructor.
-public class C8 {       //
-    private ArrayList<String> books;        //
+public class C8 {       // Creating a class named 'Library' which contains player details
+    private ArrayList<String> books;        // Creating an instance variable of type array list
 
-    C8(ArrayList<String> books){        //
+    C8(ArrayList<String> books) {        // Creating a parameterized constructor that takes array list as an arguments
+        // Initialing values to the instance variables
         this.books = books;
     }
 
-    public void getBooks() {        //
-        System.out.println((books));
+    public void getBooks() {        // Creating a getter method to get the instance variable book
+        System.out.println(books);      // Displaying the books
     }
 
-    public static void main(String[] args) {        //
-        ArrayList<String> bookTitles = new ArrayList<>();       //
+    public static void main(String[] args) {        // Main method
+        ArrayList<String> bookTitles = new ArrayList<>();       // Creating an array list that holds the book titles
 
+        // Inserting elements to the array list
         bookTitles.add("Harry Potter");
         bookTitles.add("Prince of Persia");
         bookTitles.add("Joker");
 
-        C8 obj = new C8(bookTitles);        //
+        C8 obj = new C8(bookTitles);        // Creating an objects by passing array list as an arguments
 
+        // Displaying the object's parameter using the getter method
         System.out.println("Displaying the books from the collection:");
-        obj.getBooks();     //
+        obj.getBooks();
     }
 }
