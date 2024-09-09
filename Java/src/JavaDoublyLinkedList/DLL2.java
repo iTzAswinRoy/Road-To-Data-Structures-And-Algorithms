4893677901d605a96eb4ed1737066d618b7d7fb9
iTzAswinRoy <aswinroy255@gmail.com>
2024-09-09 23:40:04 +0530

    Updated Code

--- Java/src/JavaDoublyLinkedList/DLL2.java
+++ Java/src/JavaDoublyLinkedList/DLL2.java
@@ -7,7 +7,7 @@ public class DLL2 {     //
     DLL2() {        //
         this.size = 0;
     }
-    public void insertNode(int value){
+    public void insertNode(int value){      //
         Node newNode = new Node(value);
         if(tail == null){
             head = newNode;
