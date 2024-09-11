cbd6dea379305c7b9395b3c81966d4b51c1414d7
iTzAswinRoy <aswinroy255@gmail.com>
2024-09-11 23:52:29 +0530

    Updated Code

---
+++ Java/src/JavaHashSet/HS6.java
@@ -0,0 +1,19 @@
+package JavaHashSet;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+// Write a program that converts an array of strings into a HashSet to remove duplicates.
+public class HS6 {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+
+        int[] array = {1,5,2,7,2,5,1};
+        System.out.println("Given array:\n"+ Arrays.toString(array));
+
+        for (int i = 0; i < array.length; i++) {
+            set.add(array[i]);
+        }
+        System.out.println("\nConverting arrays into hash map:\n"+set);
+    }
+}
