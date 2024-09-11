1e152211fb55d4356affa523bbb1d6da364e22c9
iTzAswinRoy <aswinroy255@gmail.com>
2024-09-11 23:51:58 +0530

    Updated Code

---
+++ Java/src/JavaHashSet/HS1.java
@@ -0,0 +1,23 @@
+package JavaHashSet;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+// Write a program that adds a list of integers to a HashSet and prints the set.
+public class HS1 {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(1);
+        set.add(2);
+        set.add(2);
+        set.add(3);
+        set.add(3);
+        set.add(4);
+        set.add(4);
+
+        System.out.println("Adding elements to the hash set...");
+        System.out.println("\nOriginal hash set:\n" + set);
+
+    }
+}
