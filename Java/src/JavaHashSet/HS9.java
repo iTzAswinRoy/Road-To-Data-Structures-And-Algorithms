16bbcd7c1b2dbe5e93f2a3af592a8e2b0d827120
iTzAswinRoy <aswinroy255@gmail.com>
2024-09-11 23:52:48 +0530

    Updated Code

---
+++ Java/src/JavaHashSet/HS9.java
@@ -0,0 +1,26 @@
+package JavaHashSet;
+
+import java.util.HashSet;
+
+// Write a program that finds the difference between two HashSets (i.e., elements present in one set but not the other).
+public class HS9 {
+    public static void main(String[] args) {
+        // Create two HashSet objects
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+
+        // Add elements to set2
+        set2.add(3);
+        set2.add(4);
+        set2.add(5);
+
+        HashSet<Integer> difference = new HashSet<>(set1);
+        difference.removeAll(set2);
+
+        System.out.println("Hash set after removing all the common elements:\n"+ difference);
+    }
+}
