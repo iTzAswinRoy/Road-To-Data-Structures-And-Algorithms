8a1a70b71f284c1e604d177fedd7346ed465d739
iTzAswinRoy <aswinroy255@gmail.com>
2024-09-12 23:09:07 +0530

    Updated Code

---
+++ Java/src/JavaPattern/P4.java
@@ -0,0 +1,20 @@
+package JavaPattern;
+
+// Print a pyramid with n levels using asterisks (*)
+public class P4 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = i+1; k > 0; k--) {
+                System.out.print(" *");
+
+            }
+            System.out.println();
+        }
+    }
+}
