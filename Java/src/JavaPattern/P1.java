0a25226f95ce1a5838fe95104e8e610a17472d4d
iTzAswinRoy <aswinroy255@gmail.com>
2024-09-12 23:08:49 +0530

    Updated Code

---
+++ Java/src/JavaPattern/P1.java
@@ -0,0 +1,14 @@
+package JavaPattern;
+
+// Print a n x n square using asterisks (*).
+public class P1 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
