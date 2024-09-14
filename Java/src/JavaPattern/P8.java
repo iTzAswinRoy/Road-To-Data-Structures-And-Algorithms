4964e707b5f88268fab0253f0ad867c0ec3fd22e
iTzAswinRoy <aswinroy255@gmail.com>
2024-09-14 19:31:06 +0530

    Updated Code

--- Java/src/JavaPattern/P8.java
+++ Java/src/JavaPattern/P8.java
@@ -1,13 +1,18 @@
 package JavaPattern;
 
-// Print a hollow diamond shape with n levels using asterisks (*).
+// Print a right-angled triangle where each row contains increasing numbers starting from 1.
 public class P8 {
     public static void main(String[] args) {
         int n = 5;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i ==0 || j == n-1 ||)
+        int count = 1;
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = n-i ; j < n; j++) {
+                System.out.print(" "+count);
+                count++;
             }
+            count = 1;
+            System.out.println();
         }
     }
 }
