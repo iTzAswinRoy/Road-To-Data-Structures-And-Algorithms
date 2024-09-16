f0533aecff2b824a443a73faa3deb5e54604bc17
iTzAswinRoy <aswinroy255@gmail.com>
2024-09-16 21:33:52 +0530

    Updated Code

---
+++ Java/src/JavaMatrix/M2.java
@@ -0,0 +1,49 @@
+package JavaMatrix;
+
+import java.util.Arrays;
+
+// Write a function that subtracts one matrix from another of the same size.
+public class M2 {
+    public static void main(String[] args) {
+        M2 obj = new M2();
+
+        int[][] mat1 = {{1,2,3}, {4,5,6}};
+        int[][] mat2 = {{10,20,30}, {40,50,60}};
+
+        System.out.println("Given matrix:");
+        System.out.println("Matrix - 1:");
+        obj.display(mat1);
+
+        System.out.println("\nMatrix - 2:");
+        obj.display(mat2);
+
+        try {
+            System.out.println("\nDifference of matrix:");
+            obj.display(obj.subMatrix(mat1, mat2));
+
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public int[][] subMatrix(int[][] data1, int[][] data2) {
+        if (data1 == null || data2 == null || data1.length != data2.length || data1[0].length != data2[0].length) {
+            throw new IllegalArgumentException("Matrix size is not equal!");
+        }
+        int[][] result = new int[data1.length][data1[0].length];
+
+        for (int i = 0; i < data1.length; i++) {
+
+            for (int j = 0; j < data1[0].length; j++) {
+                result[i][j] = Math.abs(data1[i][j] - data2[i][j]);
+            }
+        }
+        return result;
+    }
+
+    public void display(int[][] data){
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(Arrays.toString(data[i]));
+        }
+    }
+}
