cfd618dc084b451399aeeae97924c192b1a31f7b
iTzAswinRoy <aswinroy255@gmail.com>
2024-09-17 22:13:28 +0530

    Updated Code

--- Java/src/JavaConstructor/C14.java
+++ Java/src/JavaConstructor/C14.java
@@ -1,21 +1,24 @@
 package JavaConstructor;
 
 // Write a class Computer with an inner class Processor. Initialize the Processor using the outer class's constructor.
-public class C14 {      //
-    private String brand;       //
-    private Processor processor;
+public class C14 {      // Creating a class named 'Computer' that contains 'database'
+    // Creating instance variables for the class
 
-    C14(String brand, int ram, int storage, int graphicsCard) {
-        this.brand = brand;     //
-        this.processor = new Processor(ram, storage, graphicsCard);     //
+    private String brand;       // Crating a variable to store the brand
+    private Processor processor;        // Crating a reference variable of class processor
+
+    C14(String brand, int ram, int storage, int graphicsCard) {         // Creating a parameterized constructor that takes 3 arguments
+        // Initialing values to the instance variables
+        this.brand = brand;
+        this.processor = new Processor(ram, storage, graphicsCard);         // Initialling values to new object of class 'processor' type
     }
 
-    public String getBrand() {      //
-        return brand;
+    public String getBrand() {      // Creating a getter method to get the instance variable brand
+        return brand;        // Returning the brand
     }
 
-    public void displayDetails() {          //
-        System.out.println("Computer brand: " + getBrand());        //
+    public void displayDetails() {          // Method to display the details
+        System.out.println("Computer brand: " + getBrand());        // Calling the getter method to display the instance variable
         processor.getProcessorDetails();        //
     }
 
