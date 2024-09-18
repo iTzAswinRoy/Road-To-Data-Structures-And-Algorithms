b98323dc42d14d59fc8f5d27d4afeae7be5ff8e8
iTzAswinRoy <aswinroy255@gmail.com>
2024-09-18 22:13:30 +0530

    Updated Code

--- Java/src/JavaHashSet/HS1.java
+++ Java/src/JavaHashSet/HS1.java
@@ -14,7 +14,7 @@ public class HS1 {
         set.add(3);
         set.add(3);
         set.add(4);
-        set.add(4);
+        set.add(4);//
 
         System.out.println("Adding elements to the hash set...");
         System.out.println("\nOriginal hash set:\n" + set);
