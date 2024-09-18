969a3dc637d15ed2f8cd0df63ffadaa80aa3dc76
iTzAswinRoy <aswinroy255@gmail.com>
2024-09-18 22:12:32 +0530

    Updated Code

--- Java/src/JavaHashSet/HS2.java
+++ Java/src/JavaHashSet/HS2.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 // Write a program to remove a specified element from a HashSet.
 public class HS2 {
     public static void main(String[] args) {
-        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();//
 
         set.add(1);
         set.add(2);
