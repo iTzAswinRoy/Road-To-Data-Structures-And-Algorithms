c5dbe19fbf837d5a3562847890edc10fd4690b91
iTzAswinRoy <aswinroy255@gmail.com>
2024-09-19 23:42:24 +0530

    Updated Code

--- Java/src/JavaDoublyLinkedList/DLL2.java
+++ Java/src/JavaDoublyLinkedList/DLL2.java
@@ -31,7 +31,7 @@ public class DLL2 {     //
         }
     }
     public void display(){//
-        Node temp = head;
+        Node temp = head;//
         for (int i = 0; i < size; i++) {
             System.out.print(temp.value+" -> ");
             temp = temp.next;
