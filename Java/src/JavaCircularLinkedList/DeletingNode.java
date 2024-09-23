e12f44662ba8d10cefa204e1c551b5ccc2cfb6c7
iTzAswinRoy <aswinroy255@gmail.com>
2024-09-23 19:24:09 +0530

    Circular Linked List

--- Java/src/JavaCircularLinkedList/DeletingNode.java
+++ Java/src/JavaCircularLinkedList/DeletingNode.java
@@ -2,12 +2,23 @@ package JavaCircularLinkedList;
 
 // Implement a function to delete a node from a circular linked list.
 public class DeletingNode {       // Circular linked list class
-    int size;       // Creating instance variable for size
-    Node head;      // Creating head pointer
-    Node tail;      // Creating tail pointer
+    // Creating instance variables for the class
+    int size;       // Creating a variable to store size
+    Node head;      // Crating a reference variable 'head' of class Node type
+    Node tail;      // Crating a reference variable 'tail' of class Node type
 
     DeletingNode() {        // Creating a default constructor
         this.size = 0;      // Initializing the size to 0
+    }
+
+    class Node {        // Creating a 'Node' class
+        // Creating instance variables for the class 'Node'
+        int value;      // Creating an instance variable to store a value for the node
+        Node next;      // Creating a reference, pointing towards the next node
+
+        Node(int value) {       // Creating a parameter constructor by passing the value
+            this.value = value;      // Assigning the parameter value to the instance variable
+        }
     }
 
     public void insertNode(int value) {     // Method to insert node in the circular linked list
@@ -84,15 +95,6 @@ public class DeletingNode {       // Circular linked list class
         } while (temp != head);     // Condition keeps going until the pointer reaches the tail node
 
         System.out.println("HEAD");     // Displaying 'head' for reference
-    }
-
-    class Node {        // Creating a node class
-        int value;      // Creating an instance variable to store a value for the node
-        Node next;      // Creating a reference, pointing towards the next node
-
-        Node(int value) {       // Creating a parameter constructor by passing the value
-            this.value = value;      // Assigning the parameter value to the instance variable
-        }
     }
 
     public static void main(String[] args) {        // Main method
