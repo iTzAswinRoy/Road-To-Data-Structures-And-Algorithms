63249c303372e486c18c7f28274bfa22ed686fba
iTzAswinRoy <aswinroy255@gmail.com>
2024-10-01 21:25:17 +0530

    Updated Code

--- Java/src/JavaDoublyLinkedList/DLL4.java
+++ Java/src/JavaDoublyLinkedList/DLL4.java
@@ -11,6 +11,7 @@ public class DLL4 {     // Creating a class for doubly linked list
     }
 
     class Node{     //
+        // Creating instance variables for the class Node
         private Node prev;
         private Node next;
         private int value;
