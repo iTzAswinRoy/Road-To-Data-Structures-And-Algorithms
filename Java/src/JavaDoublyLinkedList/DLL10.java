423c6a287c996e9f374f79d9e6e9a98dedbad96a
iTzAswinRoy <aswinroy255@gmail.com>
2024-10-09 21:38:03 +0530

    Updated Code

--- Java/src/JavaDoublyLinkedList/DLL10.java
+++ Java/src/JavaDoublyLinkedList/DLL10.java
@@ -1,84 +1,106 @@
 package JavaDoublyLinkedList;
 // Split a doubly linked list into two equal halves. If the list has an odd number of nodes, the first half should have one more node than the second.
-public class DLL10 {
-    Node head;
-    Node tail;
-    int size;
-    DLL10() {
-        this.size = 0;
+public class DLL10 {        // Creating a class for doubly linked list
+    // Creating instance variables for the class
+    private Node head;      // Crating a reference variable 'head' of class Node type
+    private Node tail;      // Crating a reference variable 'tail' of class Node type
+    private int size;       // Creating a variable to store the size
+
+    DLL10() {       // Creating a default constructor
+        this.size = 0;      // Initializing size to the instance variable
     }
-    public void insertNode(int value){//
-        Node newNode = new Node(value);//
-        if(tail == null){//
-            head = newNode;//
-            tail = head;//
-        } else {
-            tail.next = newNode;//
-            newNode.prev = tail;//
-            tail = newNode;//
+
+    class Node {        // Creating a class for doubly linked list
+        // Creating instance variables for the class
+        private Node next;      // Creating a reference variable, pointing towards the next node
+        private Node prev;      // Creating a reference variable, pointing towards the previous node
+        private int value;      // Creating an instance variable to store a value of the node
+
+        Node(int value) {       // Creating a parameter constructor by passing the value
+            this.value = value;     // Assigning the parameter value to the instance variable
         }
-        size++;//
     }
-    public void splittingList(){//
+
+    public void insertNode(int value) {     // Method to insert node to the doubly linked list
+        Node newNode = new Node(value);     // Creating a new node by passing a value
+
+        if (tail == null) {     // Checking if the tail is null
+            head = newNode;     // Assigning the head to new node
+            tail = head;        // Updating the tail to head since there's only one node
+
+        } else {
+            tail.next = newNode;        // Updating the tail with the new node
+            newNode.prev = tail;        // Assigning the previous pointer of the node to tail
+            tail = newNode;         // Now updating the tail as new node
+        }
+        size++;     // Incrementing size by 1
+    }
+
+    public void splittingList() {       // Method to split the doubly linked list
+        // Creating a separate doubly linked list
         DLL10 list1 = new DLL10();
         DLL10 list2 = new DLL10();
-        Node temp = head;
-        if(size % 2 == 0){
-            for (int i = 0; i < size / 2; i++) {
-                list1.insertNode(temp.value);
-                temp = temp.next;
+
+        Node temp = head;       // Creating a reference variable 'temp' pointing at the head
+
+        if (size % 2 == 0) {        // Checking if the size fo the linked list is even
+            for (int i = 0; i < size / 2; i++) {        // Iterating till the middle of the linked list
+                list1.insertNode(temp.value);       // Inserting the node to the first linked list
+                temp = temp.next;       // Moving the linked list to the next node
             }
-            while (temp != null){
-                list2.insertNode(temp.value);
-                temp = temp.next;
-            }
-        } else {
-            for (int i = 0; i <= size / 2; i++) {
-                list1.insertNode(temp.value);
-                temp = temp.next;
-            }
-            while (temp != null){
-                list2.insertNode(temp.value);
-                temp = temp.next;
+
+        } else {        // Odd linked list
+            for (int i = 0; i <= size / 2; i++) {       // Iterating till the middle of the linked list
+                list1.insertNode(temp.value);       // Inserting the node to the first linked list
+                temp = temp.next;       // Moving the 'temp' pointer to the next node
             }
         }
-        System.out.println("\nFirst half:");
+
+        // Inserting the remaking nodes to the second linked list
+        while (temp != null) {
+            list2.insertNode(temp.value);
+            temp = temp.next;       // Moving the 'temp' pointer to the next node
+        }
+
+        // Displaying the first half of the doubly linked list
+        System.out.println("First half:");
         list1.display();
 
+        // Displaying the second half of the doubly linked list
         System.out.println("\nSecond half:");
         list2.display();
     }
-    public void display() {
-        Node temp = head;
-        for (int i = 0; i < size; i++) {
-            System.out.print(temp.value + " -> ");
-            temp = temp.next;
-        }
-        if (temp == null) {
-            System.out.println("END");
-        }
-    }
-    class Node {
-        Node next;
-        Node prev;
-        int value;
 
-        Node(int value) {
-            this.value = value;
+    public void display() {     // Method to display the linked list
+        Node temp = head;       // Creating a reference variable 'temp' pointing at head
+
+        for (int i = 0; i < size; i++) {        // Iterating through linked list
+            System.out.print(temp.value + " -> ");       // Displaying the node
+            temp = temp.next;       // Moving the 'temp' pointer to the next node
+        }
+
+        if (temp == null) {      // Checking if 'temp' is null
+            System.out.println("END");      // Displaying 'END' for reference
         }
     }
-    public static void main(String[] args) {
+
+    public static void main(String[] args) {        // Main method
+        // Creating an object to store the doubly linked list
         DLL10 doublyLinkedList = new DLL10();
+
+        // Calling the method to insert the node in doubly linked list
         doublyLinkedList.insertNode(1);
         doublyLinkedList.insertNode(2);
         doublyLinkedList.insertNode(3);
         doublyLinkedList.insertNode(4);
         doublyLinkedList.insertNode(5);
 
+        // Displaying the doubly linked list
         System.out.println("Original doubly linked list:");
         doublyLinkedList.display();
 
-        System.out.println("\nSplitting the the doubly linked list.");
+        // Calling the method to split the doubly linked list
+        System.out.println("\nSplitting the the doubly linked list:");
         doublyLinkedList.splittingList();
     }
 }
