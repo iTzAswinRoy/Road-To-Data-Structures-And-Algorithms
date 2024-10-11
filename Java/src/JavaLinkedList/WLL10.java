dd122da45becabbab27d505067a638022e273094
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-10-11 17:15:30 +0530

    Update WLL10.java

--- Java/src/JavaLinkedList/WLL10.java
+++ Java/src/JavaLinkedList/WLL10.java
@@ -14,7 +14,7 @@ public class WLL10 {
             head = newNode;
             tail = head;//
         } else {
-            tail.next = newNode;
+            tail.next = newNode;//
             tail = newNode;//
         }
         size++;
