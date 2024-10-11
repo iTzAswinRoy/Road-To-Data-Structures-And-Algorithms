5367138955f1590efcfb083fba385abd6a60e765
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-10-11 17:14:21 +0530

    Update WLL10.java

--- Java/src/JavaLinkedList/WLL10.java
+++ Java/src/JavaLinkedList/WLL10.java
@@ -2,7 +2,7 @@ package JavaLinkedList;
 // Write a Java program to get the first and last occurrence of the specified elements in a linked list.
 public class WLL10 {
     Node head;//
-    Node tail;
+    Node tail;//
     int size;
     WLL10(){
         this.size = 0;
