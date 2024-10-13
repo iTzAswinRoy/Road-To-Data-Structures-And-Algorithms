0687a2b73ea285e546f0d138b53d6b126d3bec95
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-10-13 19:59:29 +0530

    Update P10.java

--- Java/src/JavaPattern/P10.java
+++ Java/src/JavaPattern/P10.java
@@ -7,7 +7,7 @@ public class P10 {
 
         for (int i = 0; i < n; i++) {//
 
-            for (int j = 0; j <= i; j++) {
+            for (int j = 0; j <= i; j++) {//
                 System.out.print("*");//
             }
 
