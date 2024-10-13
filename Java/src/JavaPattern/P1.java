940797d774e763277ef48cf27fb3bf9238b32565
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-10-13 19:58:40 +0530

    Update P1.java

--- Java/src/JavaPattern/P1.java
+++ Java/src/JavaPattern/P1.java
@@ -5,7 +5,7 @@ public class P1 {//
     public static void main(String[] args) {//
         int n = 5;//
         for (int i = 0; i < n; i++) {//
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < n; j++) {//
                 System.out.print("*");
             }
             System.out.println();
