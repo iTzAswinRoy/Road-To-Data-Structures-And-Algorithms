1a43b67e5854e243fabfd2e8624789c35514257d
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-10-13 19:58:24 +0530

    Update P1.java

--- Java/src/JavaPattern/P1.java
+++ Java/src/JavaPattern/P1.java
@@ -3,7 +3,7 @@ package JavaPattern;
 // Print a n x n square using asterisks (*).
 public class P1 {//
     public static void main(String[] args) {//
-        int n = 5;
+        int n = 5;//
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print("*");
