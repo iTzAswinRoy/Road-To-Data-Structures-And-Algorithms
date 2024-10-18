349ca486c8d28c6f9cbd0658ca5feaff3aeec2de
iTzAswinRoy <aswinroy255@gmail.com>
2024-10-18 22:43:51 +0530

    Updated Code

--- Java/src/JavaSegmentTree/T5.java
+++ Java/src/JavaSegmentTree/T5.java
@@ -1,4 +1,5 @@
 package JavaSegmentTree;
+
 // Build a segment tree that supports:
 // Update: Change the value of a particular index.
 //        Query: Count how many even and odd numbers exist in a given range [L, R].
@@ -7,7 +8,20 @@ package JavaSegmentTree;
 //        - Count even and odd numbers from index 1 to 4.
 //        - Update index 3 to 10.
 //        - Count even and odd numbers from index 1 to 4.
-
 public class T5 {
+    Node root;
 
+    class Node{
+        int data;
+        Node left;
+        Node right;
+        int startIndex;
+        int endIndex;
+
+     Node(int startIndex, int endIndex){
+         this.startIndex = startIndex;
+         this.endIndex = endIndex;
+     }
+
+    }
 }
