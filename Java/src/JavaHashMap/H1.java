65523193d976f659c3270f01fd866ec1bb0b4e8d
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-10-24 23:14:16 +0530

    Update H1.java

--- Java/src/JavaHashMap/H1.java
+++ Java/src/JavaHashMap/H1.java
@@ -8,7 +8,7 @@ public class H1 {       //
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i <= array.length; i++){
-            int complement = target - array[i];
+            int complement = target - array[i];//
 
             if(map.containsKey(complement)){
                 return new int[]{map.get(complement), i};
