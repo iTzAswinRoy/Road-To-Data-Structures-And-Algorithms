a7d9e531ff8e77381ef5b185e25140268d59bd89
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-10-24 23:14:23 +0530

    Update H1.java

--- Java/src/JavaHashMap/H1.java
+++ Java/src/JavaHashMap/H1.java
@@ -7,7 +7,7 @@ public class H1 {       //
     public static int[] twoSum(int[] array, int target)// {        //
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i <= array.length; i++){
+        for (int i = 0; i <= array.length; i++){//
             int complement = target - array[i];//
 
             if(map.containsKey(complement)){
