0c9b61fabdc7d725a3c8ca4c0e6bfe1215ab91f0
iTzAswinRoy <aswinroy255@gmail.com>
2024-10-25 21:56:24 +0530

    Updated Code

--- Java/src/JavaPattern/P2.java
+++ Java/src/JavaPattern/P2.java
@@ -4,6 +4,7 @@ package JavaPattern;
 public class P2 {
     public static void main(String[] args) {
         int n = 5;      // Initializing as 5
+        // Creating a nested for loop
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print("*");
