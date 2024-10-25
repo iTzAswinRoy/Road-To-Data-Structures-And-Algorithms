5328be2a5282585650b185b99b00c4cfc975a991
iTzAswinRoy <aswinroy255@gmail.com>
2024-10-25 21:55:20 +0530

    Updated Code

--- Java/src/JavaPattern/P1.java
+++ Java/src/JavaPattern/P1.java
@@ -5,8 +5,8 @@ public class P1 {//
     public static void main(String[] args) {        // Main method
         int n = 5;      // Initializing as 5
         // Creating a nested for loop
-        for (int i = 0; i < n; i++) {//
-            for (int j = 0; j < n; j++) {//
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 System.out.print("*");      //
             }
             System.out.println();       // Giving a new line for each rows
