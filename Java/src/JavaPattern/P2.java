71637e9f7f03ea82453bc1b90eacf9c522a6fb7b
iTzAswinRoy <aswinroy255@gmail.com>
2024-10-25 21:56:10 +0530

    Updated Code

--- Java/src/JavaPattern/P2.java
+++ Java/src/JavaPattern/P2.java
@@ -3,7 +3,7 @@ package JavaPattern;
 // Print a right-angled triangle with n rows using asterisks (`*`)
 public class P2 {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 5;      // Initializing as 5
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print("*");
