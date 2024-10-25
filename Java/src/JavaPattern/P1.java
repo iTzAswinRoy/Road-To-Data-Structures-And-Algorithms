cf95fa60a5905a487c4f7a61c78e503db7d7e433
iTzAswinRoy <aswinroy255@gmail.com>
2024-10-25 21:54:06 +0530

    Updated Code

--- Java/src/JavaPattern/P1.java
+++ Java/src/JavaPattern/P1.java
@@ -4,6 +4,7 @@ package JavaPattern;
 public class P1 {//
     public static void main(String[] args) {        // Main method
         int n = 5;      // Initializing as 5
+        //
         for (int i = 0; i < n; i++) {//
             for (int j = 0; j < n; j++) {//
                 System.out.print("*");//
