81c6e9ce7960861f3d60bc296d4527f111e24545
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-10-26 22:26:38 +0530

    Update CheckSorted.java

--- Java/src/JavaRecursion/ArrayList/CheckSorted.java
+++ Java/src/JavaRecursion/ArrayList/CheckSorted.java
@@ -9,7 +9,7 @@ public class CheckSorted {//
 
         ArrayList<Integer> list = new ArrayList<>();
         list.add(5);//
-        list.add(2);
+        list.add(2);//
         list.add(4);
         list.add(1);
         list.add(3);
