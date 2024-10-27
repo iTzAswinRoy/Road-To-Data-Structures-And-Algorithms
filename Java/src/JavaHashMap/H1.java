35a633f7eea0f2f0b60a23d3d25903c742cb5386
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-10-27 22:01:21 +0530

    Update H1.java

--- Java/src/JavaHashMap/H1.java
+++ Java/src/JavaHashMap/H1.java
@@ -21,7 +21,7 @@ public class H1 {       //
     public static void main(String[] args) {//
         int[] numbArray = {2, 7, 11, 5};//
         int target = 16;//
-        int[] result = twoSum(numbArray, target);
+        int[] result = twoSum(numbArray, target);//
 
         System.out.println("Indices: " + result[0] + ", " + result[1]);
     }
