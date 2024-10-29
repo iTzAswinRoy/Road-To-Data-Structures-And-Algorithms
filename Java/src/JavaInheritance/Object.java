b2652f6eef1e7887bb68db0a4117765dc9ca7d1a
iTzAswinRoy <aswinroy255@gmail.com>
2024-10-29 21:43:59 +0530

    Constructor

--- Java/src/JavaInheritance/Object.java
+++ Java/src/JavaInheritance/Object.java
@@ -3,9 +3,11 @@ package JavaInheritance;
 //  Write a Java program to create a class called 'Shape' with methods called getPerimeter() and getArea().
 //  Create a subclass called 'Circle' that overrides the getPerimeter() and getArea() methods to calculate the area and perimeter of a circle.
 class Shape {       // Created a class called 'Shape'
+
     public double getPerimeter() {      // Creating an empty method 'perimeter' which returns 0
         return 0;
     }
+
     public double getArea() {       // Creating an empty method 'area' which returns 0
         return 0;
     }
