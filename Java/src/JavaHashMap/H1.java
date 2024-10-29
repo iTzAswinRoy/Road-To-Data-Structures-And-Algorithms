4930ea696ca3dab2b8bda0812f18897b9362cae0
iTzAswinRoy <aswinroy255@gmail.com>
2024-10-29 21:34:12 +0530

    Updated Code

--- Java/src/JavaHashMap/H1.java
+++ Java/src/JavaHashMap/H1.java
@@ -3,12 +3,12 @@ import java.util.HashMap;//
 
 // Given an array of integers, return the indices of the two numbers that add up to a specific target.
 // Use a hash map to store numbers and their indices.
-public class H1 {       //
-    public static int[] twoSum(int[] array, int target)// {        //
+public class H1 {
+    public static int[] twoSum(int[] array, int target){
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i <= array.length; i++){//
-            int complement = target - array[i];//
+        for (int i = 0; i <= array.length; i++){
+            int complement = target - array[i];
 
             if(map.containsKey(complement)){
                 return new int[]{map.get(complement), i};
@@ -18,7 +18,7 @@ public class H1 {       //
         return new int[]{};//
     }//
 
-    public static void main(String[] args) {//
+    public static void main(String[] args) {
         int[] numbArray = {2, 7, 11, 5};
         int target = 16;
         int[] result = twoSum(numbArray, target);
