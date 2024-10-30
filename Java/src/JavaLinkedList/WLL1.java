a335683e6ee5d03500d3d99a1404fd9cbdcfd439
iTzAswinRoy <aswinroy255@gmail.com>
2024-10-30 21:27:41 +0530

    Updated Code

--- Java/src/JavaLinkedList/WLL1.java
+++ Java/src/JavaLinkedList/WLL1.java
@@ -1,21 +1,37 @@
 package JavaLinkedList;
+
 // Write a Java program to append the specified element to the end of a linked list.
 public class WLL1 {
-    Node head;
-    Node tail;
-    int size;
+    // Creating instance variables for the class
+    private int size;       // Creating a variable to store size
+    private Node head;      // Crating a reference variable 'head' of class Node type
+    private Node tail;      // Crating a reference variable 'tail' of class Node type
 
-    public WLL1(){
-        this.size = 0;
+    public WLL1() {      // Creating a default constructor
+        this.size = 0;      // Initializing the size to 0
     }
 
-    public void insertFirst(int value){
-        Node newNode = new Node(value);
-        newNode.next = head;
-        head = newNode;
-        if(tail == null){
-            tail = head;
-            return;
+    public class Node {     // Creating a class for node
+        // Creating instance variables for the class
+        private int value;      // Creating an instance variable to store a value of the node
+        private Node next;      // Creating a reference variable, pointing towards the next node
+
+        public Node(int value) {        // Creating a parameter constructor by passing the value
+            this.value = value;         // Initializing values to the instance variable
+        }
+    }
+
+    public void insertFirst(int value) {        // Method to insert node at first in the doubly linked list
+        Node newNode = new Node(value);         // Creating a node by passing the value
+
+        // Checking if the tail is null. If it's null, then the list contains only one node
+        if (tail == null) {
+            newNode.next = head;
+            head = newNode;     // Assigning value to head
+            tail = head;        // Updating tail as head
+        } else {
+            tail.next = newNode;
+            tail = newNode;
         }
         size++;
     }
@@ -75,14 +91,6 @@ public class WLL1 {
             if (temp == null){
                 System.out.println("END");
             }
-        }
-    }
-
-    public class Node{
-        int value;
-        Node next;
-        public Node(int value){
-            this.value = value;
         }
     }
 
