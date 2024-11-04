ed71e5bc169e64f07c533f6edbd17423f6c77532
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-11-04 22:10:27 +0530

    Update BT10.java

--- Java/src/JavaBinaryTree/BT10.java
+++ Java/src/JavaBinaryTree/BT10.java
@@ -65,7 +65,7 @@ public class BT10 {
     public static void main(String[] args) {//
         BT10 obj = new BT10();//
 
-        Node root = new Node(1);
+        Node root = new Node(1);//
         root.left = new Node(2);
         root.right = new Node(3);
         root.left.left = new Node(4);
