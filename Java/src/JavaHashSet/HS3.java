bbb7a2225bb1020719ae3af11b48c9a0ad408397
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-11-08 16:59:00 +0530

    Update HS3.java

--- Java/src/JavaHashSet/HS3.java
+++ Java/src/JavaHashSet/HS3.java
@@ -9,7 +9,7 @@ public class HS3 {//
         HashSet<Integer> set = new HashSet<>();//
 
         set.add(1);//
-        set.add(2);
+        set.add(2);//
         set.add(3);//
         set.add(4);
         set.add(5);
