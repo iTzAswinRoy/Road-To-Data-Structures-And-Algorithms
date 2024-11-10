d55f6d15aba77c962845b6ae4b416983233c3cf3
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-11-10 23:45:09 +0530

    Update HS8.java

--- Java/src/JavaHashSet/HS8.java
+++ Java/src/JavaHashSet/HS8.java
@@ -6,7 +6,7 @@ import java.util.HashSet;//
 public class HS8 {//
     public static void main(String[] args) {//
     HS8 obj = new HS8();//
-    HashSet<Integer> set1 = new HashSet<>();
+    HashSet<Integer> set1 = new HashSet<>();//
 
         set1.add(1);
         set1.add(2);
