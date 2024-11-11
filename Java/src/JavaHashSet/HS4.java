d5dc062be7c3c29d3f8289c0db605f8057d32dd1
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-11-11 20:11:40 +0530

    Update HS4.java

--- Java/src/JavaHashSet/HS4.java
+++ Java/src/JavaHashSet/HS4.java
@@ -8,7 +8,7 @@ public class HS4 {//
         HS4 obj = new HS4();//
         HashSet<Integer> set = new HashSet<>();//
 
-        set.add(1);
+        set.add(1);//
         set.add(2);
         set.add(3);
         set.add(4);
