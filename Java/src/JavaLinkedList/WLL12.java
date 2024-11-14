c14aba61a81d2e7d9bddfb48441b2c70e58f7ee3
iTzAswinRoy <aswinroy255@gmail.com>
2024-11-14 23:28:08 +0530

    Linked List

--- Java/src/JavaLinkedList/WLL12.java
+++ Java/src/JavaLinkedList/WLL12.java
@@ -35,7 +35,7 @@ public class WLL12 {
     public void display(){//
         Node temp = head;//
         for (int i = 0; i < size; i++) {//
-            System.out.print(temp.value+"->");
+            System.out.print(temp.value+"->");//
             temp = temp.next;
         }
         if(temp == null){
