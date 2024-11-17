a0cf63f778e26f11a900f10302b844784ac93d16
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-11-17 22:34:13 +0530

    Update HS9.java

--- Java/src/JavaHashSet/HS9.java
+++ Java/src/JavaHashSet/HS9.java
@@ -9,7 +9,7 @@ public class HS9 {//
         HashSet<Integer> set1 = new HashSet<>();//
         HashSet<Integer> set2 = new HashSet<>();//
 
-        set1.add(1);
+        set1.add(1);//
         set1.add(2);
         set1.add(3);
 
