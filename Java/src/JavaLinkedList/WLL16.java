7f4ffb4fa9424b99e744cc6a9274ca299f829f65
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-11-18 22:56:40 +0530

    Update WLL16.java

--- Java/src/JavaLinkedList/WLL16.java
+++ Java/src/JavaLinkedList/WLL16.java
@@ -27,7 +27,7 @@ public class WLL16 {
             System.out.print(temp.value+"->");//
             temp = temp.next;//
         }
-        if(temp == null){
+        if(temp == null){//
             System.out.println("END");
         }
     }
