cef4bd9aba140f1782cbd03802cbbf8ede7fb61b
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-11-18 22:55:33 +0530

    Update WLL16.java

--- Java/src/JavaLinkedList/WLL16.java
+++ Java/src/JavaLinkedList/WLL16.java
@@ -23,7 +23,7 @@ public class WLL16 {
     }
     public void display(){//
         Node temp = head;//
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {//
             System.out.print(temp.value+"->");
             temp = temp.next;
         }
