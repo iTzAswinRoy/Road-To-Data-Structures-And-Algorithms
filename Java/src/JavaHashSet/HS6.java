4cb8fe6265cd9a4c3c081f245a4d24ea54c3318c
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-11-19 22:35:01 +0530

    Update HS6.java

--- Java/src/JavaHashSet/HS6.java
+++ Java/src/JavaHashSet/HS6.java
@@ -11,7 +11,7 @@ public class HS6 {//
         int[] array = {1,5,2,7,2,5,1};//
         System.out.println("Given array:\n"+ Arrays.toString(array));//
 
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) {//
             set.add(array[i]);//
         }
         System.out.println("\nConverting arrays into hash map:\n"+set);//
