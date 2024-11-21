027f181a88f3d687966f7db13e9a3a3c125d6733
iTzAswinRoy <aswinroy255@gmail.com>
2024-11-21 22:03:50 +0530

    Linked List

--- Java/src/JavaLinkedList/WLL11.java
+++ Java/src/JavaLinkedList/WLL11.java
@@ -47,7 +47,7 @@ public class WLL11 {
         }
 
         Node temp = head;       // Creating a 'temp' node pointing at head
-        for (int i = 0; i < size; i++) {        // i
+        for (int i = 0; i < size; i++) {        // Iterating through the linked list
             if (i == index) {
                 System.out.println("The element " + temp.value + " is at position " + i + " in the linked list.");
                 break;
