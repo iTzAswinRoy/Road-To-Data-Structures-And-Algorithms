b87ee88d235377b09f468c25d13f9345e0366eb6
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-11-22 22:36:38 +0530

    Update HS7.java

--- Java/src/JavaHashSet/HS7.java
+++ Java/src/JavaHashSet/HS7.java
@@ -6,7 +6,7 @@ import java.util.HashSet;//
 // Write a program to find the intersection of two HashSets (i.e., common elements).
 public class HS7 {
     public static void main(String[] args) {//
-        HS7 obj = new HS7();
+        HS7 obj = new HS7();//
         HashSet<Integer> set1 = new HashSet<>();
 
         set1.add(1);
