ecd1502557d4e138a98336f25b12dad35e611c25
iTzAswinRoy <aswinroy255@gmail.com>
2024-11-25 19:55:51 +0530

    Linked List

--- Java/src/JavaLinkedList/WLL15.java
+++ Java/src/JavaLinkedList/WLL15.java
@@ -10,35 +10,32 @@ public class WLL15 {
         this.size = 0;         // Initializing the size to 0
     }
     public class Node {         // Creating a 'Node' class
-        int value;
-        Node next;
-        Node(int value){
+        // Creating instance variables for the class
+        private int value;      // Creating an instance variable to store a value of the node
+        private Node next;      // Creating a reference variable, pointing towards the next node
+
+        Node(int value) {        // Creating a parameter constructor by passing the value
+            // Initializing values to the instance variable
             this.value = value;
         }
     }
 
-    public void insertNode(int value){
-        Node newNode = new Node(value);
-        if(tail == null){
-            newNode.next = head;
-            head = newNode;
-            tail = head;
-        }else {
-            tail.next = newNode;
-            tail = newNode;
+    public void insertNode(int value) {         // Method to insert node in the linked list
+        Node newNode = new Node(value);         // Creating a node by passing the value
+
+        // Checking if the tail is null, then the linked list is empty
+        if (tail == null) {
+            // Updating the head
+            newNode.next = head;        // Assigning the new node's next pointer to 'head'
+            head = newNode;         // Updating the head as new node
+            tail = head;        // Updating the head as tail
+        } else {
+            tail.next = newNode;        // Assigning the tail's next pointer to new node
+            tail = newNode;         // Assigning the tail's next pointer to new node
         }
-        size++;
+        size++;         // Incrementing size by 1
     }
-    public void display(){
-        Node temp = head;
-        for (int i = 0; i < size; i++) {
-            System.out.print(temp.value+"->");
-            temp = temp.next;
-        }
-        if(temp == null){
-            System.out.println("END");
-        }
-    }
+
     public void swapNodes(int index1, int index2){
         Node prev1 = null;
         Node curr1 = head;
@@ -70,6 +67,21 @@ public class WLL15 {
         curr1.next = curr2.next;
         curr2.next = temp;
     }
+
+    public void display() {         // Method to display the linked list
+        Node temp = head;       // Creating a 'temp' node pointing at head
+
+        while (temp != null) {      // Iterating the linked list till the 'temp' reaches null
+            System.out.print(temp.value + " -> ");      // Displaying the current node
+            temp = temp.next;       // Moving the pointer to the next node
+
+            // Checking if the 'temp' reaches the end
+            if(temp == null) {
+                System.out.println("END");      // Displaying 'END' for reference
+            }
+        }
+    }
+
     public static void main(String[] args) {
         WLL15 linkedList = new WLL15();
         linkedList.insertNode(1);
