7a27a05ff148e40aa47b65931711c312446348ef
iTzAswinRoy <aswinroy255@gmail.com>
2024-11-25 19:49:54 +0530

    Linked List

--- Java/src/JavaLinkedList/FindingElementsPosition.java
+++ Java/src/JavaLinkedList/FindingElementsPosition.java
@@ -1,20 +1,20 @@
 package JavaLinkedList;
 
 // Write a Java program to display elements and their positions in a linked list.
-public class WLL11 {
+public class FindingElementsPosition {
     // Creating instance variables for the class
-    int size;       // Creating a variable to store size
-    Node head;      // Crating a reference variable 'head' of class Node type
-    Node tail;      // Crating a reference variable 'tail' of class Node type
+    private int size;       // Creating a variable to store size
+    private Node head;      // Crating a reference variable 'head' of class Node type
+    private Node tail;      // Crating a reference variable 'tail' of class Node type
 
-    WLL11() {       // Creating a default constructor
+    FindingElementsPosition() {       // Creating a default constructor
         this.size = 0;      // Initializing the size to 0
     }
 
     public class Node {       // Creating a class for node
         // Creating instance variables for the class
-        int value;      // Creating an instance variable to store a value of the node
-        Node next;      // Creating a reference variable, pointing towards the next node
+        private int value;      // Creating an instance variable to store a value of the node
+        private Node next;      // Creating a reference variable, pointing towards the next node
 
         Node(int value) {       // Creating a parameter constructor by passing the value
             // Initializing values to the instance variable
@@ -39,22 +39,18 @@ public class WLL11 {
         size++;      // Incrementing size by 1
     }
 
-    public void elementPosition(int index) {        // Method to find the position of the node
-        // Checking if the index is out of bound or not
-        if (index > size) {
-            System.out.println("Index out of bound!");      // Displaying the error message
-            return;         // Exits the method
-        }
-
+    public void findingPosition(int element) {        // Method to find the position of the node
         Node temp = head;       // Creating a 'temp' node pointing at head
+
         for (int i = 0; i < size; i++) {        // Iterating through the linked list
             // Checking if the required index
-            if (i == index) {
-                System.out.println("The element " + temp.value + " is at position " + i + " in the linked list.");      // Displaying the elements positions
-                break;      // Exits the loop
+            if (temp.value == element) {
+                System.out.println("The element " + element + " is at position " + i + " in the linked list.");      // Displaying the elements positions
+                return;      // Exits the method
             }
-            temp = temp.next;
+            temp = temp.next;       // Moving the 'temp' pointer to the next node
         }
+        System.err.println("Element not found!");      // Displaying the error message
     }
 
     public void display() {         // Method to display the linked list
@@ -73,14 +69,14 @@ public class WLL11 {
 
     public static void main(String[] args) {        // Main method
         // Creating an object to store the linked list
-        WLL11 linkedList = new WLL11();
+        FindingElementsPosition linkedList = new FindingElementsPosition();
 
         // Calling the method to insert the node in the linked list
-        linkedList.insertNode(1);
-        linkedList.insertNode(2);
-        linkedList.insertNode(3);
-        linkedList.insertNode(4);
-        linkedList.insertNode(5);
+        linkedList.insertNode(11);
+        linkedList.insertNode(22);
+        linkedList.insertNode(33);
+        linkedList.insertNode(44);
+        linkedList.insertNode(55);
 
         // Calling the method to display the linked list
         System.out.println("Original linked list:");
@@ -88,12 +84,6 @@ public class WLL11 {
 
         // Calling the method to find the elements position
         System.out.println("\nFinding elements position:");
-
-
-
-
-
-
-        linkedList.elementPosition(2);          // rewrite
+        linkedList.findingPosition(33);
     }
 }
