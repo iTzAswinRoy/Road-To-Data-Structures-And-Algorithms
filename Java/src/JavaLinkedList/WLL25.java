0440b6eee4f1bc17898b9a523f82cca5dcefe8f8
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-11-27 22:52:14 +0530

    Update WLL25.java

--- Java/src/JavaLinkedList/WLL25.java
+++ Java/src/JavaLinkedList/WLL25.java
@@ -8,7 +8,7 @@ public class WLL25 {
         this.size = 0;//
     }
     public void insertNode(int value){
-        Node newNode = new Node(value);
+        Node newNode = new Node(value);//
         if(tail == null){
             newNode.next = head;//
             head = newNode;
