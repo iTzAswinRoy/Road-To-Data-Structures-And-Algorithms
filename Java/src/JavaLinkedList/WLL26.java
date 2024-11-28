2afaa6a0aff2c56753c26f5e42c70429aba53d3c
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-11-28 21:47:02 +0530

    Update WLL26.java

--- Java/src/JavaLinkedList/WLL26.java
+++ Java/src/JavaLinkedList/WLL26.java
@@ -10,7 +10,7 @@ public class WLL26 {
     public void insertNode(int value){
         Node newNode = new Node(value);//
         if(tail == null){
-            newNode.next = head;
+            newNode.next = head;//
             head = newNode;
             tail =head;
         } else {
