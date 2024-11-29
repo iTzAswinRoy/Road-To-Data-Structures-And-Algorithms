312cc880fd2226a68103e1b141d6897dc6d587a3
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-11-29 22:32:31 +0530

    Update WLL23.java

--- Java/src/JavaLinkedList/WLL23.java
+++ Java/src/JavaLinkedList/WLL23.java
@@ -13,7 +13,7 @@ public class WLL23 {
         if(tail == null){//
             newNode.next = head;//
             head = newNode;
-            tail = head;
+            tail = head;//
         } else {
             tail.next = newNode;
             tail = newNode;
