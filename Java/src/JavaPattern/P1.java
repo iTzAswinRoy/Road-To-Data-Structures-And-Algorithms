4572808860a7f86cc7af5337ae0f8eb36d13cc16
iTzAswinRoy <aswinroy255@gmail.com>
2024-12-06 22:46:45 +0530

    Pattern Printing

--- Java/src/JavaPattern/P1.java
+++ Java/src/JavaPattern/P1.java
@@ -1,9 +1,10 @@
 package JavaPattern;
 
 // Print a n x n square using asterisks (*).
-public class P1 {//
+public class P1 {
     public static void main(String[] args) {        // Main method
         int n = 5;      // Initializing as 5
+
         // Creating a nested for loop
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
