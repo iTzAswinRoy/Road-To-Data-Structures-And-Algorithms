91d439e174e113234528bc8ddbeb3877c64778f1
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-12-08 22:48:43 +0530

    Update P6.java

--- Java/src/JavaPattern/P6.java
+++ Java/src/JavaPattern/P6.java
@@ -20,7 +20,7 @@ public class P6 {
 
         for (int i = 0; i < n; i++) {//
 
-            for (int j = 0; j < i; j++) {//
+            for (int j = 0; j < i; j++) {
                 System.out.print(" ");//
             }
 
