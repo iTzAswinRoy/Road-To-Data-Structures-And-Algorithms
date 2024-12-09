a074c13e4fea948b555cc97eb430f521940ab81a
iTzAswinRoy <aswinroy255@gmail.com>
2024-12-09 21:56:30 +0530

    Pattern Printing

--- Java/src/JavaPattern/P7.java
+++ Java/src/JavaPattern/P7.java
@@ -2,19 +2,21 @@ package JavaPattern;
 
 // Print a hollow square of n x n dimensions using asterisks (*).
 public class P7 {
-    public static void main(String[] args) {
-        int n = 5;
-        for (int i = 0; i < n; i++) {
+    public static void main(String[] args) {        // Main method
+        int n = 5;      // Initializing 'n' as 5
 
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n; i++) {       // Outer loop for each row
 
-                if(i == 0 || i == n-1 ||  j == 0 || j == n-1){
-                    System.out.print(" *");
+            for (int j = 0; j < n; j++) {       // Inner loop for each column
+
+                // Checking if it's the first or last row, or the first or last column
+                if (i == 0 || i == n-1 ||  j == 0 || j == n-1) {
+                    System.out.print(" *");          // Print '*' for boundary cells
                 } else {
-                    System.out.print("  ");
+                    System.out.print("  ");          // Print '*' for boundary cells
                 }
             }
-            System.out.println();
+            System.out.println();       // Move to the next line after printing each row
         }
     }
 }
