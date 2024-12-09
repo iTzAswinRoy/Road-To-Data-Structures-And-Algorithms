f4ea281deed9f8dee54076cb9de402d132d53097
iTzAswinRoy <aswinroy255@gmail.com>
2024-12-09 21:56:01 +0530

    Pattern Printing

--- Java/src/JavaPattern/P3.java
+++ Java/src/JavaPattern/P3.java
@@ -3,10 +3,11 @@ package JavaPattern;
 // Print an inverted right-angled triangle with n rows using asterisks (*).
 public class P3 {
     public static void main(String[] args) {        // Main method
-        int n = 5;          // Initializing as 5
+        int n = 5;         // Initializing 'n' as 5
 
         // Creating a nested for loop
         for (int i = 0; i < n; i++) {
+
             for (int j = i; j < n; j++) {           // Iterating the loop starting from the 5 to 0
                 System.out.print("*");          // Printing '*' for each line
             }
