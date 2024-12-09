b5847f4ed74b68b3b887f50e4d46543b289c4690
iTzAswinRoy <aswinroy255@gmail.com>
2024-12-09 21:56:39 +0530

    Pattern Printing

--- Java/src/JavaPattern/P9.java
+++ Java/src/JavaPattern/P9.java
@@ -1,18 +1,20 @@
 package JavaPattern;
 
 //Print Floyd’s triangle of `n` rows. Floyd's triangle is a triangular array of natural numbers.
- public class P9 {
-    public static void main(String[] args) {
-        int n = 5;
-        int count = 1;
+public class P9 {
+    public static void main(String[] args) {        // Main method
+        int n = 5;      // Initializing 'n' as 5
+        int count = 1;      // Creating a variable to track the number to print in each row
+
+        // Outer loop for each row
         for (int i = 1; i <= n; i++) {
 
-            for (int j = n-i ; j < n; j++) {
-                System.out.print(" "+count);
-                count++;
+            // Inner loop for printing the numbers in the current row
+            for (int j = n-i ; j < n; j++) {        // Determines the number of elements in the row
+                System.out.print(" "+count);        // Print the current number with a space
+                count++;        // Increment count by 1
             }
-            System.out.println();
+            System.out.println();       // Move to the next line after completing the current row
         }
     }
 }
-
