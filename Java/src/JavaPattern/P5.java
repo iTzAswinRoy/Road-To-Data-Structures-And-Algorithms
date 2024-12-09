47f0eb70cd234ee5f8d0087c2698ebcc02c96488
iTzAswinRoy <aswinroy255@gmail.com>
2024-12-09 21:56:11 +0530

    Pattern Printing

--- Java/src/JavaPattern/P5.java
+++ Java/src/JavaPattern/P5.java
@@ -2,19 +2,19 @@ package JavaPattern;
 
 // Print an inverted pyramid with n levels using asterisks (*).
 public class P5 {
-    public static void main(String[] args) {
-        int n = 5;
+    public static void main(String[] args) {         // Main method
+        int n = 5;          // Initializing 'n' as 5
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {       // Outer loop: Controls the rows
 
-            for (int j = 0; j < i; j++) {
+            for (int j = 0; j < i; j++) {       // Inner loop 1: Prints spaces for alignment
                 System.out.print(" ");
             }
 
-            for (int k = i; k < n; k++) {
+            for (int k = i; k < n; k++) {       // Inner loop 2: Prints '*' for the current row
                 System.out.print(" *");
             }
-            System.out.println();
+            System.out.println();       // Move to the next line after a row is printed
         }
     }
 }
