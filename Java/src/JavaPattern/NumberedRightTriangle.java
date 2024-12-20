ef23a036c0b3a843f836b5321f224be6e9ad1729
iTzAswinRoy <aswinroy255@gmail.com>
2024-12-20 20:52:30 +0530

    Pattern

--- Java/src/JavaPattern/NumberedRightTriangle.java
+++ Java/src/JavaPattern/NumberedRightTriangle.java
@@ -1,7 +1,7 @@
 package JavaPattern;
 
 // Print a right-angled triangle where each row contains increasing numbers starting from 1.
-public class P8 {
+public class NumberedRightTriangle {
     public static void main(String[] args) {        // Main method
         int n = 5;      // Initializing 'n' as 5
         int count = 1;      // Creating a variable to track the number to print in each row
