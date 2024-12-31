d878d326088e2a03b653f5f1da85e8a97371762d
iTzAswinRoy <aswinroy255@gmail.com>
2024-12-31 22:46:00 +0530

    Stack

--- Java/src/JavaStack/WS5.java
+++ Java/src/JavaStack/WS5.java
@@ -1,20 +1,25 @@
 package JavaStack;
 //  Write a Java program to find the maximum and minimum elements in a stack.
-public class WS5 {
-    int[] arr;
-    int top;
-    WS5(int size) {
-        arr= new int[size];
-        top = -1;
+public class WS5 {      // Creating a class for stack
+    // Creating instance variables for the class
+    private int[] arr;      // Creating a reference variable of an array
+    private int top;        // Creating a reference variable top
+
+    WS5 (int size) {          // Creating a parameterized constructor by passing a value
+        // Initializing the reference variable
+        arr = new int[size];        // Here, we're creating an array object whenever an object is created
+        top = -1;       // Initializing the top to -1, which indicates the stack size is empty
     }
-    public void push(int num){
-        if(top == arr.length-1){
-            System.out.println("Stack is full");
+
+    public void push(int num) {      // Method to add element into the stack
+        if (top == arr.length - 1) {        // Checking if the stack is full
+            System.out.println("Stack is full");//
         } else {
-            top++;
-            arr[top] = num;
+            top++;      // Incrementing top by 1
+            arr[top] = num;     // Adding the value to the stack
         }
     }
+
     public void pop(){
         if(top == -1){
             System.out.println("Stack is empty");
