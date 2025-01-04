b49ca7cfd5aaab8031a186bf1fe7fbe71476701b
iTzAswinRoy <aswinroy255@gmail.com>
2025-01-04 16:16:29 +0530

    ArrayList

--- Java/src/JavaArrayList/RetrieveElement.java
+++ Java/src/JavaArrayList/RetrieveElement.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;     // Importing array list class
 import java.util.Scanner;       // Importing scanner class
 
 // Write a Java program to retrieve an element (at a specified index) from a given array list.
+
 public class RetrieveElement {
     public static void main(String[] args) {    // Main method
         RetrieveElement obj = new RetrieveElement();    // Creating an object
