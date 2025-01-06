5667b3d50a8b6dc479eccec16918f611e35e4f9b
iTzAswinRoy <aswinroy255@gmail.com>
2025-01-06 21:23:23 +0530

    Recursion

--- Java/src/JavaRecursion/ArrayList/CheckSorted.java
+++ Java/src/JavaRecursion/ArrayList/CheckSorted.java
@@ -1,10 +1,48 @@
-package JavaRecursion.ArrayList;//
+package JavaRecursion.ArrayList;
 
-import java.util.ArrayList;//
+import java.util.ArrayList;         // Importing Array list class
 
 // Write a recursive function to check if an ArrayList of integers is sorted in ascending order.
-public class CheckSorted {//
-    public static void main(String[] args) {//
+public class CheckSorted {
+    public ArrayList<Integer> sortingList(ArrayList<Integer> data, int index) {         // Method to sort the list in ascending order
+        // Base case
+        if(index == data.size()) {      // Checking if the index reaches the size of the list
+            return data;        // Returning the array list
+        }
+
+        boolean isSwapped = false;      // Creating a check to avoid unnecessary running of for loop
+
+        for (int i = 0; i < data.size() - index - 1; i++) {
+            if (data.get(i) > data.get(i + 1)) {
+                swap(data, i, i + 1);
+                isSwapped = true;
+            }
+        }
+
+        if (!isSwapped) {      // If no swaps were made after a recursive call, the array is sorted
+            return data;
+        }
+        return sortingList(data,index + 1);
+    }
+
+    public void swap(ArrayList<Integer> data, int i, int j) {
+        int temp = data.get(i);
+        data.set(i, data.get(j));
+        data.set(j, temp);
+    }
+
+    public boolean isSorted(ArrayList<Integer> data, int index) {
+        if (index == data.size() - 1 || data.isEmpty()) {
+            return true;
+        }
+
+        if(data.get(index) > data.get(index + 1)) {
+            return false;
+        }
+        return isSorted(data, index + 1);
+    }
+
+    public static void main(String[] args) {        // Main method
         CheckSorted obj = new CheckSorted();//
 
         ArrayList<Integer> list = new ArrayList<>();
@@ -15,42 +53,7 @@ public class CheckSorted {//
         list.add(3);//
 
         System.out.println("Given array list:\n" + list);
-        System.out.println("\nSorting the array list:\n"+obj.sortingList(list, 0));
+        System.out.println("\nSorting the array list:\n" + obj.sortingList(list, 0));
         System.out.println("\nChecking if the array list are sorted in order.\n" + obj.isSorted(list, 0));
-    }
-
-    public ArrayList<Integer> sortingList(ArrayList<Integer> data, int index){
-        if(index == data.size()){
-            return data;
-        }
-        boolean isSwapped = false;      // Creating a check to avoid unnecessary running of for loop
-
-        for (int i = 0; i < data.size()-index-1; i++) {
-            if(data.get(i) > data.get(i+1)) {
-                swap(data, i, i + 1);
-                isSwapped = true;
-            }
-        }
-        if(!isSwapped){      // If no swaps were made after a recursive call, the array is sorted
-            return data;
-        }
-        return sortingList(data,index+1);
-    }
-
-    public void swap(ArrayList<Integer> data, int i, int j){
-        int temp = data.get(i);
-        data.set(i, data.get(j));
-        data.set(j, temp);
-    }
-
-    public boolean isSorted(ArrayList<Integer> data, int index){
-        if (index == data.size() - 1 || data.isEmpty()) {
-            return true;
-        }
-        if(data.get(index) > data.get(index+1)){
-            return false;
-        }
-
-        return isSorted(data, index+1);
     }
 }
