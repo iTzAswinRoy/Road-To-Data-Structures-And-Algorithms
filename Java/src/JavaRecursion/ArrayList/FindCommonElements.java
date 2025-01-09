0f2c86aafb305828fffea5be3935bb9ba621600e
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2025-01-09 23:42:56 +0530

    Update FindCommonElements.java

--- Java/src/JavaRecursion/ArrayList/FindCommonElements.java
+++ Java/src/JavaRecursion/ArrayList/FindCommonElements.java
@@ -10,7 +10,7 @@ public class FindCommonElements {
         ArrayList<Integer> list1 = new ArrayList<>();//
         list1.add(1);//
         list1.add(2);//
-        list1.add(3);
+        list1.add(3);//
         list1.add(4);
         list1.add(5);
 
