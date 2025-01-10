1835e6683f6fc9abffb5579bed3d2d48fb4e6def
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2025-01-10 21:13:57 +0530

    Update WS14.java

--- Java/src/JavaStack/WS14.java
+++ Java/src/JavaStack/WS14.java
@@ -5,7 +5,7 @@ public class WS14 {
     int top;//
     WS14(int size) {//
         arr = new int[size];//
-        top = -1;
+        top = -1;//
     }
     public void push(int num) {
         if (top == arr.length - 1) {
