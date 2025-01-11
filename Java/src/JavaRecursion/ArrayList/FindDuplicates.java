07c017dc6138b6805197dd091214df61f16bd52f
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2025-01-11 23:42:39 +0530

    Update FindDuplicates.java

--- Java/src/JavaRecursion/ArrayList/FindDuplicates.java
+++ Java/src/JavaRecursion/ArrayList/FindDuplicates.java
@@ -13,7 +13,7 @@ public class FindDuplicates {//
         list.add(5);//
         list.add(1);//
         list.add(2);//
-        list.add(5);
+        list.add(5);//
 
         System.out.println("Given array list:\n" + list);
         System.out.println("\nFinding duplicate elements in the array list:\n" + obj.duplicateElements(list, 0, new ArrayList<>()));
