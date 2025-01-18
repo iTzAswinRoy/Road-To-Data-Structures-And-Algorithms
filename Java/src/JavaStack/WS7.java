f8d95399d451f157eadb29bf1e17c6eb948dc45e
iTzAswinRoy <aswinroy255@gmail.com>
2025-01-18 22:13:07 +0530

    Stack

--- Java/src/JavaStack/WS7.java
+++ Java/src/JavaStack/WS7.java
@@ -1,72 +1,89 @@
 package JavaStack;
+
 // Write a Java program to count all stack elements.
-public class WS7 {
-    int[] arr;
-    int top;
 
-    WS7(int size) {
-        arr = new int[size];
-        top = -1;
+public class WS7 {      // Creating a class for stack
+    // Creating instance variables for the class
+    private int[] arr;      // Creating a reference variable of an array
+    private int top;        // Creating a reference variable top
+
+    WS7 (int size) {          // Creating a parameterized constructor by passing a value
+        // Initializing the reference variable
+        arr = new int[size];        // Here, we're creating an array object whenever an object is created
+        top = -1;       // Initializing the top to -1, which indicates the stack size is empty
     }
 
-    public void push(int num) {
-        if (top == arr.length - 1) {
+    public void push(int num) {      // Method to add element into the stack
+        if (top == arr.length - 1) {        // Checking if the stack is full
             System.out.println("Stack is full");
+
         } else {
-            top++;
-            arr[top] = num;
+            top++;      // Incrementing top by 1
+            arr[top] = num;     // Adding the value to the stack
         }
     }
-    public void pop() {
-        if (top == -1) {
+
+    public int pop() {      // Method to remove element from the stack
+        if (top == -1) {        // Checking if the stack is empty
             System.out.println("Stack is empty");
+            return 0;       // Returns 0, if empty
+
         } else {
-            top--;
+            System.out.println("\nPopping element from the stack:");
+
+            int popElement = arr[top];      // Accessing the last element from the stack
+            top--;      // Decrementing top by 1
+
+            return popElement;      // Returning the popped element
         }
     }
-    public void peek() {
-        if (top == -1) {
-            System.out.println("Stack is empty");
-        } else {
-            System.out.println(arr[top]);
+
+    public int peek() {        // Method to get the last element from the stack
+        if (top == -1) {        // Checking if the stack is empty
+            System.out.println("stack is empty");
+            return 0;
         }
+        System.out.println("\nPeeking the last element from the stack:");
+        return arr[top];        // Restringing the last element
     }
-    public void display() {
+
+    public int countElements() {        // Method to count all elements in the stack
+        int count = 0;      // Creating a variable to store count
+
+        // checking if the chat is empty
         if (top == -1) {
-            System.out.println("Stack is empty");
+            return count;       // Returns the count
         } else {
+            // Iterating through the stack
             for (int i = top; i >= 0; i--) {
-                System.out.print(arr[i]+" ");
+                count++;      // Incrementing count by 1
             }
         }
-        System.out.println();
+        return count;       // Retuning the count
     }
-    public boolean isEmpty() {
-        if (top == -1) {
-            return true;
+
+    public void display() {         // Method to display elements int the stack
+        // Iterating each element from end to beginning
+        for (int i = top; i >= 0; i--) {
+            System.out.println(arr[i]);     // Displaying the element
         }
-        return false;
     }
-    public int countElements(){
-        int count = 0;
-        if(top == -1){
-            return count;
-        } else {
-            for (int i = top; i >= 0; i--) {
-                count++;
-            }
-        }
-        return count;
-    }
-    public static void main(String[] args) {
-        WS7 obj = new WS7(5);
-        System.out.println("Given stack:");
+
+    public static void main(String[] args) {        // Main method
+        WS7 obj = new WS7(5);       // Creating an object of the class by passing a value
+
+        // Calling the method to insert elements into the stack
         obj.push(1);
         obj.push(2);
         obj.push(3);
         obj.push(4);
         obj.push(5);
+
+        // Displaying all the elements in the stack
+        System.out.println("Given stack:");
         obj.display();
-        System.out.println("Number of elements in the stack is "+ obj.countElements());
+
+        // Calling the method to count the elements in the stack
+        System.out.println("\nNumber of elements in the stack is " + obj.countElements());
     }
 }
