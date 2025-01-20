b8a378796b5ec43269bbd6dafe17908f0c11f8c5
iTzAswinRoy <aswinroy255@gmail.com>
2025-01-20 23:21:03 +0530

    Stack

--- Java/src/JavaStack/WS21.java
+++ Java/src/JavaStack/WS21.java
@@ -1,82 +1,102 @@
 package JavaStack;
+
 // Write a Java program that implements a stack and finds common elements between two stacks.
-public class WS21 {
-    int[] arr;
-    int top;
-    WS21(int size){
-        arr = new int[size];
-        top = -1;
+
+public class WS21 {      // Creating a class for stack
+    // Creating instance variables for the class
+    private int[] arr;      // Creating a reference variable of an array
+    private int top;        // Creating a reference variable top
+
+    WS21 (int size) {          // Creating a parameterized constructor by passing a value
+        // Initializing the reference variable
+        arr = new int[size];        // Here, we're creating an array object whenever an object is created
+        top = -1;       // Initializing the top to -1, which indicates the stack size is empty
     }
-    public void push(int num){
-        if(top == arr.length - 1){
+
+    public void push(int num) {      // Method to add element into the stack
+        if (top == arr.length - 1) {        // Checking if the stack is full
             System.out.println("Stack is full");
+
         } else {
-            top++;
-            arr[top] = num;
+            top++;      // Incrementing top by 1
+            arr[top] = num;     // Adding the value to the stack
         }
     }
-    public void pop(){
-        if(top == -1){
+
+    public int pop() {      // Method to remove element from the stack
+        if (top == -1) {        // Checking if the stack is empty
             System.out.println("Stack is empty");
+            return 0;       // Returns 0, if empty
+
         } else {
-            top--;
+            int popElement = arr[top];      // Accessing the last element from the stack
+            top--;      // Decrementing top by 1
+
+            return popElement;      // Returning the popped element
         }
     }
-    public void peek(){
-        if( top == -1 ){
-            System.out.println("Stack is empty");
-        } else {
-            System.out.println(arr[top]);
+
+    public int peek() {        // Method to get the last element from the stack
+        if (top == -1) {        // Checking if the stack is empty
+            System.out.println("stack is empty");
+            return 0;
         }
+        System.out.println("\nPeeking the last element from the stack:");
+        return arr[top];        // Restringing the last element
     }
-    public boolean isEmpty(){
-        if(top == -1){
-            return true;
-        }
-        return false;
-    }
-    public void display(){
-        if( top == -1 ){
-            System.out.println("Stack is empty");
-        } else {
-            for (int i = top; i >= 0 ; i--) {
-                System.out.print(arr[i]+" ");
-            }
-        }
-        System.out.println("\n");
-    }
-    static void commonElement(WS21 obj1, WS21 obj2) {
-        WS21 newStack = new WS21(Math.min(obj1.arr.length, obj2.arr.length));
+
+    public static void commonElement(WS21 obj1, WS21 obj2) {          // Method to get the common elements from the both the stack
+        WS21 newStack = new WS21(Math.min(obj1.arr.length, obj2.arr.length));           // Creating a stack to get store the common elements
+
+        // Using nested for loop
         for (int i = obj1.top; i >= 0; i--) {
             for (int j = obj2.top; j >= 0; j--) {
+
+                // Checking if the element is common
                 if (obj1.arr[i] == obj2.arr[j]) {
-                    newStack.push(obj1.arr[i]);
+                    newStack.push(obj1.arr[i]);         // Pushing the element which are common to the new stack
                 }
             }
         }
-        newStack.display();
+        newStack.display();         // Calling the method to display the elements in new stack
     }
-    public static void main(String[] args) {
-        WS21 obj1 = new WS21(5);
-        System.out.println("Stack - 1:");
+
+    public void display() {         // Method to display elements int the stack
+        // Iterating each element from end to beginning
+        for (int i = top; i >= 0; i--) {
+            System.out.println(arr[i]);     // Displaying the element
+        }
+    }
+
+    public static void main(String[] args) {        // Main method
+        WS21 obj1 = new WS21(5);        // Creating a new stack by passing a value
+
+        // Calling the method to insert elements into the stack
         obj1.push(1);
         obj1.push(2);
         obj1.push(3);
         obj1.push(4);
         obj1.push(5);
+
+        // Displaying all the elements in the stack
+        System.out.println("Stack - 1:");
         obj1.display();
 
-        System.out.println("Stack - 2:");
-        WS21 obj2 = new WS21(5);
-        obj2.push(2);
-        obj2.push(4);
-        obj2.push(3);
-        obj2.push(1);
-        obj2.push(4);
+        WS21 obj2 = new WS21(5);            // Creating a new stack by passing a value
 
+        // Calling the method to insert elements into the stack
+        obj2.push(3);
+        obj2.push(6);
+        obj2.push(9);
+        obj2.push(1);
+        obj2.push(2);
+
+        // Displaying all the elements in the stack
+        System.out.println("\nStack - 2:");
         obj2.display();
 
-        System.out.println("Common elements from both the stacks are... ");
+        // Calling the method to get the common elements from the both the stack
+        System.out.println("\nCommon elements from both the stacks are... ");
         commonElement(obj1, obj2);
     }
 }
