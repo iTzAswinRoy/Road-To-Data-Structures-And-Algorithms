1b3d6e539099dd5b343185f121402fb79904fd5e
iTzAswinRoy <aswinroy255@gmail.com>
2025-01-20 23:25:42 +0530

    Stack

--- Java/src/JavaStack/RemoveStack.java
+++ Java/src/JavaStack/RemoveStack.java
@@ -2,12 +2,12 @@ package JavaStack;
 
 // Write a Java program to remove all elements from a stack.
 
-public class WS6 {      // Creating a class for stack
+public class RemoveStack {      // Creating a class for stack
     // Creating instance variables for the class
     private int[] arr;      // Creating a reference variable of an array
     private int top;        // Creating a reference variable top
 
-    WS6 (int size) {          // Creating a parameterized constructor by passing a value
+    RemoveStack(int size) {          // Creating a parameterized constructor by passing a value
         // Initializing the reference variable
         arr = new int[size];        // Here, we're creating an array object whenever an object is created
         top = -1;       // Initializing the top to -1, which indicates the stack size is empty
@@ -70,7 +70,7 @@ public class WS6 {      // Creating a class for stack
     }
 
     public static void main(String[] args) {        // Main method
-        WS6 obj = new WS6(5);       // Creating an object of the class by passing a value
+        RemoveStack obj = new RemoveStack(5);       // Creating an object of the class by passing a value
 
         // Calling the method to insert elements into the stack
         obj.push(1);
