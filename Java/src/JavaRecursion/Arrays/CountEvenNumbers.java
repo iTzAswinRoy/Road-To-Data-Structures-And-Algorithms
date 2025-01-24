133f07c92b7a982b7f32f2600b2fa6297e262fc9
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2025-01-24 11:49:33 +0530

    Update CountEvenNumbers.java

--- Java/src/JavaRecursion/Arrays/CountEvenNumbers.java
+++ Java/src/JavaRecursion/Arrays/CountEvenNumbers.java
@@ -8,7 +8,7 @@ public class CountEvenNumbers {
         CountEvenNumbers obj = new CountEvenNumbers();
 
         int[] array = {2,12,7,10,99};//
-        System.out.println("Given array:\n"+ Arrays.toString(array));
+        System.out.println("Given array:\n"+ Arrays.toString(array));//
         System.out.println("\nProduct of the array:\n"+obj.countEven(array, 0, 0));
 
     }
