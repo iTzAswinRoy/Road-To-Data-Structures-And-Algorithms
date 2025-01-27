a8e9f1729b85b013eda83c72177511485e1ec015
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2025-01-27 22:43:57 +0530

    Update FindingSecondMaximum.java

--- Java/src/JavaRecursion/Arrays/FindingSecondMaximum.java
+++ Java/src/JavaRecursion/Arrays/FindingSecondMaximum.java
@@ -20,7 +20,7 @@ public class FindingSecondMaximum {
         }
 
         if (data[length - 1] > max) {
-            max = data[length - 1];
+            max = data[length - 1];//
         }
         return findMax(data, length - 1, max);
     }
