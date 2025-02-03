e38e72986efe4668e973d14e7197613082be312f
iTzAswinRoy <aswinroy255@gmail.com>
2025-02-03 21:08:00 +0530

    Stack

--- Java/src/JavaStack/WS26.java
+++ Java/src/JavaStack/WS26.java
@@ -1,72 +1,95 @@
 package JavaStack;
 // Write a Java program that implements a stack and checks if all elements of the stack satisfy a condition.
-public class WS26 {
-    int[] arr;
-    int top;
-    WS26(int size){
-        arr = new int[size];
-        top = -1;
+public class WS26 {      // Creating a class for stack
+    // Creating instance variables for the class
+    private int[] arr;      // Creating a reference variable of an array
+    private int top;        // Creating a reference variable top
+
+    WS26 (int size) {          // Creating a parameterized constructor by passing a value
+        // Initializing the reference variable
+        arr = new int[size];        // Here, we're creating an array object whenever an object is created
+        top = -1;       // Initializing the top to -1, which indicates the stack size is empty
     }
-    public void push(int num){
-        if(top == arr.length - 1){
+
+    public void push(int num) {      // Method to add element into the stack
+        if (top == arr.length - 1) {        // Checking if the stack is full
             System.out.println("Stack is full");
+
         } else {
-            top++;
-            arr[top] = num;
+            top++;      // Incrementing top by 1
+            arr[top] = num;     // Adding the value to the stack
         }
     }
-    public void pop(){
-        if(top == -1){
+
+    public int pop() {      // Method to remove element from the stack
+        if (top == -1) {        // Checking if the stack is empty
             System.out.println("Stack is empty");
+            return 0;       // Returns 0, if empty
+
         } else {
-            top--;
+            int popElement = arr[top];      // Accessing the last element from the stack
+            top--;      // Decrementing top by 1
+
+            return popElement;      // Returning the popped element
         }
     }
-    public void peek() {
-        if (top == -1) {
-            System.out.println("Stack is empty");
-        } else {
-            System.out.println(arr[top]);
+
+    public int peek() {        // Method to get the last element from the stack
+        if (top == -1) {        // Checking if the stack is empty
+            System.out.println("stack is empty");
+            return 0;
         }
+        System.out.println("\nPeeking the last element from the stack:");
+        return arr[top];        // Restringing the last element
     }
-    public boolean isEmpty(){
-        if(top == -1){
+
+    public boolean isEmpty() {      // Method to check if the stack is empty
+        if(top == -1) {
             return true;
-        }
-        return false;
-    }
-    public void display(){
-        if( top == -1 ){
-            System.out.println("Stack is empty");
         } else {
-            for (int i = top; i >= 0 ; i--) {
-                System.out.print(arr[i]+" ");
-            }
+            return false;
         }
-        System.out.println("\n");
     }
-    public boolean checkEven(){
-        if(isEmpty()){
+
+    public boolean checkEven() {        // Method to check if the elements inside the stack is even
+        // Checking if the stack is empty
+        if (isEmpty()) {
             System.out.println("Stack is empty");
+
         } else {
+            // Iterating through the for stack
             for (int i = top; i >= 0; i--) {
+                // Check if the element is even
                 if (arr[i] % 2 != 0) {
                     return false;
                 }
             }
         }
-        return true;
+        return true;        // Returns true, if the stack contains only even elements
     }
-    public static void main(String[] args) {
-        WS26 obj = new WS26(5);
-        System.out.println("Original stack :");
+
+    public void display() {         // Method to display elements int the stack
+        // Iterating each element from end to beginning
+        for (int i = top; i >= 0; i--) {
+            System.out.println(arr[i]);     // Displaying the element
+        }
+    }
+
+    public static void main(String[] args) {        // Main method
+        WS26 obj = new WS26(5);         // Creating an object of the class by passing a value
+
+        // Calling the method to insert elements into the stack
         obj.push(2);
         obj.push(4);
         obj.push(6);
         obj.push(8);
         obj.push(10);
-        obj.display();
-        System.out.println("Checking if all the elements in the stack are even: "+obj.checkEven());
 
+        // Displaying all the elements in the stack
+        System.out.println("Original stack :");
+        obj.display();
+
+        // Calling the method to check if the elements inside the stack is even
+        System.out.println("Checking if all the elements in the stack are even: " + obj.checkEven());
     }
 }
