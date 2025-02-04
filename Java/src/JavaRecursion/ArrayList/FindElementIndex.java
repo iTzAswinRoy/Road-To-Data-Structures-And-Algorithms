02a35be5551ba435a34cc63616cef568c3147156
iTzAswinRoy <aswinroy255@gmail.com>
2025-02-04 22:36:55 +0530

    Recursion using Array List

--- Java/src/JavaRecursion/ArrayList/FindElementIndex.java
+++ Java/src/JavaRecursion/ArrayList/FindElementIndex.java
@@ -11,7 +11,7 @@ public class FindElementIndex {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
-        list.add(3);//
+        list.add(3);
         list.add(4);//
         list.add(5);//
 
