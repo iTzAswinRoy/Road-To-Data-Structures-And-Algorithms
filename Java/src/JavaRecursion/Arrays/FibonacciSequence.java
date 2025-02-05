2dec7c451d32805ea28d12d1ba9ce5b3630ce07d
iTzAswinRoy <aswinroy255@gmail.com>
2025-02-05 13:33:03 +0530

    Recursion using Array

--- Java/src/JavaRecursion/Arrays/FibonacciSequence.java
+++ Java/src/JavaRecursion/Arrays/FibonacciSequence.java
@@ -2,19 +2,24 @@ package JavaRecursion.Arrays;
 
 // Implement a recursive function to find the n-th Fibonacci number.
 public class FibonacciSequence {
-    public static void main(String[] args) {
-        FibonacciSequence obj = new FibonacciSequence();
+    public static void main(String[] args) {        // Main method
+        FibonacciSequence obj = new FibonacciSequence();        // Creating an object of the class
 
-        int result = obj.findFibonacciNumber(5);
-        System.out.println(result);
+        int result = obj.findFibonacciNumber(5);            // Calling the method to find the fibonacci number
+        System.out.println(result);         // Displaying the result
     }
-    public int findFibonacciNumber(int num){
-        if(num == 0){
+
+    public int findFibonacciNumber(int num) {           // Method to find the fibonacci number
+        // Base case
+        if (num == 0) {         // Checking if the num is 0
             return 0;
         }
-        if(num == 1){
+
+        // Base case
+        if (num == 1) {         // Checking if the num is 1
             return 1;
         }
-        return findFibonacciNumber(num - 1) + findFibonacciNumber(num - 2);
+
+        return findFibonacciNumber(num - 1) + findFibonacciNumber(num - 2);         // Calling the recursive function by adding two recursive calls
     }
 }
