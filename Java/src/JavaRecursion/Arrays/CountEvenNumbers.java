66547de21f0075e06320dd7d8afa132ba60391a2
iTzAswinRoy <aswinroy255@gmail.com>
2025-02-05 13:32:51 +0530

    Recursion using Array List

--- Java/src/JavaRecursion/Arrays/CountEvenNumbers.java
+++ Java/src/JavaRecursion/Arrays/CountEvenNumbers.java
@@ -1,30 +1,34 @@
 package JavaRecursion.Arrays;
 
-import java.util.Arrays;//
+import java.util.Arrays;        // Importing the arrays class
 
 //Write a recursive function to count the number of even numbers in an array.
-public class CountEvenNumbers {//
-    public static void main(String[] args) {//
-        CountEvenNumbers obj = new CountEvenNumbers();
+public class CountEvenNumbers {
+    public static void main(String[] args) {            // Main method
+        CountEvenNumbers obj = new CountEvenNumbers();          // Creating an object of the class
 
-        int[] array = {2,12,7,10,99};//
-        System.out.println("Given array:\n"+ Arrays.toString(array));//
-        System.out.println("\nProduct of the array:\n"+obj.countEven(array, 0, 0));//
+        int[] array = {2,12,7,10,99};           // Creating an array with values
 
+        System.out.println("Given array:\n" + Arrays.toString(array));          // Displaying the array
+        System.out.println("\nFinding the no. of even elements in the array:\n" + obj.countEven(array, 0, 0));           // Calling the method to find the number of even elements in the array
     }
 
-    public int countEven(int[] data, int index, int count){
-        if (index == data.length){//
-            return count;
+    public int countEven(int[] data, int index, int count) {            // Method to find the number of even elements in the array
+        // Base case
+        if (index >= data.length) {         // Checking if the index is greater than the size of the list
+            return count;       // Returning the count
         }
-        if(checkEven(data[index])){//
-            count++;//
+
+        if (checkEven(data[index])) {           // Calling the method to check if the element is even
+            count++;          // Incrementing the count by 1
         }
-        return countEven(data, index+1, count);//
+
+        return countEven(data, index + 1, count);           // Calling the recursive function by incrementing the index by 1
     }
 
-    public boolean checkEven(int num){
-        if(num % 2 == 0){
+    public boolean checkEven(int num) {         // Method to check if the element is even
+        // Checking if the element is even
+        if (num % 2 == 0) {
             return true;
         }
         return false;
