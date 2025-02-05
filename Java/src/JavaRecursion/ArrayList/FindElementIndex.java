f3025936f2667aa5820cc4a926b76e7878cdaad5
iTzAswinRoy <aswinroy255@gmail.com>
2025-02-05 13:32:18 +0530

    Recursion using Array List

--- Java/src/JavaRecursion/ArrayList/FindElementIndex.java
+++ Java/src/JavaRecursion/ArrayList/FindElementIndex.java
@@ -1,55 +1,52 @@
 package JavaRecursion.ArrayList;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.ArrayList;         // Importing the array list class
+import java.util.Scanner;           // Importing scanner class
 
 // Write a recursive function to find the first index of a specific element in an ArrayList.
 public class FindElementIndex {
-    public static void main(String[] args) {
-        FindElementIndex obj = new FindElementIndex();
+    public static void main(String[] args) {        // Main method
+        FindElementIndex obj = new FindElementIndex();       // Creating an object of the class
+        Scanner in = new Scanner(System.in);        // Creating a scanner object
 
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();        // Creating an array list
+
+        // Calling the method to add elements in the array list
         list.add(1);
         list.add(2);
         list.add(3);
         list.add(4);
         list.add(5);
 
+        // Displaying the array list
         System.out.println("Given array list:\n" + list);
+
         System.out.println("\nEnter the element to find its index:");
+        int findElement = in.nextInt();         // Getting inputs from the user
 
-        Scanner in = new Scanner(System.in);
-        int findElement = in.nextInt();
+        int result = obj.findingIndex(list, 0, findElement);        // Calling the method to find the index of the element
 
-        if (obj.elementCheck(list, 0, findElement) ) {
-            int result = obj.findingIndex(list, 0, findElement);
-
+        // Checking if the element is present in the array list
+        if (result >= 0) {
             System.out.println("\nElement " + findElement + " is found at given index " + result);
         } else {
             System.out.println("\nElement not found!");
         }
-
-        in.close();
+        in.close();     // Closing the scanner
     }
 
-    public int findingIndex(ArrayList<Integer> data, int index, int element) {
-            if (index >= data.size()) {
-                return -1;
-            }
-            if (data.get(index) == element) {
-                return index;
-            }
-            return findingIndex(data, index + 1, element);
-    }
-
-    public boolean elementCheck(ArrayList<Integer> data, int index, int specificElement) {
+    public int findingIndex(ArrayList<Integer> data, int index, int element) {          // Method to find the index of the element
+        // Base case
+        // Checking if the index in within the bound
         if (index >= data.size()) {
-            return false;
+            return -1;      // If not, returns a negative value
         }
 
-        if (data.get(index) == specificElement) {
-            return true;
+        // Checking if the element match with the current index
+        if (data.get(index) == element) {
+            return index;       // Retuning the index
         }
-        return elementCheck(data, index + 1, specificElement);
+
+        return findingIndex(data, index + 1, element);          // Calling the recursive function by incrementing the index by 1
     }
 }
