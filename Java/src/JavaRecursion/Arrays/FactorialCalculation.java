e0377db13e9a89365659ff146375de51ce4115f5
iTzAswinRoy <aswinroy255@gmail.com>
2025-02-05 13:32:56 +0530

    Recursion using Array List

--- Java/src/JavaRecursion/Arrays/FactorialCalculation.java
+++ Java/src/JavaRecursion/Arrays/FactorialCalculation.java
@@ -2,17 +2,20 @@ package JavaRecursion.Arrays;
 
 // Write a recursive function to calculate the factorial of a number n.
 public class FactorialCalculation {
-    public static void main(String[] args){
-        FactorialCalculation obj = new FactorialCalculation();
+    public static void main(String[] args) {         // Main method
+        FactorialCalculation obj = new FactorialCalculation();      // Creating an object of the class
 
-        int result = obj.factorial(5);
-        System.out.println(result);
+        int result = obj.factorial(5);          // Calling the method to find the factorial of the number
+        System.out.println(result);         // Displaying the result
     }
 
-    public int factorial(int num){
-        if(num == 1){
-            return num;
+    public int factorial(int num) {         // Method to find the factorial of the number
+        // Base case
+        if (num == 1) {
+            return num;     // Returning the num
         }
-       return factorial(num - 1) * num;
+
+        // Calling the recursive function by multiplying the current number
+        return factorial(num - 1) * num;
     }
 }
