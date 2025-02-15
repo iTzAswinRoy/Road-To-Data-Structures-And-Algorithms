1fa862ca4bd02409054dd702ff8cde4a7d901ad5
iTzAswinRoy <aswinroy255@gmail.com>
2025-02-15 22:37:36 +0530

    Recursion using Array

--- Java/src/JavaRecursion/Arrays/FindingMaximumElement.java
+++ Java/src/JavaRecursion/Arrays/FindingMaximumElement.java
@@ -6,9 +6,9 @@ import java.util.Arrays;       // Importing arrays class
 
 public class FindingMaximumElement {
     public static void main(String[] args) {        // Main Method
-        FindingMaximumElement obj = new FindingMaximumElement();
+        FindingMaximumElement obj = new FindingMaximumElement();        // Creating an object of the class
 
-        int[] array = {8,2,9,3};
+        int[] array = {8,2,9,3};            // Creating an array
         System.out.println("Given array:\n" + Arrays.toString(array));        // Displaying the array
 
         int max = obj.findMax(array, array.length, Integer.MIN_VALUE);          // Calling the method to find the maximum element in the array
--- Java/src/JavaRecursion/Arrays/FindingSecondMaximum.java
+++ Java/src/JavaRecursion/Arrays/FindingSecondMaximum.java
@@ -6,13 +6,15 @@ import java.util.Arrays;            // Importing arrays class
 
 public class FindingSecondMaximum {
     public static void main(String[] args) {            // Main Method
-        FindingSecondMaximum obj = new FindingSecondMaximum();
+        FindingSecondMaximum obj = new FindingSecondMaximum();          // Creating an object of the class
 
-        int[] array = {8, 2, 9, 3};
-        int result = obj.findSecondMax(array, array.length, obj.findMax(array, array.length, Integer.MIN_VALUE), Integer.MIN_VALUE);//
+        int[] array = {8, 2, 9, 3};         // Creating an array
 
-        System.out.println("Given array:\n" + Arrays.toString(array));
-        System.out.println("\nThe second maximum element in the array:\n" + result);//
+        // Calling the method to find the second max in the array by storing it in a variable
+        int result = obj.findSecondMax(array, array.length, obj.findMax(array, array.length, Integer.MIN_VALUE), Integer.MIN_VALUE);
+
+        System.out.println("Given array:\n" + Arrays.toString(array));          // Converting the array to sting
+        System.out.println("\nThe second maximum element in the array:\n" + result);        // Displaying the result
     }
 
     public int findMax(int[] data, int length, int max) {
