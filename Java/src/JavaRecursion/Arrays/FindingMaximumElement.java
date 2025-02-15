004a412199abf59223d0a4149fc9dcf2f885b383
iTzAswinRoy <aswinroy255@gmail.com>
2025-02-15 22:26:31 +0530

    Recursion using Array

--- Java/src/JavaRecursion/Arrays/FindingMaximumElement.java
+++ Java/src/JavaRecursion/Arrays/FindingMaximumElement.java
@@ -1,26 +1,31 @@
 package JavaRecursion.Arrays;
 
-import java.util.Arrays;//
+import java.util.Arrays;       // Importing arrays class
 
 // Write a recursive function to find the maximum element in an array of integers.
+
 public class FindingMaximumElement {
-    public static void main(String[] args) {
+    public static void main(String[] args) {        // Main Method
         FindingMaximumElement obj = new FindingMaximumElement();
 
         int[] array = {8,2,9,3};
-        System.out.println("Given array:\n"+Arrays.toString(array));//
+        System.out.println("Given array:\n" + Arrays.toString(array));        // Displaying the array
 
-        int max = obj.findMax(array, array.length, Integer.MIN_VALUE);//
-        System.out.println("result:\n"+max);//
+        int max = obj.findMax(array, array.length, Integer.MIN_VALUE);          // Calling the method to find the maximum element in the array
+        System.out.println("result:\n" + max);        // Displaying the maximum value
     }
 
-    public int findMax(int[] data, int length, int max) {
-        if (length <= 0) {//
-            return max;
+    public int findMax(int[] data, int length, int max) {         // Method to find the maximum element in the array
+        // Base case
+        if (length <= 0) {      // Checking if the length is less than the zero
+            return max;         // Returning the max value
         }
-        if (data[length-1] > max) {
-            max = data[length-1];
+
+        // Checking if the current element is greater than the max value
+        if (data[length - 1] > max) {
+            max = data[length - 1];         // Updating the max value
         }
-        return findMax(data, length -1 , max);
+
+        return findMax(data, length - 1 , max);         // Calling the recursive function by passing the max value and reducing the length by 1
     }
 }
