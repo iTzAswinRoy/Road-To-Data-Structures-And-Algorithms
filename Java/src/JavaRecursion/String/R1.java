30fd671d2e840611ba87cc501e0e71ed7e4baa0d
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2025-02-23 22:15:20 +0530

    Update R1.java

--- Java/src/JavaRecursion/String/R1.java
+++ Java/src/JavaRecursion/String/R1.java
@@ -2,7 +2,7 @@ package JavaRecursion.String;
 
 // Skip a character
 public class R1 {//
-    public static void main(String[] args) {//
+    public static void main(String[] args) {
         R1 obj = new R1();//
         String temp = "baccad";//
 
