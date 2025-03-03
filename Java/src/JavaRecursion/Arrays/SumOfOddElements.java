2ec6a97a8de6a03b667caedbc612ffbbe5aa2d82
Aswin Roy <165917090+iTzAswinRoy@users.noreply.github.com>
2025-03-03 23:02:09 +0530

    Update SumOfOddElements.java

--- Java/src/JavaRecursion/Arrays/SumOfOddElements.java
+++ Java/src/JavaRecursion/Arrays/SumOfOddElements.java
@@ -1,6 +1,6 @@
 package JavaRecursion.Arrays;
 
-import java.util.Arrays;//
+import java.util.Arrays;
 
 // Write a recursive function to find the sum of elements at odd indices in an array.
 public class SumOfOddElements {//
