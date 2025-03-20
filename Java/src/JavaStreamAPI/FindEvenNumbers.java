f42e9e201c82cca4a5da58acf6ee920f06bab2bf
iTzAswinRoy <aswinroy255@gmail.com>
2025-03-20 23:02:45 +0530

    Stream API

---
+++ Java/src/JavaStreamAPI/FindEvenNumbers.java
@@ -0,0 +1,19 @@
+package JavaStreamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+
+import java.util.stream.Collectors;
+
+// Given a list of integers, use the Stream API to find all the even numbers.
+
+public class FindEvenNumbers {
+     public static void main(String[] args) {
+         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+         List<Integer> result = list.stream().filter(n -> n % 2 == 0)
+                 .collect(Collectors.toList());
+
+         System.out.println(result);
+     }
+}
