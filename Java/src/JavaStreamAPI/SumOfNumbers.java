e4b21d0f769fdd75dfd0d824e2e5fbe9ec629a0b
iTzAswinRoy <aswinroy255@gmail.com>
2025-03-20 23:03:07 +0530

    Stream API

---
+++ Java/src/JavaStreamAPI/SumOfNumbers.java
@@ -0,0 +1,16 @@
+package JavaStreamAPI;
+
+// Given a list of integers, use the Stream API to calculate the sum of all the numbers.
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+
+        int result = list.stream().mapToInt(Integer::intValue).sum();
+
+        System.out.println(result);
+    }
+}
