e700efce86c8f941dfe94a705e000e8c7f6e0931
iTzAswinRoy <aswinroy255@gmail.com>
2025-03-20 23:02:50 +0530

    Stream API

---
+++ Java/src/JavaStreamAPI/CountStringsWithSpecificLetter.java
@@ -0,0 +1,15 @@
+package JavaStreamAPI;
+
+
+import java.util.List;
+
+// Given a list of strings, use the Stream API to count how many strings start with the letter "A".
+public class CountStringsWithSpecificLetter {
+    public static void main(String[] args) {
+        List<String> words = List.of("Apple", "Banana", "Avocado", "Cherry", "Apricot", "Blueberry");
+
+        long count = words.stream().filter(n -> n.startsWith("A")).count();
+
+        System.out.println(count);
+    }
+}
